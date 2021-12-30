0bb783e19c946b5f73b34aba4d8bbd2febc8f3c8
kamaljohnson <kamaljohnson12345@gmail.com>
2021-12-30 14:21:31 +0530

    refactor: rename issue -> ticket in hooks

--- helpdesk/hooks.py
+++ helpdesk/hooks.py
@@ -9,7 +9,7 @@ app_license = "AGPLv3"
 
 # TODO: the logic for has_website_permission is minimised a little for now, will need to re-check
 has_website_permission = {
-	"Issue": "helpdesk.helpdesk.doctype.issue.issue.has_website_permission",
+	"Ticket": "helpdesk.helpdesk.doctype.ticket.ticket.has_website_permission",
 }
 
 doc_events = {
@@ -19,15 +19,15 @@ doc_events = {
 	"Communication": {
 		"on_update": [
 			"helpdesk.helpdesk.doctype.service_level_agreement.service_level_agreement.on_communication_update",
-			"helpdesk.helpdesk.doctype.issue.issue.set_first_response_time",
+			"helpdesk.helpdesk.doctype.ticket.ticket.set_first_response_time",
 		]
 	},
-	"Contact": {"on_trash": "helpdesk.helpdesk.doctype.issue.issue.update_issue",},
+	"Contact": {"on_trash": "helpdesk.helpdesk.doctype.ticket.ticket.update_ticket",},
 }
 
 scheduler_events = {
 	"daily": [
-		"helpdesk.helpdesk.doctype.issue.issue.auto_close_tickets",
+		"helpdesk.helpdesk.doctype.ticket.ticket.auto_close_tickets",
 		"helpdesk.helpdesk.doctype.service_level_agreement.service_level_agreement.check_agreement_status",
 	]
 }
