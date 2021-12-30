9ef532a67afbb73d8f50c9711cd988be9bc23dcc
kamaljohnson <kamaljohnson12345@gmail.com>
2021-12-30 19:29:30 +0530

    feat: add doctypes for category/articles

---
+++ helpdesk/helpdesk/doctype/category/category.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class Category(NestedSet):
+	pass
