01ccf3ccc989cbd39558ecec41e3da5da3d90894
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-03 07:41:02 +0530

    feat: add validator for category in article

--- helpdesk/helpdesk/doctype/article/article.py
+++ helpdesk/helpdesk/doctype/article/article.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2021, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class Article(Document):
-	pass
+	def validate(self):
+		category_doc = frappe.get_doc('Category', self.category)
+		if category_doc.is_group:
+			frappe.throw(_('Article category should not be a group'))
