23648ceb064f17879aa33e3ed74d50a097cb0b6f
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-05 05:25:11 +0530

    feat: add top-tags (helpful/views) in form view

--- helpdesk/helpdesk/doctype/article/article.js
+++ helpdesk/helpdesk/doctype/article/article.js
@@ -3,13 +3,26 @@
 
 frappe.ui.form.on('Article', {
 	refresh: function(frm) {
-		if(frm.doc.linked_web_page && frm.doc.published) {
-			frm.add_custom_button(
-				'Open Web Page',
-				() => {
-					window.open(`/${frm.doc.route}`)
-				}
-			);
-		}
+		frm.dashboard.clear_headline();
+
+		frm.dashboard.set_headline_alert(`
+			<div class="row">
+				<div class="col-md-4 col-xs-12">
+					<span class="indicator whitespace-nowrap red">
+						<span>Views: ${frm.doc.views || 0}</span>
+					</span>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<span class="indicator whitespace-nowrap green">
+						<span>Helpful: ${frm.doc.helpful || 0}</span>
+					</span>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<span class="indicator whitespace-nowrap red">
+						<span>Not Helpful: ${frm.doc.not_helpful || 0}</span>
+					</span>
+				</div>
+			</div>
+		`);
 	}
 });
