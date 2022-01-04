66d8a39749a1175f16bb5f5fd493984ca392eeba
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-05 04:58:07 +0530

    refactor: use has-web-page feature instead of web-page linking

--- helpdesk/helpdesk/doctype/article/article.py
+++ helpdesk/helpdesk/doctype/article/article.py
@@ -2,36 +2,17 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
-from frappe import _, throw
+from frappe.website.website_generator import WebsiteGenerator
+from frappe import _
 
-class Article(Document):
+class Article(WebsiteGenerator):
 	def validate(self):
 		category_doc = frappe.get_doc('Category', self.category)
 		if category_doc.is_group:
 			frappe.throw(_('Article category should not be a group'))
 
 	def before_save(self):
-		if self.linked_web_page:	
-			web_page = frappe.get_doc('Web Page', self.linked_web_page)
-			self.sync_web_page_details(web_page)
-			web_page.save()
-		else:
-			web_page = frappe.new_doc('Web Page')
-			self.sync_web_page_details(web_page)
-			web_page.insert()
-			
-			self.linked_web_page = web_page.name
-			self.route = web_page.route
-
-	def sync_web_page_details(self, web_page):
-			web_page.title = self.title
-			web_page.content_type = 'HTML'
-			web_page.main_section_html = self.content
-			web_page.published = self.published
-			web_page.show_title = True
-
-			web_page.route = self.get_page_route()
+		self.route = self.get_page_route()
 
 	def get_page_route(self, route='', category=None):
 		def change_case(str):
