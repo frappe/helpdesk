b4f27de8b24dc51de33b753e843b67d63317e1f7
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-05 10:36:29 +0530

    feat: add web-page routing for category in hc, with sample page

--- helpdesk/helpdesk/doctype/category/category.py
+++ helpdesk/helpdesk/doctype/category/category.py
@@ -1,8 +1,50 @@
 # Copyright (c) 2021, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.utils.nestedset import NestedSet
 
 class Category(NestedSet):
-	pass
+	def before_save(self):
+		self.route = self.get_page_route()
+		if self.linked_web_page:
+			web_page = frappe.get_doc('Web Page', self.linked_web_page)
+			self.sync_web_page_details(web_page)
+			web_page.save()
+		else:
+			web_page = frappe.new_doc('Web Page')
+			self.sync_web_page_details(web_page)
+			web_page.insert()
+			self.linked_web_page = web_page.name
+
+	def sync_web_page_details(self, web_page):
+		web_page.title = self.name
+		web_page.route = self.route
+		web_page.content_type = 'HTML'
+		web_page.main_section_html = '<div> <h1>Hello</h1> </div>'
+		web_page.published = True
+
+	def get_page_route(self, route='', category=None):
+		def change_case(str):
+			res = [str[0].lower()]
+			for c in str[1:]:
+				if c in ('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
+					res.append('_')
+					res.append(c.lower())
+				elif c in (' '):
+					continue
+				else:
+					res.append(c)
+			
+			return ''.join(res)
+
+		if not category:
+			category = self.name
+		
+		category_doc = frappe.get_doc('Category', category)
+		route = f'{change_case(category_doc.name)}{f"/{route}" if route else ""}'
+		
+		if category_doc.parent_category:
+			return self.get_page_route(route, category_doc.parent_category)
+		else:
+			return f'hc/{route}'
--- helpdesk/www/hc/index.py
+++ helpdesk/www/hc/index.py
@@ -9,3 +9,7 @@ def get_context(context):
 def get_child_categories(category):
     child_categories = frappe.get_all('Category', fields=['name', 'description', 'thumbnail', 'parent_category', 'is_group'], filters={'parent_category': ['=', category]})
     return child_categories
+
+@frappe.whitelist()
+def get_category_website_route(category):
+    return frappe.get_doc('Category', category)
