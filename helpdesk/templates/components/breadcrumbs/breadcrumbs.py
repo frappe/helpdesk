415805ba1a0d0b55506d1f4fbd4e4587032c9647
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-19 03:32:15 +0530

    feat: add breadcrumbs parents generator using route

---
+++ helpdesk/templates/components/breadcrumbs/breadcrumbs.py
@@ -0,0 +1,28 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist(allow_guest=True)
+def get_breadcrumbs(route):
+    print(route)
+    allowed_doctypes = [
+        {
+            "name": "Article",
+            "title_field": "title"
+        },
+        {
+            "name": "Category",
+            "title_field": "category_name"
+        }
+    ]
+    
+    parents = []
+    splits = route.split("/")
+    if splits:
+        for index, route in enumerate(splits, start=1):
+            full_route = "/".join(splits[:index])
+            for doctype in allowed_doctypes:
+                label = frappe.get_all(doctype["name"], filters=[["route", "=", full_route[1:]]], pluck=doctype["title_field"])
+                if label:
+                    parents.append({"route": "/" + full_route, "label": label[0]})
+                    break
+        return parents
