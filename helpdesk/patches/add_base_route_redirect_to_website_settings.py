9b0c39185a5fc97f66565a2169a2cba648552fa0
kamaljohnson <kamaljohnson12345@gmail.com>
2022-01-25 09:48:40 +0530

    feat: add kb to home page, remove home page redirect

--- helpdesk/patches/add_base_route_redirect_to_website_settings.py
+++
@@ -1,4 +0,0 @@
-from helpdesk.setup.install import add_base_route_redirect_to_website_settings
-
-def execute():
-    add_base_route_redirect_to_website_settings()
--- helpdesk/setup/install.py
+++ helpdesk/setup/install.py
@@ -1,26 +1,17 @@
 import frappe
 
 def before_install():
-    add_base_route_redirect_to_website_settings()
+    set_home_page_to_kb()
 
 def after_install():
     add_default_categories()
 
-def add_base_route_redirect_to_website_settings():
+def set_home_page_to_kb():
     website_settings = frappe.get_doc("Website Settings")
-    
-    for route_redirects in website_settings.route_redirects:
-        if(route_redirects.source == "/" or route_redirects.source == ""):
-            return
 
-    base_route = frappe.get_doc({
-        "doctype": "Website Route Redirect",
-        "source": "/" ,
-        "target": "/support/kb"
-    })
-
-    website_settings.append('route_redirects', base_route)
-    website_settings.save()
+    if not website_settings.home_page:
+        website_settings.home_page = '/support/kb'
+        website_settings.save()
 
 def add_default_categories():
     frappe.get_doc({
