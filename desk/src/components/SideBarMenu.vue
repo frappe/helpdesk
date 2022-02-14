d6a5fa8f79a41e696f53d447dbe1239c6dc62eef
kamaljohnson <kamaljohnson12345@gmail.com>
2022-02-14 11:18:22 +0530

    feat: add settings button on side menu bar

--- desk/src/components/SideBarMenu.vue
+++ desk/src/components/SideBarMenu.vue
@@ -1,16 +1,23 @@
 <template>
-	<div class="space-y-5 px-3">
-		<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
-			<CustomIcons name="ticket" class="group-hover:stroke-blue-600" width="34" height="34"/>
+	<div class="flex flex-col" :style="{ height: viewportWidth > 768 ? 'calc(100vh - 8rem)' : null }">
+		<div class="space-y-5 px-3 mb-auto">
+			<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
+				<CustomIcons name="ticket" class="group-hover:stroke-blue-600" width="34" height="34"/>
+			</div>
+			<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
+				<CustomIcons name="knowledge-base" class="group-hover:stroke-blue-600" width="30" height="30"/>
+			</div>
+			<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
+				<CustomIcons name="customers" class="group-hover:stroke-blue-600" width="32" height="32"/>
+			</div>
+			<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
+				<CustomIcons name="reports" class="group-hover:stroke-blue-600" width="30" height="30"/>
+			</div>
 		</div>
-		<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
-			<CustomIcons name="knowledge-base" class="group-hover:stroke-blue-600" width="30" height="30"/>
-		</div>
-		<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
-			<CustomIcons name="customers" class="group-hover:stroke-blue-600" width="32" height="32"/>
-		</div>
-		<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
-			<CustomIcons name="reports" class="group-hover:stroke-blue-600" width="30" height="30"/>
+		<div class="space-y-5 px-3 mt-auto">
+			<div class="group p-2 hover:bg-blue-50 rounded-md cursor-pointer">
+				<CustomIcons name="settings" class="group-hover:stroke-blue-600" width="30" height="30"/>
+			</div>
 		</div>
 	</div>
 </template>
@@ -20,6 +27,7 @@ import CustomIcons from "./CustomIcons.vue"
 
 export default {
 	name: 'SideBarMenu',
+	inject: ['viewportWidth'],
 	props: ['selectedMenuItem'],
 	data() {
 		return {
