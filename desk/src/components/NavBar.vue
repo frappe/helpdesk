0b17cee33c44c194f5d2c688db4d161fb75deec2
kamaljohnson <kamaljohnson12345@gmail.com>
2022-02-16 09:10:46 +0530

    fix: padding issue in navbar

--- desk/src/components/NavBar.vue
+++ desk/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flow-root pb-3 pt-4 pl-5 pr-10 border-b">
+    <div class="flow-root pb-3 pt-4 pl-5 pr-8 border-b">
         <div class="float-right flex space-x-3">
             <div class="border-r-2 pr-2 pt-1">
                 <FeatherIcon class="w-5 h-5" name="bell" />
