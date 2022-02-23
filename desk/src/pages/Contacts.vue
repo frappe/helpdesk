30686c5f885cd5391b1a0d5e90cc464ada6fa1f2
kamaljohnson <kamaljohnson12345@gmail.com>
2022-02-24 04:03:06 +0530

    feat: create new contact

--- desk/src/pages/Contacts.vue
+++ desk/src/pages/Contacts.vue
@@ -4,7 +4,7 @@
 			<div class="float-left ml-4">
 				<div class="flex items-center space-x-4">
 					<Input type="checkbox" value="" />
-					<Button icon-left="plus" appearance="primary" @click="() => {showNewTicketDialog = true}">Add Contact</Button>
+					<Button icon-left="plus" appearance="primary" @click="() => {showNewContactDialog = true}">Add Contact</Button>
 				</div>
 			</div>
 			<div class="float-right">
@@ -14,12 +14,13 @@
 		<div v-if="contacts">
 			<ContactList :contacts="contacts" />
 		</div>
+		<NewContactDialog v-model="showNewContactDialog" @contact-created="(contact) => {contactCreated(contact)}"/>
 	</div>
 </template>
 <script>
 import { Input } from 'frappe-ui'
 import ContactList from '../components/contacts/ContactList.vue'
-import NewTicketDialog from '../components/tickets/NewTicketDialog.vue'
+import NewContactDialog from '../components/global/NewContactDialog.vue'
 
 export default {
 	name: 'Contacts',
@@ -27,11 +28,11 @@ export default {
 	components: {
 		ContactList,
 		Input,
-		NewTicketDialog
+		NewContactDialog
 	},
 	data() {
 		return {
-			showNewTicketDialog: false
+			showNewContactDialog: false
 		}
 	},
     resources: {
@@ -53,6 +54,12 @@ export default {
         contacts() {
             return this.$resources.contacts.data || null
         }
-    }
+    },
+	methods: {
+		contactCreated(contact) {
+			this.$resources.contacts.fetch();
+			this.showNewContactDialog = false
+		}
+	}
 }
 </script>
