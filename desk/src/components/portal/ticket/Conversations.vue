fe53ef194f534429ef27cc56d2376eb7fe2b7163
kamaljohnson <kamaljohnson12345@gmail.com>
2022-03-11 09:52:17 +0530

    feat: add fetching converstions loading text

--- desk/src/components/portal/ticket/Conversations.vue
+++ desk/src/components/portal/ticket/Conversations.vue
@@ -1,33 +1,39 @@
 <template>
 	<div>
-		<div
-			:v-if="conversations"
-			v-for="(conversation, index) in conversations" :key="conversation.name" 
-			class="flex flex-col pr-3"
-			ref="conversationContainer"
-		>
-			<div :ref="`conversation-${index}`">
-				<ConversationCard 
-					:userName="(conversation.sender.first_name ? conversation.sender.first_name : '') + ' ' + (conversation.sender.last_name ? conversation.sender.last_name : '')" 
-					:profilePicUrl="conversation.sender.image ? conversation.sender.image : ''" 
-					:time="conversation.creation" 
-					:message="conversation.content"
-					color="gray" 
-				/>
-				<!-- TODO: dynamically set color based on user -->
+		<div v-if="conversations">
+			<div
+				v-for="(conversation, index) in conversations" :key="conversation.name" 
+				class="flex flex-col pr-3"
+				ref="conversationContainer"
+			>
+				<div :ref="`conversation-${index}`">
+					<ConversationCard 
+						:userName="(conversation.sender.first_name ? conversation.sender.first_name : '') + ' ' + (conversation.sender.last_name ? conversation.sender.last_name : '')" 
+						:profilePicUrl="conversation.sender.image ? conversation.sender.image : ''" 
+						:time="conversation.creation" 
+						:message="conversation.content"
+						color="gray" 
+					/>
+					<!-- TODO: dynamically set color based on user -->
+				</div>
 			</div>
+		</div>
+		<div v-else class="p-5">
+			<LoadingText text="Fetching conversations..."/>
 		</div>
 	</div>
 </template>
 
 <script>
 import ConversationCard from "./ConversationCard.vue"
+import { LoadingText } from 'frappe-ui'
 
 export default {
 	name: "Conversations",
 	props: ["ticketId", "scrollToBottom"],
 	components: {
-		ConversationCard
+		ConversationCard,
+		LoadingText
 	},
 	resources: {
 		conversations() {
