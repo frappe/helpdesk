b43c2cfbff68d96b8695efe07e53550aee259a69
kamaljohnson <kamaljohnson12345@gmail.com>
2022-03-31 22:49:48 +0530

    feat: add settings top section action buttons

--- desk/src/components/desk/settings/SettingsTopPanel.vue
+++ desk/src/components/desk/settings/SettingsTopPanel.vue
@@ -1,12 +1,55 @@
 <template>
-    <div class="flex py-3 items-center border-b pl-5">
-        <div class="font-bold text-2xl">Settings</div>
+    <div class="flow-root py-3 pl-5 pr-8 border-b">
+        <div class="float-left">
+            <div class="flex items-center">
+                <div class="font-bold text-2xl">Settings</div>
+            </div>
+        </div>
+        <div class="float-right">
+            <div class="flex space-x-3 items-center">
+                <div v-for="action in actions" :key="action">
+                    <Button :appearance="action.appearance" @click="action.onClick()" :icon-left="action.icon">{{ action.label }}</Button>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ["selectedSetting"]
+    props: ["selectedSetting"],
+    computed: {
+        actions() {
+            switch(this.$route.name) {
+                case 'Agents':
+                    return [
+                        {
+                            label: 'Add Agent',
+                            icon: 'plus',
+                            appearance: 'primary',
+                            onClick: () => {
+                                // TODO: redirect to new agent page
+                            }
+                        }
+                    ]
+                case 'SlaPolicies':
+                    return [
+                        {
+                            label: 'Add Policy',
+                            icon: 'plus',
+                            appearance: 'primary',
+                            onClick: () => {
+                                this.$router.push({
+                                    name: 'NewSlaPolicy'
+                                })
+                            }
+                        }
+                    ]
+                default:
+                    return []
+            }
+        }
+    }
 }
 </script>
 
