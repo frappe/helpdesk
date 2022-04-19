153c74c6279dad84f6885b314e83daa586e8840a
kamaljohnson <kamaljohnson12345@gmail.com>
2022-04-19 11:18:00 +0530

    refactor: role profile -> individual role assign

---
+++ helpdesk/patches/add_roles_to_agent_user.py
@@ -0,0 +1,7 @@
+import frappe
+
+def execute():
+    agents = frappe.get_all("Agent", fields=["user", "name"])
+    for agent in agents:
+        agent_doc = frappe.get_doc("Agent", agent)
+        agent_doc.set_user_roles()
