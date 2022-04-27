528baddf9c8f4716caa64ee939fde0f1806228dc
kamaljohnson <kamaljohnson12345@gmail.com>
2022-04-27 18:41:50 +0530

    fix: time formating in activity card

--- desk/src/components/desk/ticket/ActivityCard.vue
+++ desk/src/components/desk/ticket/ActivityCard.vue
@@ -8,7 +8,7 @@
 		<div class="grow pr-[6px]">
 			<div class="flex flex-col">
 				<div><span class="font-medium text-gray-900">{{ activity.owner }}</span> <span class="font-normal text-gray-700">{{ activity.action }}</span></div>
-				<div class="text-gray-600 text-[11px]">{{ $dayjs(activity.creation).fromNow() }}</div>
+				<div class="text-gray-600 text-[11px]">{{ $dayjs.longFormating($dayjs(activity.creation).fromNow()) }}</div>
 			</div>
 		</div>
 	</div>
