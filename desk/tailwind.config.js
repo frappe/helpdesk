6bfa4f84bd1e5171b86c651c9e14b7a36247e782
kamaljohnson <kamaljohnson12345@gmail.com>
2022-05-20 15:49:39 +0530

    fix: frappe-ui css issue

--- desk/tailwind.config.js
+++ desk/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
+    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+    '../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
