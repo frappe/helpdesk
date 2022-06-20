64a09e9c8fa76be6ab0591a119345c2116b32f3c
kamaljohnson <kamaljohnson12345@gmail.com>
2022-06-20 20:27:31 +0530

    fix: ticket list component issue in contact page view

--- desk/src/pages/desk/Tickets.vue
+++ desk/src/pages/desk/Tickets.vue
@@ -69,134 +69,7 @@
 							</div>
 						</div>
 					</div>
-					<div
-						@pointerenter="() => { showSelectAllCheckbox = true}"
-						@pointerleave="() => { showSelectAllCheckbox = false}"
-						class="bg-[#F7F7F7] group flex items-center text-base font-medium text-gray-500 py-[10px] pl-[11px] pr-[49.80px] rounded-[6px] select-none"
-					>
-						<div class="w-[37px] h-[14px]">
-							<Input 
-								type="checkbox" 
-								@click="manager.selectAll()" 
-								:checked="manager.allItemsSelected" 
-								class="cursor-pointer mr-1 hover:visible" 
-								:class="manager.allItemsSelected || showSelectAllCheckbox ? 'visible' : 'invisible'" 
-							/>
-						</div>
-						<div 
-							class="sm:w-1/12 flex flex-row items-center space-x-[7px] cursor-pointer"
-							@click="manager.toggleOrderBy('name')"
-						>
-							<span>#</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'name'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-						<div 
-							class="sm:w-8/12 flex flex-row items-center space-x-[6px] cursor-pointer"
-							@click="manager.toggleOrderBy('subject')"
-						>
-							<span>Subject</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'subject'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-						<div 
-							class="sm:w-3/12 flex flex-row items-center space-x-[6px] cursor-pointer"
-							@click="manager.toggleOrderBy('status')"
-						>
-							<span>Status</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'status'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-						<div 
-							class="sm:w-3/12 flex flex-row items-center space-x-[6px] cursor-pointer"
-							@click="manager.toggleOrderBy('contact')"
-						>
-							<span>Created By</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'contact'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-						<div 
-							class="sm:w-2/12 flex flex-row items-center space-x-[6px] cursor-pointer"
-							@click="manager.toggleOrderBy('resolution_by')"
-						>
-							<span>Due In</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'resolution_by'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-						<div
-							class="sm:w-1/12 flex flex-row items-center space-x-[6px] cursor-pointer"
-							@click="manager.toggleOrderBy('modified')"
-						>
-							<span>Modified</span>
-							<div class="w-[10px]">
-								<CustomIcons 
-									v-if="manager.options.order_by.split(' ')[0] === 'modified'"
-									:name="manager.options.order_by.split(' ')[1] === 'desc' ? 'chevron-down' : 'chevron-up'"
-									class="h-[6px] fill-gray-400 stroke-transparent" 
-								/>
-							</div>
-						</div>
-					</div>
-					<div 
-						id="rows" 
-						class="flex flex-col space-y-2 overflow-scroll"
-						:style="{ height: viewportWidth > 768 ? 'calc(100vh - 6.4rem)' : null }"
-					>
-						<div v-if="manager.loading">
-							<div v-for="n in 3" :key="n">
-								<TicketListItemSkeleton />
-							</div>
-						</div>
-						<div v-else>
-							<div v-if="manager.list.length > 0">
-								<div v-for="ticket in manager.list" :key="ticket.name">
-									<TicketListItem :ticket="ticket" @toggle-select="manager.select(ticket)" :selected="manager.itemSelected(ticket)" />
-								</div>
-							</div>
-							<div v-else>
-								<div class="grid place-content-center h-48 w-full">
-									<div>
-										<CustomIcons name="empty-list" class="h-12 w-12 mx-auto mb-2" />
-										<div class="text-gray-500 mb-2">No tickets found</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div v-if="!manager.loading && !(manager.list.length == 0 && manager.start == 0)" class="flex justify-center">
-							<div class="flex flex-row space-x-2">
-								<Button appearance="minimal" icon-left="chevron-left" @click="manager.previousPage()">Previous</Button>
-								<Button appearance="primary"> {{ manager.currPage }} </Button>
-								<Button appearance="minimal" @click="manager.getPage(manager.currPage + 1)"> {{ manager.currPage + 1 }} </Button>
-								<Button appearance="minimal" @click="manager.getPage(manager.currPage + 2)"> {{ manager.currPage + 2 }}</Button>
-								<Button appearance="minimal" icon-right="chevron-right" @click="manager.nextPage()">Next</Button>
-							</div>
-						</div>
-					</div>
+					<TicketList :manager="manager" />
 				</div>
 			</template>
 		</ListManager>
@@ -204,31 +77,26 @@
 	</div>
 </template>
 <script>
-import { Input, Dropdown, FeatherIcon, Dialog } from 'frappe-ui'
-import NewTicketDialog from '@/components/desk/tickets/NewTicketDialog.vue'
-import CustomIcons from '@/components/desk/global/CustomIcons.vue'
-import FilterBox from '@/components/desk/global/FilterBox.vue'
+import { Dropdown, FeatherIcon, Dialog } from 'frappe-ui'
 import { inject, ref } from 'vue'
-import TicketListItem from '@/components/desk/tickets/TicketListItem.vue'
-import TicketListItemSkeleton from '@/components/desk/tickets/TicketListItemSkeleton.vue'
+import NewTicketDialog from '@/components/desk/tickets/NewTicketDialog.vue'
+import FilterBox from '@/components/desk/global/FilterBox.vue'
+import TicketList from '@/components/desk/tickets/TicketList.vue'
 import ListManager from '@/components/global/ListManager.vue'
 import LoadingText from 'frappe-ui/src/components/LoadingText.vue'
 
 export default {
 	name: 'Tickets',
 	components: {
-    Input,
-    Dialog,
-    NewTicketDialog,
-    CustomIcons,
-    Dropdown,
-    FeatherIcon,
-    FilterBox,
-    ListManager,
-    TicketListItem,
-	TicketListItemSkeleton,
-    LoadingText
-},
+		Dialog,
+		NewTicketDialog,
+		Dropdown,
+		FeatherIcon,
+		FilterBox,
+		ListManager,
+		TicketList,
+		LoadingText
+	},
 	setup() {
 		const user = inject('user')
 		const tickets = inject('tickets')
