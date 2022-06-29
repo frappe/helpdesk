b8279249055bacdf8b27c9df931fc9d6312a9385
kamaljohnson <kamaljohnson12345@gmail.com>
2022-06-29 11:04:03 +0530

    refactor: list page controller ui

--- desk/src/components/global/ListPageController.vue
+++ desk/src/components/global/ListPageController.vue
@@ -1,11 +1,11 @@
 <template>
     <div v-if="!manager.loading && !(manager.list.length == 0 && manager.start == 0)" class="flex justify-center">
         <div v-if="manager.hasPage(2)" class="flex flex-row space-x-2">
-            <Button v-if="manager.hasPage(manager.currPage -1)" appearance="minimal" icon-left="chevron-left" @click="manager.previousPage()">Previous</Button>
+            <Button :disabled="!manager.hasPage(manager.currPage - 1)" appearance="minimal" icon-left="chevron-left" @click="manager.previousPage()">Previous</Button>
             <Button appearance="primary"> {{ manager.currPage }} </Button>
-            <Button v-if="manager.hasPage(manager.currPage + 1)" appearance="minimal" @click="manager.getPage(manager.currPage + 1)"> {{ manager.currPage + 1 }} </Button>
-            <Button v-if="manager.hasPage(manager.currPage + 2)" appearance="minimal" @click="manager.getPage(manager.currPage + 2)"> {{ manager.currPage + 2 }}</Button>
-            <Button v-if="manager.hasPage(manager.currPage + 1)" appearance="minimal" icon-right="chevron-right" @click="manager.nextPage()">Next</Button>
+            <Button :disabled="!manager.hasPage(manager.currPage + 1)" appearance="minimal" @click="manager.getPage(manager.currPage + 1)"> {{ manager.currPage + 1 }} </Button>
+            <Button :disabled="!manager.hasPage(manager.currPage + 2)" appearance="minimal" @click="manager.getPage(manager.currPage + 2)"> {{ manager.currPage + 2 }}</Button>
+            <Button :disabled="!manager.hasPage(manager.currPage + 1)" appearance="minimal" icon-right="chevron-right" @click="manager.nextPage()">Next</Button>
         </div>
     </div>
 </template>
