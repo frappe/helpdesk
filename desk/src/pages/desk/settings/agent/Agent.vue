128ea373f09b9d4d904b8980df744cb2631b9323
kamaljohnson <kamaljohnson12345@gmail.com>
2022-07-11 11:27:29 +0530

    feat: add agent page route

---
+++ desk/src/pages/desk/settings/agent/Agent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-row w-full h-full">
+        <AgentInfo class="shrink-0 border-r border-[#F4F5F6]" :agent="agentId" />
+        <RelatedInfo class="grow" :agent="agentId" />
+    </div>
+</template>
+
+<script>
+import AgentInfo from '@/components/desk/settings/agents/AgentInfo.vue'
+import RelatedInfo from '@/components/desk/settings/agents/RelatedInfo.vue'
+
+export default {
+    name: 'Agent',
+    props: ['agentId'],
+    components: {
+        AgentInfo,
+        RelatedInfo
+    },
+    setup() {
+
+    },
+    activated() {
+		this.$event.emit('set-selected-setting', 'Agents')
+    },
+
+}
+</script>
