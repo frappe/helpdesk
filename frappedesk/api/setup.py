0feac6a25a23e7a23b65017eecc2a968357c3940
kamaljohnson <kamaljohnson12345@gmail.com>
2022-07-21 20:55:48 +0530

    feat: add new ticket on setup

--- frappedesk/api/setup.py
+++ frappedesk/api/setup.py
@@ -1,4 +1,5 @@
 import frappe
+from frappedesk.frappedesk.doctype.ticket.ticket import create_communication_via_contact
 
 @frappe.whitelist()
 def initial_agent_setup():
@@ -14,3 +15,29 @@ def initial_agent_setup():
 			support_settings_doc.initial_agent_set = True
 			support_settings_doc.save()
 			return
+
+@frappe.whitelist()
+def initial_demo_ticket_created():
+	support_settings_doc = frappe.get_doc("Support Settings", "Support Settings")
+	if support_settings_doc.initial_demo_ticket_created:
+		return    
+	ticket_count = len(frappe.get_all("Ticket"))
+	if ticket_count == 0:
+		agent = frappe.get_last_doc("Agent")
+		if agent:
+			new_ticket_doc = frappe.new_doc("Ticket")
+			new_ticket_doc.subject = "Demo Ticket"
+			new_ticket_doc.description = "This is a demo ticket created by the initial setup script."
+			new_ticket_doc.ticket_type = "Question"
+			new_ticket_doc.priority = "Low"
+			new_ticket_doc.raised_by = "Administrator"
+			new_ticket_doc.via_customer_portal = True
+			new_ticket_doc.insert()
+
+			create_communication_via_contact(new_ticket_doc.name, new_ticket_doc.description)
+
+			support_settings_doc.initial_demo_ticket_created = True
+			support_settings_doc.save()
+			return
+	else:
+		return
