47a0b473c7fb1c40d4f30c56f7e247f2905ad310
kamaljohnson <kamaljohnson12345@gmail.com>
2022-08-09 04:31:56 +0530

    refactor: remove child table based category/article ordering

--- frappedesk/frappedesk/doctype/category/category.py
+++ frappedesk/frappedesk/doctype/category/category.py
@@ -21,71 +21,11 @@ class Category(WebsiteGenerator):
 			all_group_categories = frappe.get_all("Category", filters={"is_group": ["=", "1"]}, pluck="name")
 			if(len(all_group_categories) >= 6):
 				frappe.throw(_("Can only create a maximum of 6 groups categories"))
-		
-	def after_insert(self):
-		self.update_article_and_sub_category_ordering()
 
 	#TODO: when renamed, website route should be updated
 	def before_save(self):
 		if self.is_group or self.parent_category:
 			self.route = self.get_page_route()
-
-	def on_update(self):
-		self.update_article_and_sub_category_ordering()
-		if self.parent_category and not self.is_group:
-			parent_category_doc = frappe.get_doc("Category", self.parent_category)
-			if not next((x for x in parent_category_doc.sub_categories if x.sub_category == self.category_name), None):
-				parent_category_doc.append('sub_categories', {'sub_category': self.category_name})
-				parent_category_doc.save()
-
-	def update_article_and_sub_category_ordering(self):
-		if self.is_group:
-			# reset previous sub categories to null
-			all_previous_sub_categories = frappe.get_all("Category", filters={"parent_category": ["=", self.name]}, pluck="name")
-			for category in all_previous_sub_categories:
-				category_doc = frappe.get_doc("Category", category)
-				category_doc.parent_category = ""
-				category_doc.order = 0
-				category_doc.save()
-
-			# set parent_category fields for all the sub_cateogries
-			for index, category in enumerate(self.sub_categories):
-				category_doc = frappe.get_doc("Category", category.sub_category)
-				if category_doc:
-					if not category_doc.is_group:
-						if not category_doc.parent_category or category_doc.parent_category == self.name:
-							category_doc.parent_category = self.name
-							category_doc.order = index
-							category_doc.set_page_route()
-							category_doc.save()
-						else:
-							frappe.throw(_(f"{category_doc.category_name} is already a child category of {category_doc.parent_category}, please remove it and try again"))
-					else:
-						frappe.throw(_(f"{category_doc.category_name} is a group category, and cannot be added as a sub category"))
-				else:
-					frappe.throw(_(f"No category named {category.sub_category} found"))
-		else:
-			# reset previous article with this category to null
-			all_previous_category_articles = frappe.get_all("Article", filters={"category": ["=", self.name]})
-			for article in all_previous_category_articles:
-				article_doc = frappe.get_doc("Article", article)
-				article_doc.category = ""
-				article_doc.order = 0
-				article_doc.save()
-			
-			# set parent_category fields for all the sub_cateogries
-			for index, article in enumerate(self.articles):
-				article_doc = frappe.get_doc("Article", article.article)
-				if article_doc:
-					if not article_doc.category or article_doc.category == self.name:
-						article_doc.category = self.name
-						article_doc.order = index
-						article_doc.set_page_route()
-						article_doc.save()
-					else:
-						frappe.throw(_(f"{article_doc.title} is already a child category of {article_doc.category}, please remove it and try again"))
-				else:
-					frappe.throw(_(f"No article named {article.article} found"))
 
 	def set_page_route(self):
 		self.route = self.get_page_route()
