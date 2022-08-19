c74f2dce208b0f496fc15e23003edbff79f722dc
kamaljohnson <kamaljohnson12345@gmail.com>
2022-08-19 09:42:01 +0530

    feat: add initial frappe desk name setup

--- frappedesk/api/settings.py
+++ frappedesk/api/settings.py
@@ -1,9 +1,17 @@
 import frappe
 
-@frappe.whitelist(allow_guest=True)
+@frappe.whitelist()
 def update_helpdesk_name(name):
     doc = frappe.get_doc("Frappe Desk Settings")
     doc.helpdesk_name = name
     doc.save(ignore_permissions=True)
 
     return doc.helpdesk_name
+
+@frappe.whitelist()
+def skip_helpdesk_name_setup():
+    doc = frappe.get_doc("Frappe Desk Settings")
+    doc.initial_helpdesk_name_setup_skipped = True
+    doc.save(ignore_permissions=True)
+
+    return doc
