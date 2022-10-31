a47566873a186989650a660d7c6997a2228fc208
kamaljohnson <kamaljohnson12345@gmail.com>
2022-10-31 08:57:12 +0530

    feat: add patch to create assignement rules for exsisting agent groups & add agents to the user list

--- frappedesk/frappedesk/doctype/agent_group/agent_group.py
+++ frappedesk/frappedesk/doctype/agent_group/agent_group.py
@@ -7,58 +7,61 @@ from frappe.exceptions import DoesNotExistError
 
 
 class AgentGroup(Document):
-	# TODO: handle document rename
-	def after_insert(self):
-		self.create_assignment_rule()
+    # TODO: handle document rename
+    def after_insert(self):
+        self.create_assignment_rule()
 
-	def on_trash(self):
-		try:
-			rule = self.get_assignment_rule()
-			if rule:
-				rule.delete(ignore_permissions=True)
-		except DoesNotExistError:
-			# TODO: Log this error
-			pass
+    def on_trash(self):
+        try:
+            rule = self.get_assignment_rule()
+            if rule:
+                rule.delete(ignore_permissions=True)
+        except DoesNotExistError:
+            # TODO: Log this error
+            pass
 
-	def create_assignment_rule(self):
-		rule_doc = frappe.new_doc("Assignment Rule")
-		rule_doc.name = f"Support Rotation - {self.name}"
-		rule_doc.document_type = "Ticket"
-		rule_doc.assign_condition = f"status == 'Open' and agent_group == '{self.name}'"
+    def create_assignment_rule(self):
+        if frappe.db.exists("Assignment Rule", f"Support Rotation - {self.name}"):
+            return
 
-		for day in [
-			"Monday",
-			"Tuesday",
-			"Wednesday",
-			"Thursday",
-			"Friday",
-			"Saturday",
-			"Sunday",
-		]:
-			day_doc = frappe.get_doc({"doctype": "Assignment Rule Day", "day": day})
-			rule_doc.append("assignment_days", day_doc)
+        rule_doc = frappe.new_doc("Assignment Rule")
+        rule_doc.name = f"Support Rotation - {self.name}"
+        rule_doc.document_type = "Ticket"
+        rule_doc.assign_condition = f"status == 'Open' and agent_group == '{self.name}'"
 
-		rule_doc.save(ignore_permissions=True)
+        for day in [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ]:
+            day_doc = frappe.get_doc({"doctype": "Assignment Rule Day", "day": day})
+            rule_doc.append("assignment_days", day_doc)
 
-	def get_assignment_rule(self):
-		return get_assignement_rule_by_group_name(self.name)
+        rule_doc.save(ignore_permissions=True)
+
+    def get_assignment_rule(self):
+        return get_assignement_rule_by_group_name(self.name)
 
 
 def get_assignement_rule_by_group_name(name):
-	rule_doc = None
-	try:
-		rule_doc = frappe.get_last_doc(
-			"Assignment Rule",
-			filters=[
-				[
-					"Assignment Rule",
-					"assign_condition",
-					"=",
-					f"status == 'Open' and agent_group == '{name}'",
-				],
-			],
-		)
-	except DoesNotExistError:
-		pass
+    rule_doc = None
+    try:
+        rule_doc = frappe.get_last_doc(
+            "Assignment Rule",
+            filters=[
+                [
+                    "Assignment Rule",
+                    "assign_condition",
+                    "=",
+                    f"status == 'Open' and agent_group == '{name}'",
+                ],
+            ],
+        )
+    except DoesNotExistError:
+        pass
 
-	return rule_doc
+    return rule_doc
---
+++ frappedesk/patches/create_assignment_rules_for_agent_groups.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+def execute():
+    all_groups = frappe.get_all("Agent Group", fields=["name"])
+    for group in all_groups:
+        group_doc = frappe.get_doc("Agent Group", group.name)
+        group_doc.create_assignment_rule()
+        group_doc.save()
+
+    agents = frappe.get_all("Agent")
+    for agent in agents:
+        agent_doc = frappe.get_doc("Agent", agent.name)
+        agent_doc.is_active = True
+        agent_doc.save()
