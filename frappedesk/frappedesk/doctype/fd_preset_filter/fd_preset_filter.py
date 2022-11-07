f97ea1683a93f22a52fe7ebe3fcfa7272b3cbc1e
kamaljohnson <kamaljohnson12345@gmail.com>
2022-11-07 21:18:44 +0530

    feat: add validators for fd preset filters

--- frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
+++ frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
@@ -1,9 +1,15 @@
 # Copyright (c) 2022, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FDPresetFilter(Document):
-	pass
+	def validate(self):
+		if self.type == "System":
+			frappe.throw("Please set Type as Global or User instead of System")
+
+	def on_trash(self):
+		if self.type == "System":
+			frappe.throw("System filters cannot be deleted")
