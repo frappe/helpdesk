1ee919199af03a9018c39b963fc92790f8d98c99
kamaljohnson <kamaljohnson12345@gmail.com>
2022-11-09 11:05:30 +0530

    feat: add system preset filters
    filters added:
    - My Open Tickets
    - My Replied Tickets
    - My Resolved Tickets
    - My Closed Tickets

--- frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
+++ frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
@@ -6,10 +6,6 @@ from frappe.model.document import Document
 
 
 class FDPresetFilter(Document):
-	def validate(self):
-		if self.type == "System":
-			frappe.throw("Please set Type as Global or User instead of System")
-
 	def on_trash(self):
 		if self.type == "System":
 			frappe.throw("System filters cannot be deleted")
