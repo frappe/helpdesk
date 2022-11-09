730e5a4f65e0304cf426aaa93a832d0bc12d637a
kamaljohnson <kamaljohnson12345@gmail.com>
2022-11-09 16:15:16 +0530

    fix: user preset filters, not showing in preset dropdown issue

--- frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
+++ frappedesk/frappedesk/doctype/fd_preset_filter/fd_preset_filter.py
@@ -6,6 +6,10 @@ from frappe.model.document import Document
 
 
 class FDPresetFilter(Document):
+	def before_save(self):
+		if self.type == "User":
+			self.user = frappe.session.user
+
 	def on_trash(self):
 		if self.type == "System":
 			frappe.throw("System filters cannot be deleted")
