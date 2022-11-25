4d9ed491289dd41bc4737addea23ee8c5abc16bc
kamaljohnson <kamaljohnson12345@gmail.com>
2022-11-25 16:25:38 +0530

    feat: add custom fields view to customer portal

--- desk/src/pages/portal/ticketing/Ticket.vue
+++ desk/src/pages/portal/ticketing/Ticket.vue
@@ -1,5 +1,9 @@
 <template>
-	<div v-if="ticket" class="max-w-4xl mx-auto mt-5">
+	<div
+		v-if="ticket"
+		class="mx-auto mt-5 max-w-4xl"
+		:class="{ 'max-w-5xl': customFields?.length > 0 }"
+	>
 		<div
 			:style="{
 				height: viewportWidth > 768 ? 'calc(100vh - 14rem)' : null,
@@ -29,248 +33,278 @@
 					{{ ticket.subject }}
 				</div>
 			</div>
-			<div class="grow flex flex-col h-full space-y-2">
-				<div class="overflow-auto grow">
-					<div
-						v-if="['Closed', 'Resolved'].includes(ticket.status)"
-						class="w-full bg-gray-100 rounded-[8px] py-[10px] px-[15px] flex flex-row items-center justify-between mb-[12px]"
-					>
-						<div class="flex flex-row items-center space-x-[9px]">
-							<CustomIcons
-								name="circle-check"
-								class="w-[18px] h-[18px]"
-							/>
-							<div class="text-base">
-								This ticket has been closed.
+			<div class="flex flex-row h-full">
+				<div class="grow flex flex-col h-full space-y-2">
+					<div class="overflow-auto grow">
+						<div
+							v-if="
+								['Closed', 'Resolved'].includes(ticket.status)
+							"
+							class="w-full bg-gray-100 rounded-[8px] py-[10px] px-[15px] flex flex-row items-center justify-between mb-[12px]"
+						>
+							<div
+								class="flex flex-row items-center space-x-[9px]"
+							>
+								<CustomIcons
+									name="circle-check"
+									class="w-[18px] h-[18px]"
+								/>
+								<div class="text-base">
+									This ticket has been closed.
+								</div>
+							</div>
+							<div
+								class="text-[#096CC3] text-[12px] font-medium cursor-pointer hover:text-blue-500"
+								@click="reopenTicket()"
+							>
+								Reopen ticket
 							</div>
 						</div>
-						<div
-							class="text-[#096CC3] text-[12px] font-medium cursor-pointer hover:text-blue-500"
-							@click="reopenTicket()"
-						>
-							Reopen ticket
-						</div>
+						<CustomerSatisfactionFeedback
+							:ticket="ticket"
+							v-if="
+								['Closed', 'Resolved'].includes(ticket.status)
+							"
+						/>
+						<Conversations
+							:ticketId="ticket.name"
+							:scrollToBottom="scrollConversationsToBottom"
+						/>
 					</div>
-					<CustomerSatisfactionFeedback
-						:ticket="ticket"
-						v-if="['Closed', 'Resolved'].includes(ticket.status)"
-					/>
-					<Conversations
-						:ticketId="ticket.name"
-						:scrollToBottom="scrollConversationsToBottom"
-					/>
-				</div>
-				<div
-					v-if="
-						!editing && ['Open', 'Replied'].includes(ticket.status)
-					"
-					class="mt-5 ml-9"
-				>
-					<Button @click="startEditing()" appearance="primary"
-						>Reply</Button
+					<div
+						v-if="
+							!editing &&
+							['Open', 'Replied'].includes(ticket.status)
+						"
+						class="mt-5 ml-9"
 					>
-				</div>
-				<div class="flex flex-col">
-					<div class="grow">
-						<div class="flex">
-							<Avatar
-								v-if="editing"
-								:label="user"
-								:imageURL="user.profile_image"
-								size="md"
-							/>
-							<div class="w-full ml-2 pt-1">
-								<div class="flex flex-col space-y-2">
-									<div v-if="editing" class="text-lg">
-										{{ user.doc.full_name }}
-									</div>
-									<div
-										class="grow"
-										v-if="
-											!['Closed', 'Resolved'].includes(
-												ticket.status
-											)
-										"
-									>
-										<TextEditor
-											v-if="editing"
-											ref="replyEditor"
-											:content="content"
-											editor-class="text-[13px] min-h-[180px] max-h-[300px] max-w-full overflow-y-scroll"
-											v-on:keydown="
-												handleShortcuts($event)
+						<Button @click="startEditing()" appearance="primary"
+							>Reply</Button
+						>
+					</div>
+					<div class="flex flex-col">
+						<div class="grow">
+							<div class="flex">
+								<Avatar
+									v-if="editing"
+									:label="user"
+									:imageURL="user.profile_image"
+									size="md"
+								/>
+								<div class="w-full ml-2 pt-1">
+									<div class="flex flex-col space-y-2">
+										<div v-if="editing" class="text-lg">
+											{{ user.doc.full_name }}
+										</div>
+										<div
+											class="grow"
+											v-if="
+												![
+													'Closed',
+													'Resolved',
+												].includes(ticket.status)
 											"
-											@change="
-												(val) => {
-													content = val
-												}
-											"
-											@click="
-												$refs.replyEditor.editor.commands.focus()
-											"
-											placeholder="Type a response"
-											class="border border-gray-300 rounded-[8px] p-[12px]"
 										>
-											<template #bottom>
-												<div>
-													<div
-														v-if="
-															attachments.length
-														"
-														class="max-h-[100px] overflow-y-scroll rounded flex flex-col"
-													>
-														<ul
-															class="flex flex-wrap gap-2 py-2"
+											<TextEditor
+												v-if="editing"
+												ref="replyEditor"
+												:content="content"
+												editor-class="text-[13px] min-h-[180px] max-h-[300px] max-w-full overflow-y-scroll"
+												v-on:keydown="
+													handleShortcuts($event)
+												"
+												@change="
+													(val) => {
+														content = val
+													}
+												"
+												@click="
+													$refs.replyEditor.editor.commands.focus()
+												"
+												placeholder="Type a response"
+												class="border border-gray-300 rounded-[8px] p-[12px]"
+											>
+												<template #bottom>
+													<div>
+														<div
+															v-if="
+																attachments.length
+															"
+															class="max-h-[100px] overflow-y-scroll rounded flex flex-col"
 														>
-															<li
-																class="flex items-center p-1 space-x-2 bg-gray-100 border-gray-400 border shadow rounded"
-																v-for="file in attachments"
-																:key="file"
-																:title="
-																	file.name
-																"
+															<ul
+																class="flex flex-wrap gap-2 py-2"
 															>
-																<div
-																	class="flex flex-row items-center space-x-1"
-																>
-																	<FeatherIcon
-																		name="file-text"
-																		class="h-[15px] stroke-gray-600"
-																	/>
-																	<span
-																		class="text-[12px] text-gray-700 font-normal ml-2 max-w-[100px] truncate"
-																	>
-																		{{
-																			file.file_name
-																		}}
-																	</span>
-																</div>
-																<button
-																	class="grid w-4 h-4 text-gray-700 rounded hover:bg-gray-300 place-items-center"
-																	@click="
-																		() => {
-																			attachments =
-																				attachments.filter(
-																					(
-																						x
-																					) =>
-																						x.name !=
-																						file.name
-																				)
-																		}
+																<li
+																	class="flex items-center p-1 space-x-2 bg-gray-100 border-gray-400 border shadow rounded"
+																	v-for="file in attachments"
+																	:key="file"
+																	:title="
+																		file.name
 																	"
 																>
-																	<FeatherIcon
-																		class="w-3"
-																		name="x"
-																	/>
-																</button>
-															</li>
-														</ul>
-													</div>
-													<div
-														v-if="
-															showTextFormattingMenu
-														"
-													>
-														<TextEditorFixedMenu
-															class="my-1 overflow-x-auto rounded shadow-sm border"
-															:buttons="
-																textEditorMenuButtons
-															"
-														/>
-													</div>
-													<div
-														class="pt-2 select-none flex flex-row items-center space-x-2"
-														v-if="$refs.replyEditor"
-													>
-														<Button
-															:loading="
-																$resources
-																	.submitConversation
-																	.loading
-															"
-															@click="
-																submitConversation()
-															"
-															appearance="primary"
-														>
-															Send
-														</Button>
-														<div
-															class="flex flex-row items-center space-x-2"
-														>
-															<CustomIcons
-																:class="
-																	showTextFormattingMenu
-																		? 'bg-gray-200'
-																		: ''
-																"
-																name="text-formatting"
-																class="h-7 w-7 rounded p-1"
-																role="button"
-																@click="
-																	() => {
-																		showTextFormattingMenu =
-																			!showTextFormattingMenu
-																	}
-																"
-															/>
-															<FileUploader
-																@success="
-																	(file) =>
-																		attachments.push(
-																			file
-																		)
-																"
-															>
-																<template
-																	v-slot="{
-																		progress,
-																		uploading,
-																		openFileSelector,
-																	}"
-																>
-																	<FeatherIcon
-																		name="paperclip"
-																		class="h-[17px]"
+																	<div
+																		class="flex flex-row items-center space-x-1"
+																	>
+																		<FeatherIcon
+																			name="file-text"
+																			class="h-[15px] stroke-gray-600"
+																		/>
+																		<span
+																			class="text-[12px] text-gray-700 font-normal ml-2 max-w-[100px] truncate"
+																		>
+																			{{
+																				file.file_name
+																			}}
+																		</span>
+																	</div>
+																	<button
+																		class="grid w-4 h-4 text-gray-700 rounded hover:bg-gray-300 place-items-center"
 																		@click="
-																			openFileSelector
+																			() => {
+																				attachments =
+																					attachments.filter(
+																						(
+																							x
+																						) =>
+																							x.name !=
+																							file.name
+																					)
+																			}
 																		"
-																		role="button"
-																		:disabled="
-																			uploading
-																		"
-																	/>
-																</template>
-															</FileUploader>
+																	>
+																		<FeatherIcon
+																			class="w-3"
+																			name="x"
+																		/>
+																	</button>
+																</li>
+															</ul>
 														</div>
 														<div
-															class="grow flex flex-row-reverse"
+															v-if="
+																showTextFormattingMenu
+															"
 														>
-															<FeatherIcon
-																name="trash-2"
-																role="button"
-																class="h-4 w-4"
-																@click="
-																	() => {
-																		content =
-																			''
-																		attachments =
-																			[]
-																		editing = false
-																	}
+															<TextEditorFixedMenu
+																class="my-1 overflow-x-auto rounded shadow-sm border"
+																:buttons="
+																	textEditorMenuButtons
 																"
 															/>
 														</div>
+														<div
+															class="pt-2 select-none flex flex-row items-center space-x-2"
+															v-if="
+																$refs.replyEditor
+															"
+														>
+															<Button
+																:loading="
+																	$resources
+																		.submitConversation
+																		.loading
+																"
+																@click="
+																	submitConversation()
+																"
+																appearance="primary"
+															>
+																Send
+															</Button>
+															<div
+																class="flex flex-row items-center space-x-2"
+															>
+																<CustomIcons
+																	:class="
+																		showTextFormattingMenu
+																			? 'bg-gray-200'
+																			: ''
+																	"
+																	name="text-formatting"
+																	class="h-7 w-7 rounded p-1"
+																	role="button"
+																	@click="
+																		() => {
+																			showTextFormattingMenu =
+																				!showTextFormattingMenu
+																		}
+																	"
+																/>
+																<FileUploader
+																	@success="
+																		(
+																			file
+																		) =>
+																			attachments.push(
+																				file
+																			)
+																	"
+																>
+																	<template
+																		v-slot="{
+																			progress,
+																			uploading,
+																			openFileSelector,
+																		}"
+																	>
+																		<FeatherIcon
+																			name="paperclip"
+																			class="h-[17px]"
+																			@click="
+																				openFileSelector
+																			"
+																			role="button"
+																			:disabled="
+																				uploading
+																			"
+																		/>
+																	</template>
+																</FileUploader>
+															</div>
+															<div
+																class="grow flex flex-row-reverse"
+															>
+																<FeatherIcon
+																	name="trash-2"
+																	role="button"
+																	class="h-4 w-4"
+																	@click="
+																		() => {
+																			content =
+																				''
+																			attachments =
+																				[]
+																			editing = false
+																		}
+																	"
+																/>
+															</div>
+														</div>
 													</div>
-												</div>
-											</template>
-										</TextEditor>
+												</template>
+											</TextEditor>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
+					</div>
+				</div>
+				<div
+					v-if="customFields?.length > 0"
+					class="ml-5 flex flex-col space-y-[12px] w-[200px]"
+				>
+					<!-- Show all the ticket custom fields that can be viewed by an agent -->
+					<div
+						v-for="customField in customFields"
+						:key="customField.fieldname"
+					>
+						<TicketField
+							:ticketId="ticket.name"
+							:fieldname="customField.fieldname"
+							:editable="customField.is_editable_by_agent"
+						/>
 					</div>
 				</div>
 			</div>
@@ -286,6 +320,7 @@ import { FeatherIcon, FileUploader, TextEditor, Avatar } from "frappe-ui"
 import { TextEditorFixedMenu } from "frappe-ui/src/components/TextEditor"
 import CustomIcons from "@/components/desk/global/CustomIcons.vue"
 import CustomerSatisfactionFeedback from "@/components/portal/ticket/CustomerSatisfactionFeedback.vue"
+import TicketField from "@/components/global/TicketField.vue"
 
 export default {
 	name: "Tickets",
@@ -300,6 +335,7 @@ export default {
 		CustomIcons,
 		CustomerSatisfactionFeedback,
 		Avatar,
+		TicketField,
 	},
 	data() {
 		return {
@@ -395,8 +431,21 @@ export default {
 				this.attachments.length == 0
 			)
 		},
+		customFields() {
+			return this.$resources.customFields.data || null
+		},
 	},
 	resources: {
+		customFields() {
+			return {
+				method: "frappedesk.api.ticket.get_custom_fields",
+				params: {
+					doctype: "Ticket",
+					view: "Customer Portal",
+				},
+				auto: true,
+			}
+		},
 		submitConversation() {
 			return {
 				method: "frappedesk.api.ticket.submit_conversation_via_contact",
