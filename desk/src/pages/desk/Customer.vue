fabf7c552dc51b823ad9be9bef8affd89512435b
fadilsid <fadil@tridz.com>
2022-12-02 09:47:24 +0000

    feat: breadcrumbs in customer info page

--- desk/src/pages/desk/Customer.vue
+++ desk/src/pages/desk/Customer.vue
@@ -2,7 +2,23 @@
 	<div class="flex flex-col">
 		<div class="flex border-b h-[52px] px-[24px] shrink-0">
 			<div class="grow my-auto text-[16px] font-semibold text-gray-900">
-				Customers
+				<BaseBreadcrumbs
+					:breadcrumbs="[
+						{
+							label: 'Customers',
+							name: 'customer',
+							handler: () => {
+								$router.push({
+									path: `/frappedesk/customers`,
+								})
+							},
+						},
+						{
+							label: `${customerId}`,
+							name: 'customerId',
+						},
+					]"
+				/>
 			</div>
 		</div>
 		<div class="customer">
@@ -16,11 +32,13 @@
 
 <script>
 import CustomerInfo from "@/components/desk/customers/CustomerInfo.vue"
+import BaseBreadcrumbs from "@/components/global/BaseBreadcrumbs.vue"
 export default {
 	name: "Customer",
 	props: ["customerId"],
 	components: {
 		CustomerInfo,
+		BaseBreadcrumbs,
 	},
 }
 </script>
