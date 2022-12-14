f6693bf9bf1eac9988789013f8cb6c746a284936
kamaljohnson <kamaljohnson12345@gmail.com>
2022-12-14 10:10:21 +0530

    fix: fieldname not shown issue in custom fields

--- frappedesk/frappedesk/doctype/ticket_custom_fields_config/ticket_custom_fields_config.py
+++ frappedesk/frappedesk/doctype/ticket_custom_fields_config/ticket_custom_fields_config.py
@@ -10,7 +10,12 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 class TicketCustomFieldsConfig(Document):
 	def get_field_info(self, fieldname):
 		custom_field_item = next(
-			(field for field in self.custom_fields if field.fieldname == fieldname), None
+			(
+				field
+				for field in self.custom_fields
+				if field.custom_field.split("-")[1] == fieldname
+			),
+			None,
 		)
 		if custom_field_item:
 			return custom_field_item.as_dict()
