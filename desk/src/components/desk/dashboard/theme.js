efec9c203b8891d9f6e757e44df1db3d952d79db
esafwan <esafwan@gmail.com>
2022-12-17 21:43:31 +0000

    Added initial theme options, colors.

--- desk/src/components/desk/dashboard/theme.js
+++ desk/src/components/desk/dashboard/theme.js
@@ -1,4 +1,4 @@
-export default {
+export default {  
     color: ['#2D95F0', '#5FD8C4', '#EEA4EF', ], // custom colors
     backgroundColor: '#ffffff', // custom background color
     // other theme options here
