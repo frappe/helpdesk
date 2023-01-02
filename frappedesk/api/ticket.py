81f225ebb85bf738dcc8ad646667e1c9f37845ee
fadilsid <fadil@tridz.com>
2023-01-02 10:22:19 +0000

    fix: merge conflict in ticket api

--- frappedesk/api/ticket.py
+++ frappedesk/api/ticket.py
@@ -215,75 +215,7 @@ def get_ticket(ticket_id):
 
 @frappe.whitelist()
 def create_new(values, template="Default", attachments=[], via_customer_portal=False):
-<<<<<<< HEAD
-    ticket_doc = frappe.new_doc("Ticket")
-    ticket_doc.via_customer_portal = via_customer_portal
 
-    if "contact" in values:
-        contact_doc = frappe.get_doc("Contact", values["contact"])
-        email_id = ""
-        if contact_doc.email_id:
-            email_id = contact_doc.email_id
-        elif contact_doc.email_ids and len(contact_doc.email_ids) > 0:
-            email_id = contact_doc.email_ids[0].email_id
-
-        ticket_doc.raised_by = email_id
-        ticket_doc.contact = contact_doc.name
-
-    if via_customer_portal:
-        if not frappe.db.exists(
-            {"doctype": "Contact", "email_id": frappe.session.user}
-        ):
-            user_doc = frappe.get_doc("User", frappe.session.user)
-            new_contact_doc = frappe.get_doc(
-                doctype="Contact",
-                email_id=user_doc.email,
-                full_name=user_doc.full_name,
-                first_name=user_doc.first_name,
-                last_name=user_doc.last_name,
-                user=user_doc.name,
-            )
-            new_contact_doc.append(
-                "email_ids", {"email_id": user_doc.email, "is_primary": True}
-            )
-            new_contact_doc.insert(ignore_permissions=True)
-            ticket_doc.contact = new_contact_doc.name
-
-    ticket_doc.subject = values["subject"]
-    ticket_doc.description = values["description"]
-
-    ticket_doc.template = template
-    template_fields = frappe.get_doc("Ticket Template", template).fields
-    for field in template_fields:
-        if field.fieldname in ["subject", "description"]:
-            continue
-        if field.auto_set and field.auto_set_via == "Backend (Python)":
-            continue
-        else:
-            if field.fieldname not in values:
-                continue
-            ticket_doc.append(
-                "custom_fields",
-                {
-                    "label": field.label,
-                    "fieldname": field.fieldname,
-                    "value": values[field.fieldname],
-                    "route": f"/app/{cleanup_page_name(field.options)}/{values[field.fieldname]}"
-                    if field.fieldtype == "Link"
-                    else "",
-                    "is_action_field": field.is_action_field,
-                },
-            )
-
-    ticket_doc.insert(ignore_permissions=True)
-    # TODO: remove this if condition after refactoring doctype/ticket.py logic regarding this
-    create_communication_via_contact(
-        ticket_doc.name, ticket_doc.description, attachments
-    )
-    # if not via_customer_portal:
-
-    return ticket_doc
-=======
 	ticket_doc = frappe.new_doc("Ticket")
 	ticket_doc.via_customer_portal = via_customer_portal
 
@@ -346,7 +278,7 @@ def create_new(values, template="Default", attachments=[], via_customer_portal=F
 	# if not via_customer_portal:
 
 	return ticket_doc
->>>>>>> main
+
 
 
 @frappe.whitelist()
