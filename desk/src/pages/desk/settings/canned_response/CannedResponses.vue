1a437d0bcf198ac11a00124ef805ce50a287a2f9
fadilsid <fadil@tridz.com>
2023-01-18 17:44:36 +0000

    refactor: canned response list view

--- desk/src/pages/desk/settings/canned_response/CannedResponses.vue
+++ desk/src/pages/desk/settings/canned_response/CannedResponses.vue
@@ -1,31 +1,80 @@
 <template>
-	<div class="mt-[9px]">
+	<div class="flex flex-col px-4">
 		<ListManager
 			ref="listManager"
-			class="px-[16px]"
 			:options="{
 				doctype: 'Canned Response',
+				urlQueryFilters: true,
+				saveFiltersLocally: true,
 				fields: ['title', 'owner'],
 				limit: 20,
+				order_by: 'modified desc',
 			}"
-			@selection="
-				(selectedItems) => {
-					if (Object.keys(selectedItems).length > 0) {
-						$event.emit(
-							'show-top-panel-actions-settings',
-							'Canned Responses Bulk'
-						)
-					} else {
-						$event.emit(
-							'show-top-panel-actions-settings',
-							'CannedResponses'
-						)
-					}
-				}
-			"
 		>
 			<template #body="{ manager }">
-				<CannedResponseList :manager="manager" />
+				<ListViewer
+					:options="{
+						name: 'Response',
+						base: '12',
+						listTitle: 'Canned Responses',
+						filterBox: true,
+						presetFilters: true,
+						fields: {
+							title: {
+								label: 'Title',
+								width: '4',
+							},
+							owner: {
+								label: 'Owner',
+								width: '2',
+							},
+						},
+					}"
+					class="text-base h-[calc(100vh-9.5rem)] pt-4"
+					@add-item="
+						() => {
+							showNewCannedResponsesDialog = true
+						}
+					"
+				>
+					<template #field-title="{ row }">
+						<router-link
+							:to="{
+								path: `/frappedesk/settings/canned_responses/${row.title}`,
+							}"
+							class="text-[13px] text-gray-600 font-inter hover:text-gray-900"
+						>
+							{{ `${row.title}` }}
+						</router-link>
+					</template>
+					<template #field-owner="{ row }">
+						<div class="text-[13px] font-inter text-gray-600">
+							{{ row.owner }}
+						</div>
+					</template>
+					<template #bulk-actions="{ selectedItems }">
+						<div class="flex flex-row space-x-2">
+							<Button
+								@click="
+									() => {
+										$resources.deleteTeam
+											.submit({
+												doctype: 'Canned Response',
+												name: Object.keys(
+													selectedItems
+												),
+											})
+											.then(() => {
+												manager.unselect()
+												manager.reload()
+											})
+									}
+								"
+								>Delete</Button
+							>
+						</div>
+					</template>
+				</ListViewer>
 			</template>
 		</ListManager>
 		<AddNewCannedResponsesDialog
@@ -42,15 +91,15 @@
 </template>
 <script>
 import { inject, ref } from "vue"
-import CannedResponseList from "@/components/desk/settings/canned_responses/CannedResponseList.vue"
 import ListManager from "@/components/global/ListManager.vue"
 import AddNewCannedResponsesDialog from "@/components/desk/global/AddNewCannedResponsesDialog.vue"
+import ListViewer from "@/components/global/ListViewer.vue"
 export default {
 	name: "Canned Responses",
 	components: {
-		CannedResponseList,
 		ListManager,
 		AddNewCannedResponsesDialog,
+		ListViewer,
 	},
 	setup() {
 		const viewportWidth = inject("viewportWidth")
@@ -60,26 +109,13 @@ export default {
 			showNewCannedResponsesDialog,
 		}
 	},
-	mounted() {
-		this.$event.emit("set-selected-setting", "Canned Responses")
-		this.$event.emit("show-top-panel-actions-settings", "CannedResponses")
-		this.$event.on("show-new-canned_response-dialog", () => {
-			this.showNewCannedResponsesDialog = true
-		})
-		this.$event.on("delete-selected-canned_responses", () => {
-			this.$resources.bulk_delete_responses.submit({
-				items: Object.keys(
-					this.$refs.listManager.manager.selectedItems
-				),
-				doctype: "Canned Response",
-			})
-		})
-	},
-	unmounted() {
-		this.$event.off("show-new-canned_response-dialog")
-		this.$event.off("delete-selected-canned_responses")
-	},
+
 	resources: {
+		deleteTeam() {
+			return {
+				method: "frappe.client.delete",
+			}
+		},
 		bulk_delete_responses() {
 			return {
 				method: "frappedesk.api.doc.delete_items",
@@ -94,10 +130,6 @@ export default {
 						customIcon: "circle-check",
 						appearance: "success",
 					})
-					this.$event.emit(
-						"show-top-panel-actions-settings",
-						"CannedResponses"
-					)
 				},
 			}
 		},
