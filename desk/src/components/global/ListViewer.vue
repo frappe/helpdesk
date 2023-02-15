9fb33f563c70791a9348ebcd15dd3a560d0ab783
Sabu Siyad <hello@ssiyad.com>
2023-02-15 23:11:07 +0530

    refactor(list-viewer): add `thead` and `tbody`

--- desk/src/components/global/ListViewer.vue
+++ desk/src/components/global/ListViewer.vue
@@ -84,65 +84,68 @@
 					</slot>
 					<div class="grow h-full">
 						<table class="w-full table-auto border-separate border-spacing-y-2">
-							<tr class="bg-gray-100 text-base text-gray-500 select-none">
-								<th class="p-3 rounded-tl-md rounded-bl-md">
-									<div>
+							<thead>
+								<tr class="bg-gray-100 text-base text-gray-500 select-none">
+									<th class="p-3 rounded-tl-md rounded-bl-md">
+										<div class="">
+											<Input
+												type="checkbox"
+												@click="manager.selectAll"
+												:checked="manager.allItemsSelected"
+												class="cursor-pointer" />
+										</div>
+									</th>
+									<th
+										class="text-start last-of-type:rounded-tr-md last-of-type:rounded-br-md 
+										font-normal hover:text-gray-600 cursor-pointer fill-gray-300"
+										v-for="field in Object.keys(renderOptions.fields)"
+										:key="field"
+										@click="manager.toggleOrderBy(field)"
+									>
+										{{ renderOptions.fields[field].label }}
+										<div class="inline-table">
+											<CustomIcons
+												v-show="sortArrowShow(field)"
+												:name="sortArrowName()"
+												class="h-1 fill-gray-400 stroke-transparent"
+											/>
+										</div>
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr
+									v-for="item in manager.list"
+									class="hover:bg-gray-50"
+								>
+									<td class="p-3 rounded-tl-md rounded-bl-md">
 										<Input
 											type="checkbox"
-											@click="manager.selectAll"
-											:checked="manager.allItemsSelected"
-											class="cursor-pointer" />
-									</div>
-								</th>
-								<th
-									class="text-start last-of-type:rounded-tr-md last-of-type:rounded-br-md 
-									font-normal hover:text-gray-600 cursor-pointer fill-gray-300"
-									v-for="field in Object.keys(renderOptions.fields)"
-									:key="field"
-									@click="manager.toggleOrderBy(field)"
-								>
-									{{ renderOptions.fields[field].label }}
-									<div class="inline-table">
-										<CustomIcons
-											v-show="sortArrowShow(field)"
-											:name="sortArrowName()"
-											class="h-1 fill-gray-400 stroke-transparent"
+											@click="manager.select(item)"
+											:checked="manager.itemSelected(item)"
+											class="cursor-pointer"
 										/>
-									</div>
-								</th>
-							</tr>
-
-							<tr
-								v-for="item in manager.list"
-								class="hover:bg-gray-50"
-							>
-								<td class="p-3 rounded-tl-md rounded-bl-md">
-									<Input
-										type="checkbox"
-										@click="manager.select(item)"
-										:checked="manager.itemSelected(item)"
-										class="cursor-pointer"
-									/>
-								</td>
-								<td
-									v-for="field in Object.keys(renderOptions.fields)"
-									:key="field"
-									class="py-2 last-of-type:rounded-tr-md last-of-type:rounded-br-md last-of-type:pr-3"
-								>
-									<slot
-										:name="
-											'field-' + field
-										"
-										:field="field"
-										:value="item[field]"
-										:row="item"
+									</td>
+									<td
+										v-for="field in Object.keys(renderOptions.fields)"
+										:key="field"
+										class="py-2 last-of-type:rounded-tr-md last-of-type:rounded-br-md last-of-type:pr-3"
 									>
-										{{
-											item[field]
-										}}
-									</slot>
-								</td>
-							</tr>
+										<slot
+											:name="
+												'field-' + field
+											"
+											:field="field"
+											:value="item[field]"
+											:row="item"
+										>
+											{{
+												item[field]
+											}}
+										</slot>
+									</td>
+								</tr>
+							</tbody>
 						</table>
 					</div>
 					<div>
