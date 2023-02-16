8935cb48da0c1e9ad528976691f610643ff33595
Sabu Siyad <hello@ssiyad.com>
2023-02-16 23:50:36 +0530

    feat: install eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
 		es2021: true,
 		node: true,
 	},
-	extends: ["eslint:recommended", "plugin:json/recommended", "prettier"],
+	extends: [
+		"eslint:recommended",
+		"plugin:json/recommended",
+		"plugin:vue/vue3-recommended",
+		"prettier",
+	],
 	parserOptions: {
 		ecmaVersion: "latest",
 		sourceType: "module",
