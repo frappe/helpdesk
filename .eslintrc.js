10150bd6e01f4c52dedbe1dbc090003e7232cd16
Sabu Siyad <hello@ssiyad.com>
2023-02-16 23:59:48 +0530

    feat: install eslint-plugin-tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
 		"eslint:recommended",
 		"plugin:json/recommended",
 		"plugin:vue/vue3-recommended",
+		"plugin:tailwindcss/recommended",
 		"prettier",
 	],
 	parserOptions: {
