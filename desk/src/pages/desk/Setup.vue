385e09e1a2bcb42da5e9863918fc2f859b9fc6db
Sabu Siyad <hello@ssiyad.com>
2023-03-09 00:49:18 +0530

    chore: upgrade `frappe-ui`

--- desk/src/pages/desk/Setup.vue
+++ desk/src/pages/desk/Setup.vue
@@ -1,8 +1,8 @@
 <template>
-	<div class="bg-[#F3F5F8] w-screen" style="min-height: 100vh">
-		<div class="lg:p-[80px] md:p-[0px]">
+	<div class="w-screen bg-[#F3F5F8]" style="min-height: 100vh">
+		<div class="md:p-[0px] lg:p-[80px]">
 			<div
-				class="bg-white lg:rounded-[10px] sm:rounded-none sm:h-[100hv]"
+				class="bg-white sm:h-[100hv] sm:rounded-none lg:rounded-[10px]"
 				style="
 					 {
 						box-shadow: 0px 1px 42px rgba(97, 97, 97, 0.07);
@@ -10,49 +10,35 @@
 					}
 				"
 			>
-				<div
-					class="py-[60px] lg:px-[0px] sm:px-[10px] flex justify-center"
-				>
-					<div class="flex flex-col space-y-[60px] w-[370px]">
+				<div class="flex justify-center py-[60px] sm:px-[10px] lg:px-[0px]">
+					<div class="flex w-[370px] flex-col space-y-[60px]">
 						<div class="flex justify-center">
 							<CustomIcons name="frappedesk" class="h-[24px]" />
 						</div>
 						<div>
-							<form
-								v-if="currentStep == 1"
-								@submit.prevent="submitStep"
-							>
-								<div
-									class="text-[24px] font-bold mb-[10px] text-gray-900"
-								>
+							<form v-if="currentStep == 1" @submit.prevent="submitStep">
+								<div class="mb-[10px] text-[24px] font-bold text-gray-900">
 									Welcome to FrappeDesk
 								</div>
-								<div
-									class="text-[16px] font-normal text-gray-600 mb-[30px]"
-								>
+								<div class="mb-[30px] text-[16px] font-normal text-gray-600">
 									Thanks for verifying your e-mail.
 								</div>
-								<div
-									class="text-[16px] font-normal text-gray-900 mb-[30px]"
-								>
-									Configure your e-mail address to start
-									sending and receiving e-mails into
-									FrappeDesk.
+								<div class="mb-[30px] text-[16px] font-normal text-gray-900">
+									Configure your e-mail address to start sending and receiving
+									e-mails into FrappeDesk.
 								</div>
 								<div class="mb-[30px]">
-									<span
-										class="block mb-2 text-sm leading-4 text-gray-700"
-									>
+									<span class="mb-2 block text-sm leading-4 text-gray-700">
 										Service
 									</span>
 									<div
-										class="flex flex-wrap text-[11px] text-gray-700 mx-[-4px]"
+										class="mx-[-4px] flex flex-wrap text-[11px] text-gray-700"
 									>
 										<div
 											v-for="service in services"
 											:key="service.title"
 											role="button"
-											class="h-[80px] w-[80px] m-1 p-1.5 items-center border hover:shadow-sm rounded flex flex-col space-y-2"
+											class="m-1 flex h-[80px] w-[80px] flex-col items-center space-y-2 rounded border p-1.5 hover:shadow-sm"
 											:class="
 												service.selected
 													? 'border-2 border-blue-500 bg-blue-50'
@@ -60,169 +46,136 @@
 											"
 											@click="selectService(service)"
 										>
-											<Images
-												:name="service.image"
-												class="h-10 w-10"
-											/>
+											<Images :name="service.image" class="h-10 w-10" />
 											<div>{{ service.title }}</div>
 										</div>
 									</div>
 								</div>
 								<div
 									v-if="inputValues['service'] == 'GMail'"
-									class="mb-[30px] bg-blue-50 border-blue-500 rounded p-3 border-2 text-base text-gray-700 flex flex-row space-x-4 items-center"
+									class="mb-[30px] flex flex-row items-center space-x-4 rounded border-2 border-blue-500 bg-blue-50 p-3 text-base text-gray-700"
 								>
 									<FeatherIcon
 										name="info"
-										class="shrink-0 w-6 h-6 stroke-blue-500 stroke-2"
+										class="h-6 w-6 shrink-0 stroke-blue-500 stroke-2"
 									/>
 									<p>
-										GMail will only work if you enable
-										2-step authentication and use
-										app-specific password.
+										GMail will only work if you enable 2-step authentication and
+										use app-specific password.
 										<a
 											href="https://docs.erpnext.com/docs/v13/user/manual/en/setting-up/email/email_account_setup_with_gmail"
 											target="_blank"
 											class="text-blue-500 hover:underline"
-											>Read the step by step guide
-											here.</a
+											>Read the step by step guide here.</a
 										>
 									</p>
 								</div>
-								<label
-									class="flex flex-col space-y-[10px] mb-[30px]"
-								>
-									<span
-										class="block mb-2 text-sm leading-4 text-gray-700"
-									>
+								<label class="mb-[30px] flex flex-col space-y-[10px]">
+									<span class="mb-2 block text-sm leading-4 text-gray-700">
 										E-mail ID
 									</span>
 									<div>
 										<input
-											type="text"
-											class="rounded-[6px] w-full border-[#EBEEF0] h-[36px]"
 											v-model="inputValues['email']"
+											type="text"
+											class="h-[36px] w-full rounded-[6px] border-[#EBEEF0]"
 										/>
 										<span
-											class="text-red-500 text-base font-normal"
 											v-if="errors['email']"
+											class="text-base font-normal text-red-500"
 											>{{ errors["email"] }}</span
 										>
 									</div>
 								</label>
-								<label
-									class="flex flex-col space-y-[10px] mb-[30px]"
-								>
-									<span
-										class="block mb-2 text-sm leading-4 text-gray-700"
-									>
+								<label class="mb-[30px] flex flex-col space-y-[10px]">
+									<span class="mb-2 block text-sm leading-4 text-gray-700">
 										Password
 									</span>
 									<div>
 										<input
-											type="password"
-											class="rounded-[6px] w-full border-[#EBEEF0] h-[36px]"
 											v-model="inputValues['password']"
+											type="password"
+											class="h-[36px] w-full rounded-[6px] border-[#EBEEF0]"
 										/>
 										<span
-											class="text-red-500 text-base font-normal"
 											v-if="errors['password']"
+											class="text-base font-normal text-red-500"
 											>{{ errors["password"] }}</span
 										>
 									</div>
 								</label>
-								<Button
-									appearance="primary"
-									class="w-full mb-[14px]"
+								<Button appearance="primary" class="mb-[14px] w-full"
 									>Next</Button
 								>
 								<div class="flex justify-center pl-[15px]">
 									<div
-										class="flex flex-row items-center space-x-3 text-base font-normal text-gray-600 text-center hover:text-gray-700"
+										class="flex flex-row items-center space-x-3 text-center text-base font-normal text-gray-600 hover:text-gray-700"
 										:class="
 											$resources.completeSetup.loading
 												? 'text-gray-500 hover:text-gray-500 cursor-wait'
 												: 'cursor-pointer'
 										"
-										@click="
-											$resources.completeSetup.submit()
-										"
+										@click="$resources.completeSetup.submit()"
 									>
 										<span> Skip Onboarding </span>
 										<div class="h-[15px]">
-											<Spinner
-												v-if="
-													$resources.completeSetup
-														.loading
-												"
+											<LoadingIndicator
+												v-if="$resources.completeSetup.loading"
 											/>
 										</div>
 									</div>
 								</div>
 							</form>
-							<form
-								v-if="currentStep == 2"
-								@submit.prevent="submitStep"
-							>
-								<div
-									class="text-[24px] font-bold mb-[30px] text-gray-900"
-								>
+							<form v-if="currentStep == 2" @submit.prevent="submitStep">
+								<div class="mb-[30px] text-[24px] font-bold text-gray-900">
 									Let’s invite your teammates
 								</div>
 								<div
 									v-if="emailAccountCreationSkipped"
-									class="mb-[30px] bg-yellow-50 border-yellow-300 rounded p-3 border-2 text-base text-gray-700 flex flex-row space-x-4 items-center"
+									class="mb-[30px] flex flex-row items-center space-x-4 rounded border-2 border-yellow-300 bg-yellow-50 p-3 text-base text-gray-700"
 								>
 									<FeatherIcon
 										name="alert-triangle"
-										class="shrink-0 w-6 h-6 stroke-yellow-500 stroke-2"
+										class="h-6 w-6 shrink-0 stroke-yellow-500 stroke-2"
 									/>
 									<p>
-										Invitation emails will only be sent once
-										email account is setup up,
+										Invitation emails will only be sent once email account is
+										setup up,
 										<span
 											class="text-blue-500 hover:underline"
-											@click="goBack"
 											role="button"
+											@click="goBack"
 											>setup up now</span
 										>
 									</p>
 								</div>
-								<label
-									class="flex flex-col space-y-[10px] mb-[30px]"
-								>
-									<span
-										class="block mb-2 text-sm leading-4 text-gray-700"
-									>
+								<label class="mb-[30px] flex flex-col space-y-[10px]">
+									<span class="mb-2 block text-sm leading-4 text-gray-700">
 										E-mail IDs (comma-separated)
 									</span>
 									<div>
 										<textarea
+											v-model="inputValues['agentEmailsStr']"
 											rows="3"
-											class="max-h-[130px] min-h-[80px] placeholder-gray-400 text-[16px] font-normal rounded-[6px] w-full border-[#EBEEF0]"
+											class="max-h-[130px] min-h-[80px] w-full rounded-[6px] border-[#EBEEF0] text-[16px] font-normal placeholder:text-gray-400"
 											placeholder="tom@frappe.io, alex@frappe.io, joe@frappe.io"
-											v-model="
-												inputValues['agentEmailsStr']
-											"
 										/>
 										<span
-											class="text-red-500 text-base font-normal"
 											v-if="errors['agentEmailsStr']"
-											>{{
-												errors["agentEmailsStr"]
-											}}</span
+											class="text-base font-normal text-red-500"
+											>{{ errors["agentEmailsStr"] }}</span
 										>
 									</div>
 								</label>
 								<Button
 									:loading="submitInProgress"
 									appearance="primary"
-									class="w-full mb-[14px]"
+									class="mb-[14px] w-full"
 									>Finish</Button
 								>
-								<div class="flex justify-center mb-[30px]">
+								<div class="mb-[30px] flex justify-center">
 									<div
-										class="text-base font-normal text-gray-600 text-center hover:text-gray-700"
+										class="text-center text-base font-normal text-gray-600 hover:text-gray-700"
 										role="button"
 										@click="skip"
 									>
@@ -245,17 +198,11 @@
 	</div>
 </template>
 
-<style scoped>
-.form-input {
-	background: white !important;
-}
-</style>
-
 <script>
-import CustomIcons from "@/components/desk/global/CustomIcons.vue"
-import Images from "@/components/global/Images.vue"
-import { FeatherIcon, Spinner } from "frappe-ui"
-import { ref } from "vue"
+import CustomIcons from "@/components/desk/global/CustomIcons.vue";
+import Images from "@/components/global/Images.vue";
+import { FeatherIcon, LoadingIndicator } from "frappe-ui";
+import { ref } from "vue";
 
 export default {
 	name: "DeskSetup",
@@ -263,15 +210,15 @@ export default {
 		CustomIcons,
 		FeatherIcon,
 		Images,
-		Spinner,
+		LoadingIndicator,
 	},
 	setup() {
-		const currentStep = ref(1)
-		const totalSteps = ref(2)
+		const currentStep = ref(1);
+		const totalSteps = ref(2);
 
-		const submitInProgress = ref(false)
+		const submitInProgress = ref(false);
 
-		const emailAccountCreationSkipped = ref(true)
+		const emailAccountCreationSkipped = ref(true);
 
 		const emailDefaults = {
 			GMail: {
@@ -298,7 +245,7 @@ export default {
 			Others: {
 				use_ssl: 1,
 			},
-		}
+		};
 
 		const services = ref([
 			{
@@ -321,7 +268,7 @@ export default {
 				title: "Others",
 				name: "Others",
 			},
-		])
+		]);
 
 		const inputValues = ref({
 			service: services.value[0].name,
@@ -329,9 +276,9 @@ export default {
 			password: "",
 			agentEmailsStr: "",
 			agentEmailList: [],
-		})
+		});
 
-		const errors = ref({})
+		const errors = ref({});
 
 		return {
 			emailDefaults,
@@ -342,38 +289,38 @@ export default {
 			services,
 			inputValues,
 			errors,
-		}
+		};
 	},
 	mounted() {
 		this.$event.on("email-account-created", () => {
-			console.log("email account created successfully!!")
+			console.log("email account created successfully!!");
 
 			if (this.inputValues.agentEmailList.length > 0) {
 				this.$resources.sentInvites.submit({
 					emails: this.inputValues.agentEmailList,
 					send_welcome_mail_to_user: true,
-				})
+				});
 			}
 
-			this.$resources.completeSetup.submit()
-		})
+			this.$resources.completeSetup.submit();
+		});
 
 		this.$event.on("email-account-creation-failed", (error) => {
-			console.log("email account creation failed!!", error)
+			console.log("email account creation failed!!", error);
 
 			this.$toast({
 				title: "Email account creation failed",
 				text: error,
 				customIcon: "circle-fail",
 				appearance: "danger",
-			})
+			});
 
-			this.submitInProgress = false
-		})
+			this.submitInProgress = false;
+		});
 	},
 	methods: {
 		createEmailAccount() {
-			this.submitInProgress = true
+			this.submitInProgress = true;
 			this.$resources.createEmailAccount.submit({
 				doc: {
 					doctype: "Email Account",
@@ -403,18 +350,18 @@ export default {
 					smtp_port: 587,
 					...this.emailDefaults[this.inputValues.service],
 				},
-			})
+			});
 		},
 		submitStep() {
 			if (this.validateInputs(this.currentStep)) {
 				if (this.currentStep == 1) {
-					this.emailAccountCreationSkipped = false
+					this.emailAccountCreationSkipped = false;
 				}
 				if (this.currentStep < this.totalSteps) {
-					this.currentStep++
+					this.currentStep++;
 				} else {
 					if (!this.emailAccountCreationSkipped) {
-						this.createEmailAccount()
+						this.createEmailAccount();
 					} else {
 						if (this.inputValues.agentEmailList.length > 0) {
 							this.$resources.sentInvites
@@ -423,105 +370,97 @@ export default {
 									send_welcome_mail_to_user: false,
 								})
 								.then(() => {
-									this.$resources.completeSetup.submit()
-								})
+									this.$resources.completeSetup.submit();
+								});
 						}
 					}
 				}
 			} else {
-				console.log("input validation error!!!")
+				console.log("input validation error!!!");
 			}
 		},
 		skip() {
 			if (this.currentStep == 1) {
-				this.emailAccountCreationSkipped = true
+				this.emailAccountCreationSkipped = true;
 			}
 			if (this.currentStep == this.totalSteps) {
 				if (!this.emailAccountCreationSkipped) {
-					this.createEmailAccount()
+					this.createEmailAccount();
 				} else {
-					this.$resources.completeSetup.submit()
+					this.$resources.completeSetup.submit();
 				}
 			}
 			if (this.currentStep < this.totalSteps) {
-				this.currentStep++
+				this.currentStep++;
 			}
 		},
 		goBack() {
 			if (this.currentStep > 1) {
-				this.currentStep--
+				this.currentStep--;
 			}
 		},
 		selectService(service) {
 			this.services.forEach((s) => {
-				s.selected = false
-			})
-			service.selected = true
-			this.inputValues["service"] = service.name
+				s.selected = false;
+			});
+			service.selected = true;
+			this.inputValues["service"] = service.name;
 		},
 		validateInputs(step) {
-			this.errors = {}
+			this.errors = {};
 
 			const testEmailRegex = (val) => {
-				let emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
-				return emailRegex.test(val)
-			}
+				let emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+				return emailRegex.test(val);
+			};
 
 			const textPasswordRegex = (val) => {
-				let paaswordRegex = /^(?=.*\d)(?=.*[a-z]).{6,20}$/
-				return paaswordRegex.test(val)
-			}
+				let paaswordRegex = /^(?=.*\d)(?=.*[a-z]).{6,20}$/;
+				return paaswordRegex.test(val);
+			};
 
 			switch (step) {
 				case 1:
 					// validate email
 					if (!this.inputValues["email"]) {
-						this.errors["email"] = "Email is required"
+						this.errors["email"] = "Email is required";
 					} else if (!testEmailRegex(this.inputValues["email"])) {
-						this.errors["email"] =
-							"Please enter a valid email address"
+						this.errors["email"] = "Please enter a valid email address";
 					}
 					// validate password
 					if (!this.inputValues["password"]) {
-						this.errors["password"] = "Password is required"
-					} else if (
-						!textPasswordRegex(this.inputValues["password"])
-					) {
+						this.errors["password"] = "Password is required";
+					} else if (!textPasswordRegex(this.inputValues["password"])) {
 						this.errors["password"] =
-							"Password must be at least 6 characters long and contain at least one number and one letter"
+							"Password must be at least 6 characters long and contain at least one number and one letter";
 					}
 					// validate service
 
 					if (!this.inputValues["service"]) {
-						this.errors["service"] = "Service is required"
+						this.errors["service"] = "Service is required";
 					}
-					break
+					break;
 				case 2:
 					// validate agent emails
-					this.inputValues["agentEmailList"] = []
-					if (
-						this.inputValues["agentEmailsStr"].replaceAll(
-							" ",
-							""
-						) != ""
-					) {
+					this.inputValues["agentEmailList"] = [];
+					if (this.inputValues["agentEmailsStr"].replaceAll(" ", "") != "") {
 						let agentEmailList = this.inputValues["agentEmailsStr"]
 							.replaceAll(" ", "")
-							.split(",")
+							.split(",");
 						agentEmailList.forEach((email) => {
 							if (!testEmailRegex(email)) {
 								this.errors["agentEmailsStr"] =
-									"Please enter a valid email address"
+									"Please enter a valid email address";
 							}
-						})
-						this.inputValues["agentEmailList"] = agentEmailList
+						});
+						this.inputValues["agentEmailList"] = agentEmailList;
 					} else {
 						this.errors["agentEmailsStr"] =
-							"Please enter at least one email address"
+							"Please enter at least one email address";
 					}
-					break
+					break;
 			}
-			return Object.keys(this.errors).length === 0
+			return Object.keys(this.errors).length === 0;
 		},
 	},
 	resources: {
@@ -529,23 +468,23 @@ export default {
 			return {
 				method: "frappe.client.insert",
 				onSuccess: () => {
-					this.$event.emit("email-account-created")
+					this.$event.emit("email-account-created");
 				},
 				onError: (error) => {
-					this.$event.emit("email-account-creation-failed", error)
+					this.$event.emit("email-account-creation-failed", error);
 				},
-			}
+			};
 		},
 		sentInvites() {
 			return {
 				method: "frappedesk.api.agent.sent_invites",
 				onSuccess: () => {
-					this.$event.emit("sent-invites-success")
+					this.$event.emit("sent-invites-success");
 				},
 				onError: (error) => {
-					this.$event.emit("sent-invites-failed", error)
+					this.$event.emit("sent-invites-failed", error);
 				},
-			}
+			};
 		},
 		completeSetup() {
 			return {
@@ -561,9 +500,9 @@ export default {
 						title: "Setup Complete",
 						customIcon: "circle-check",
 						appearance: "success",
-					})
-					this.submitInProgress = false
-					this.$router.push({ name: "DeskTickets" })
+					});
+					this.submitInProgress = false;
+					this.$router.push({ name: "DeskTickets" });
 				},
 				onError: (error) => {
 					this.$toast({
@@ -571,11 +510,17 @@ export default {
 						text: error,
 						customIcon: "circle-fail",
 						appearance: "danger",
-					})
-					this.submitInProgress = false
+					});
+					this.submitInProgress = false;
 				},
-			}
+			};
 		},
 	},
-}
+};
 </script>
+
+<style scoped>
+.form-input {
+	background: white !important;
+}
+</style>
