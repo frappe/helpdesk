8ba29ae1c6496e0d4ae3e3e6803b73aff433c56b
Sabu Siyad <hello@ssiyad.com>
2023-03-11 22:30:39 +0530

    fix(ui): agent avatar in ticket list

--- desk/src/components/global/AgentAvatar.vue
+++ desk/src/components/global/AgentAvatar.vue
@@ -1,33 +1,43 @@
 <template>
-	<Avatar size="sm" :imageURL="agent.user_image" :label="agent.agent_name" />
+	<Avatar size="sm" :image-u-r-l="image" :label="name" />
 </template>
 
 <script>
-import { Avatar } from "frappe-ui"
-import { inject } from "vue"
+import { Avatar } from "frappe-ui";
 
 export default {
 	name: "AgentAvatar",
-	props: ["agent"],
 	components: {
 		Avatar,
 	},
-	setup() {
-		const agents = inject("agents")
-		return {
-			agents,
-		}
-	},
-	computed: {
-		agent() {
-			const a = this.agents.find((x) => x.name === this.agent);
-
-			// Agent could be missing, corrupt or invalid. It is better to
-			// have a fallback value, which in this case is an empty object.
-			if (!a) return {};
-
-			return a;
+	props: {
+		agent: {
+			type: String,
+			default: "",
 		},
 	},
-}
+	computed: {
+		user() {
+			return this.$resources?.user?.doc || {};
+		},
+		image() {
+			return this.user.user_image;
+		},
+		name() {
+			return this.user.full_name;
+		},
+	},
+	resources: {
+		user() {
+			if (!this.agent) return;
+
+			return {
+				type: "document",
+				doctype: "User",
+				name: this.agent,
+				cache: ["User", this.agent],
+			};
+		},
+	},
+};
 </script>
