602cacdea690b1b35f5a40444761e05db3c2ab90
Sabu Siyad <hello@ssiyad.com>
2023-03-26 00:03:21 +0530

    chore: change `vite` target `es2015` -> `es2021` (#1078)
    - This commit also add a `tsconfig.json` with target `es2021`

--- desk/vite.config.js
+++ desk/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import path from "path"
-import { getProxyOptions } from "frappe-ui/src/utils/vite-dev-server"
-import { webserver_port } from "../../../sites/common_site_config.json"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { getProxyOptions } from "frappe-ui/src/utils/vite-dev-server";
+import { webserver_port } from "../../../sites/common_site_config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +19,9 @@ export default defineConfig({
 	build: {
 		outDir: `../${path.basename(path.resolve(".."))}/public/desk`,
 		emptyOutDir: true,
-		target: "es2015",
+		target: "es2021",
 	},
 	optimizeDeps: {
 		include: ["feather-icons", "showdown"],
 	},
-})
+});
