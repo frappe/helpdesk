6bde4d94bd2689d9b8bea893dc807d020e7e2bbc
Sabu Siyad <hello@ssiyad.com>
2023-03-26 22:30:06 +0530

    refactor: rename doctype: `Category` -> `HD Article Category`

--- frappedesk/frappedesk/doctype/hd_article_category/test_hd_article_category.py
+++ frappedesk/frappedesk/doctype/hd_article_category/test_hd_article_category.py
@@ -1,9 +1,9 @@
-# Copyright (c) 2023, Frappe Technologies and Contributors
+# Copyright (c) 2021, Frappe Technologies and Contributors
 # See license.txt
 
 # import frappe
-from frappe.tests.utils import FrappeTestCase
+import unittest
 
 
-class TestHDArticleCategory(FrappeTestCase):
+class TestHDArticleCategory(unittest.TestCase):
 	pass
