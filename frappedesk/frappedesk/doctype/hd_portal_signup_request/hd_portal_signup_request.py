28fc429847d6d11a2129295a1468325be9571c1a
Sabu Siyad <hello@ssiyad.com>
2023-03-26 22:30:06 +0530

    refactor: rename doctype: `Portal Signup Request` -> `HD Portal Signup Request`

--- frappedesk/frappedesk/doctype/hd_portal_signup_request/hd_portal_signup_request.py
+++ frappedesk/frappedesk/doctype/hd_portal_signup_request/hd_portal_signup_request.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class PortalSignupRequest(Document):
+class HDPortalSignupRequest(Document):
 	pass
--- frappedesk/frappedesk/doctype/hd_portal_signup_request/test_hd_portal_signup_request.py
+++ frappedesk/frappedesk/doctype/hd_portal_signup_request/test_hd_portal_signup_request.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestPortalSignupRequest(FrappeTestCase):
+class TestHDPortalSignupRequest(FrappeTestCase):
 	pass
