58ef489c7d567f8fde75bebef02f946a40911156
Sabu Siyad <hello@ssiyad.com>
2023-03-26 22:30:06 +0530

    refactor: rename doctype: `Category`
    - `Category` -> `HD Article Category`

---
+++ frappedesk/frappedesk/doctype/hd_article_category/hd_article_category.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class HDArticleCategory(NestedSet):
+	pass
