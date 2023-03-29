4e13b9c6995d36de26ec1f503ccbb9520a9e46cd
Sabu Siyad <hello@ssiyad.com>
2023-03-29 17:48:46 +0530

    chore: remove empty patches

--- helpdesk/patches/populate_ticket_activities.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-
-def execute():
-	all_tickets = frappe.get_all("HD Ticket")
-	for ticket in all_tickets:
-		# TODO: use version to set status, type, priority, team changes
-		pass
--- helpdesk/patches/remove_base_route_redirect_and_set_home_page_to_kb.py
+++
@@ -1,7 +0,0 @@
-from helpdesk.setup.install import set_home_page_to_kb
-import frappe
-
-
-def execute():
-	# This patch is removed
-	pass
