c45f0548e08393aab3a6aba570afbceb4ed06d34
Sabu Siyad <hello@ssiyad.com>
2023-04-03 03:55:56 +0530

    chore: patch: ticket naming rule to autoincrement

---
+++ helpdesk/patches/ticket_naming_rule_to_autoincrement.py
@@ -0,0 +1,36 @@
+import frappe
+from frappe.utils import get_table_name
+
+from helpdesk.utils import alphanumeric_to_int
+
+DOCTYPE = "HD Ticket"
+MODULE = "Helpdesk"
+SEQ_SUFFIX = "_id_seq"
+
+
+def execute():
+	create_sequence()
+	modify_table()
+
+
+def modify_table():
+	table = f"`{get_table_name(DOCTYPE)}`"
+	frappe.db.sql_ddl(f"ALTER TABLE {table} MODIFY COLUMN name BIGINT(20)")
+	frappe.reload_doc(MODULE, "DocType", DOCTYPE)
+
+
+def create_sequence():
+	sequence_name = frappe.scrub(DOCTYPE + SEQ_SUFFIX)
+	frappe.db.sql_ddl(f"DROP SEQUENCE IF EXISTS {sequence_name}")
+	start_value = sequence_start()
+	frappe.db.create_sequence(DOCTYPE, check_not_exists=False, start_value=start_value)
+
+
+def sequence_start():
+	last_doc = frappe.get_last_doc(DOCTYPE)
+
+	if not last_doc:
+		return 1
+
+	last_id = alphanumeric_to_int(last_doc.name) or 0
+	return last_id + 1
--- helpdesk/utils.py
+++ helpdesk/utils.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import frappe
+import re
 
 
 def extract_mentions(html):
@@ -10,3 +11,19 @@ def extract_mentions(html):
 	for d in soup.find_all("span", attrs={"class": "mention"}):
 		mentions.append(frappe._dict(email=d.get("data-id")))
 	return mentions
+
+
+def alphanumeric_to_int(s: str) -> int | None:
+	"""
+	Get int from alphanumeric string, using regex
+	String example: "foo-123" -> 123
+
+	:param s: Alphanumeric string to be searched for
+	:return: Integer if a number is found
+	"""
+	s = re.search(r"\d+", s)
+
+	if not s:
+		return
+
+	return int(s.group(0))
