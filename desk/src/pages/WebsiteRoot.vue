cdbbd55980c84df1a9cd9e18aeff6afea06abf46
Sabu Siyad <hello@ssiyad.com>
2023-04-05 01:31:58 +0530

    feat: `/`: redirect based on permission
    Redirect to agent portal tickets if logged in user has desk
    permission. Redirect to customer portal if not

---
+++ desk/src/pages/WebsiteRoot.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const AGENT_PORTAL_LANDING = "DeskTickets";
+const CUSTOMER_PORTAL_LANDING = "PortalTickets";
+
+if (authStore.hasDeskAccess) router.replace({ name: AGENT_PORTAL_LANDING });
+else router.replace({ name: CUSTOMER_PORTAL_LANDING });
+</script>
