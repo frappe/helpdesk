2563cadc80208b3d3334165b10d4559200ea750c
Sabu Siyad <hello@ssiyad.com>
2023-04-18 22:27:06 +0530

    refactor: use pinia (global) for keymaps
    - Add/remove keymaps per components
    - Render keymaps list from global store

---
+++ desk/src/stores/keymap.ts
@@ -0,0 +1,68 @@
+import { ComputedRef, ref, Ref, watch } from "vue";
+import { defineStore } from "pinia";
+import { useMagicKeys } from "@vueuse/core";
+import { isEqual } from "lodash";
+
+type KeyCombination = Array<string>;
+type Handler = () => void;
+type Help = string;
+
+const isMac = navigator.userAgent.indexOf("Mac OS X") != -1;
+const controlKey = isMac ? "⌃" : "Ctrl";
+const altKey = isMac ? "⌥" : "Alt";
+const metaKey = isMac ? "⌘" : "Meta";
+
+class Shortcut {
+	constructor(
+		public isActive: ComputedRef<boolean>,
+		public keyCombination: KeyCombination,
+		public handler: Handler,
+		public help?: Help
+	) {}
+
+	comboString() {
+		return this.keyCombination
+			.map((k) =>
+				k
+					.replaceAll("Control", controlKey)
+					.replaceAll("Alt", altKey)
+					.replaceAll("Meta", metaKey)
+			)
+			.join(" + ");
+	}
+}
+
+export const useKeymapStore = defineStore("keymap", () => {
+	const keys = useMagicKeys();
+	const items: Ref<Array<Shortcut>> = ref([]);
+	const isOpen = ref(false);
+
+	function add(combination: KeyCombination, handler: Handler, help?: Help) {
+		const combo = Array.isArray(combination) ? combination : [combination];
+		const isActive = keys[combo.join("+")];
+		const s = new Shortcut(isActive, combo, handler, help);
+
+		remove(combo);
+		items.value.push(s);
+	}
+
+	function remove(combo: KeyCombination) {
+		items.value = items.value.filter((s) => !isEqual(s.keyCombination, combo));
+	}
+
+	function toggleVisibility(open?: boolean) {
+		isOpen.value = open ?? !isOpen.value;
+	}
+
+	watch(items, (v) => v.filter((s) => s.isActive).forEach((s) => s.handler()), {
+		deep: true,
+	});
+
+	return {
+		add,
+		isOpen,
+		items,
+		remove,
+		toggleVisibility,
+	};
+});
