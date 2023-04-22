dd01c1e544444577ae135ca950cff30b07e8cb8d
Sabu Siyad <hello@ssiyad.com>
2023-04-22 11:45:35 +0530

    fix(ui): ticket details: minor changes

--- desk/src/pages/desk/ticket/TicketDetails.vue
+++ desk/src/pages/desk/ticket/TicketDetails.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="flex h-full flex-col">
-		<div class="border-l border-gray-100">
-			<div class="my-3.5 mx-4">
+	<div class="flex flex-col">
+		<div class="border-l">
+			<div class="m-4">
 				<div class="flex items-center justify-between">
 					<div class="text-lg font-semibold text-gray-900">Ticket details</div>
 					<Button icon="x" appearance="minimal" />
 				</div>
-				<div class="h-18 my-3 flex flex-col justify-between">
+				<div class="my-6 flex flex-col justify-between gap-3.5">
 					<div class="flex justify-between">
 						<div class="text-xs text-gray-600">Customer:</div>
 						<div class="flex items-center gap-1">
@@ -34,36 +34,40 @@
 			</div>
 		</div>
 		<div class="divider"></div>
-		<div class="flex grow flex-col border-l border-gray-100">
-			<div class="mx-4 my-3.5 flex flex-col gap-3 truncate">
-				<div class="flex flex-col gap-1">
-					<div class="text-xs text-gray-600">Assigned To</div>
-					<Autocomplete placeholder="Select an agent" />
+		<div
+			class="flex grow flex-col gap-3 truncate border-l p-4"
+			:style="{
+				'overflow-y': 'scroll',
+			}"
+		>
+			<div class="flex flex-col gap-1">
+				<div class="text-xs text-gray-600">Assigned To</div>
+				<Autocomplete placeholder="Select an agent" />
+			</div>
+			<div class="flex flex-col gap-1">
+				<div class="text-xs text-gray-600">Ticket Type</div>
+				<Autocomplete placeholder="Select a ticket type" />
+			</div>
+			<div class="flex gap-2">
+				<div class="flex w-1/2 flex-col gap-1">
+					<div class="text-xs text-gray-600">Priority</div>
+					<Autocomplete placeholder="High" />
 				</div>
-				<div class="flex flex-col gap-1">
-					<div class="text-xs text-gray-600">Ticket Type</div>
-					<Autocomplete placeholder="Select a ticket type" />
-				</div>
-				<div class="flex gap-2">
-					<div class="flex w-1/2 flex-col gap-1">
-						<div class="text-xs text-gray-600">Priority</div>
-						<Autocomplete placeholder="High" />
-					</div>
-					<div class="flex w-1/2 flex-col gap-1">
-						<div class="text-xs text-gray-600">Status</div>
-						<Autocomplete placeholder="Open" />
-					</div>
-				</div>
-				<div class="flex flex-col gap-1">
-					<div class="text-xs text-gray-600">Team</div>
-					<Autocomplete placeholder="Select a team" />
+				<div class="flex w-1/2 flex-col gap-1">
+					<div class="text-xs text-gray-600">Status</div>
+					<Autocomplete placeholder="Open" />
 				</div>
 			</div>
-			<div class="grow"></div>
+			<div class="flex flex-col gap-1">
+				<div class="text-xs text-gray-600">Team</div>
+				<Autocomplete placeholder="Select a team" />
+			</div>
+		</div>
+		<div class="border-l">
 			<div
-				class="mx-4 my-3.5 flex h-8 cursor-pointer items-center justify-center rounded-lg bg-gray-100 px-3 py-2 text-base text-gray-800 hover:bg-gray-200"
+				class="mx-4 my-3.5 flex h-8 cursor-pointer items-center justify-center rounded-lg bg-gray-100 px-3 py-2 hover:bg-gray-200"
 			>
-				Save
+				<div class="text-base text-gray-800">Save</div>
 			</div>
 		</div>
 	</div>
@@ -108,7 +112,6 @@ import { Autocomplete, Avatar, Button } from "frappe-ui";
 	border-left-width: 1px;
 	border-top-width: 1px;
 	border-bottom-width: 1px;
-	/* border-width: 1px; */
 }
 
 .divider:after {
