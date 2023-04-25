4bc4c99af1ee30fab3c6fbdf94ab43c1928706ba
Sabu Siyad <hello@ssiyad.com>
2023-04-26 00:55:03 +0530

    feat: copy ticket id

--- desk/src/pages/desk/ticket/TopBar.vue
+++ desk/src/pages/desk/ticket/TopBar.vue
@@ -5,7 +5,9 @@
 		</div>
 		<div class="flex items-center gap-1 text-base text-gray-600">
 			<IconHash />
-			{{ ticket.doc.name }}
+			<div class="cursor-copy" @click="copyId">
+				{{ ticket.doc.name }}
+			</div>
 			<IconDot />
 			Last modified
 			<Tooltip :text="dateLong">
@@ -16,14 +18,27 @@
 </template>
 
 <script setup lang="ts">
-import dayjs from "dayjs";
 import { computed } from "vue";
 import { Tooltip } from "frappe-ui";
+import { useClipboard } from "@vueuse/core";
+import dayjs from "dayjs";
 import { ticket } from "./data";
+import { createToast } from "@/utils/toasts";
 import IconHash from "~icons/espresso/hash";
 import IconDot from "~icons/ph/dot-outline-fill";
 
 const date = computed(() => dayjs(ticket.doc.modified).tz(dayjs.tz.guess()));
 const dateShort = computed(() => date.value.fromNow());
 const dateLong = computed(() => date.value.format("dddd, MMMM D, YYYY h:mm A"));
+const { copy } = useClipboard();
+
+async function copyId() {
+	await copy(ticket.doc.name);
+
+	createToast({
+		title: "Copied to clipboard",
+		icon: "check",
+		iconClasses: "text-green-600",
+	});
+}
 </script>
