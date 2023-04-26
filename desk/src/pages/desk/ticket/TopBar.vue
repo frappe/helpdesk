f7d8e8cc6113e40340cdb200b65174a4404f81ac
Sabu Siyad <hello@ssiyad.com>
2023-04-26 16:50:19 +0530

    feat: top bar: back button

--- desk/src/pages/desk/ticket/TopBar.vue
+++ desk/src/pages/desk/ticket/TopBar.vue
@@ -1,9 +1,15 @@
 <template>
-	<div class="flex flex-col justify-center border-b border-gray-100 px-5 py-3">
-		<div class="line-clamp-1 text-xl font-medium text-gray-900">
-			{{ ticket.doc.subject }}
+	<div class="border-b border-gray-100 py-3">
+		<div class="flex items-center">
+			<IconCaretLeft
+				class="ml-5 mr-2 h-4 w-4 cursor-pointer text-gray-700"
+				@click="router.back"
+			/>
+			<div class="line-clamp-1 text-xl font-medium text-gray-900">
+				{{ ticket.doc.subject }}
+			</div>
 		</div>
-		<div class="flex items-center gap-1 text-base text-gray-600">
+		<div class="ml-11 flex items-center gap-1 text-base text-gray-600">
 			<IconHash />
 			<div class="cursor-copy" @click="copyId">
 				{{ ticket.doc.name }}
@@ -19,18 +25,22 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { useRouter } from "vue-router";
 import { Tooltip } from "frappe-ui";
 import { useClipboard } from "@vueuse/core";
 import dayjs from "dayjs";
 import { ticket } from "./data";
 import { createToast } from "@/utils/toasts";
+import IconCaretLeft from "~icons/ph/caret-left";
+import IconDot from "~icons/ph/dot-bold";
 import IconHash from "~icons/espresso/hash";
-import IconDot from "~icons/ph/dot-outline-fill";
+
+const { copy } = useClipboard();
+const router = useRouter();
 
 const date = computed(() => dayjs(ticket.doc.modified).tz(dayjs.tz.guess()));
 const dateShort = computed(() => date.value.fromNow());
 const dateLong = computed(() => date.value.format("dddd, MMMM D, YYYY h:mm A"));
-const { copy } = useClipboard();
 
 async function copyId() {
 	await copy(ticket.doc.name);
