db295dab1a4d26d5815b6d160e9b6eff0dc5b649
Sabu Siyad <hello@ssiyad.com>
2023-05-02 15:17:02 +0530

    fix: canned responses

--- desk/src/pages/desk/ticket/editor/BottomSection.vue
+++ desk/src/pages/desk/ticket/editor/BottomSection.vue
@@ -1,98 +1,95 @@
 <template>
-	<div class="flex flex-col divide-y rounded-b-xl">
-		<div class="ml-2 flex flex-wrap gap-2 py-2">
-			<div
-				v-for="attachment in editor.attachments"
-				:key="attachment"
-				class="flex items-center gap-1 rounded border border-gray-400 bg-gray-100 p-1 shadow"
-			>
-				<div class="flex flex-row items-center space-x-1">
-					<FeatherIcon name="file-text" class="h-4 w-4 text-gray-700" />
-					<span class="text-sm text-gray-700">
-						{{ attachment.file_name }}
-					</span>
-				</div>
-				<Button
-					class="h-4 w-4"
-					icon="x"
-					@click="
-						editor.attachments = editor.attachments.filter(
-							(x) => x.name != attachment.name
-						)
-					"
-				/>
-			</div>
-		</div>
-		<div v-if="isTextFormattingVisible" class="px-3.5 py-2">
-			<TextEditorFixedMenu :buttons="menuButtons" />
-		</div>
-		<div class="flex justify-between px-3.5 py-2">
-			<div class="flex flex-row items-center space-x-2">
-				<CustomIcons
-					:class="{
-						'bg-gray-200': isTextFormattingVisible,
-					}"
-					name="text-formatting"
-					class="h-7 w-7 rounded p-1"
-					role="button"
-					@click="isTextFormattingVisible = !isTextFormattingVisible"
-				/>
-				<FileUploader @success="(file) => editor.attachments.push(file)">
-					<template #default="{ uploading, openFileSelector }">
-						<FeatherIcon
-							name="paperclip"
-							class="h-5 w-5"
-							role="button"
-							:disabled="uploading"
-							@click="openFileSelector"
-						/>
-					</template>
-				</FileUploader>
-				<CustomIcons
-					name="add-response"
-					class="h-7 w-7 rounded p-1"
-					role="button"
-					@click="showCannedResponsesDialog = true"
-				/>
-				<CustomIcons
-					name="article-reply"
-					class="h-7 w-7 rounded p-1"
-					role="button"
-					@click="showArticleResponseDialog = true"
-				/>
-				<CannedResponsesDialog
-					:show="showCannedResponsesDialog"
-					@close="showCannedResponsesDialog = false"
-				/>
-
-				<ArticleResponseDialog
-					:show="showArticleResponseDialog"
-					@close="showArticleResponseDialog = false"
-				/>
-			</div>
-			<div class="flex items-center gap-4">
-				<IconDelete
-					class="h-5 w-5 cursor-pointer text-gray-900"
-					@click.prevent="editor.isExpanded = false"
-				/>
-				<div class="flex">
+	<div>
+		<div class="flex flex-col divide-y rounded-b-xl">
+			<div class="ml-2 flex flex-wrap gap-2 py-2">
+				<div
+					v-for="attachment in editor.attachments"
+					:key="attachment"
+					class="flex items-center gap-1 rounded border border-gray-400 bg-gray-100 p-1 shadow"
+				>
+					<div class="flex flex-row items-center space-x-1">
+						<FeatherIcon name="file-text" class="h-4 w-4 text-gray-700" />
+						<span class="text-sm text-gray-700">
+							{{ attachment.file_name }}
+						</span>
+					</div>
 					<Button
-						label="Reply"
-						appearance="primary"
-						class="rounded-r-none"
-						@click="newCommunication"
-					/>
-					<Dropdown
-						:options="dropdownOptions"
-						:button="{
-							icon: 'chevron-down',
-							appearance: 'primary',
-							class: 'rounded-l-none',
-						}"
+						class="h-4 w-4"
+						icon="x"
+						@click="removeAttachment(attachment.name)"
 					/>
 				</div>
 			</div>
+			<div v-if="isTextFormattingVisible" class="px-3.5 py-2">
+				<TextEditorFixedMenu :buttons="menuButtons" />
+			</div>
+			<div class="flex justify-between px-3.5 py-2">
+				<div class="flex flex-row items-center space-x-2">
+					<CustomIcons
+						:class="{
+							'bg-gray-200': isTextFormattingVisible,
+						}"
+						name="text-formatting"
+						class="h-7 w-7 rounded p-1"
+						role="button"
+						@click="isTextFormattingVisible = !isTextFormattingVisible"
+					/>
+					<FileUploader @success="(file) => editor.attachments.push(file)">
+						<template #default="{ uploading, openFileSelector }">
+							<FeatherIcon
+								name="paperclip"
+								class="h-5 w-5"
+								role="button"
+								:disabled="uploading"
+								@click="openFileSelector"
+							/>
+						</template>
+					</FileUploader>
+					<CustomIcons
+						name="add-response"
+						class="h-7 w-7 rounded p-1"
+						role="button"
+						@click="showCannedResponses = true"
+					/>
+					<CustomIcons
+						name="article-reply"
+						class="h-7 w-7 rounded p-1"
+						role="button"
+						@click="showArticleResponseDialog = true"
+					/>
+					<ArticleResponseDialog
+						:show="showArticleResponseDialog"
+						@close="showArticleResponseDialog = false"
+					/>
+				</div>
+				<div class="flex items-center gap-4">
+					<IconDelete
+						class="h-5 w-5 cursor-pointer text-gray-900"
+						@click.prevent="editor.isExpanded = false"
+					/>
+					<div class="flex">
+						<Button
+							label="Reply"
+							appearance="primary"
+							class="rounded-r-none"
+							@click="newCommunication"
+						/>
+						<Dropdown
+							:options="dropdownOptions"
+							:button="{
+								icon: 'chevron-down',
+								appearance: 'primary',
+								class: 'rounded-l-none',
+							}"
+						/>
+					</div>
+				</div>
+			</div>
 		</div>
+		<CannedResponses
+			:show="showCannedResponses"
+			@close="showCannedResponses = false"
+		/>
 	</div>
 </template>
 
@@ -110,13 +107,13 @@ import { useAuthStore } from "@/stores/auth";
 import { editor, ticket, clean } from "../data";
 import { TextEditorMenuButtons as menuButtons } from "../../consts";
 import ArticleResponseDialog from "@/components/desk/global/ArticleResponseDialog.vue";
-import CannedResponsesDialog from "@/components/desk/global/CannedResponsesDialog.vue";
+import CannedResponses from "./CannedResponses.vue";
 import CustomIcons from "@/components/desk/global/CustomIcons.vue";
 import IconDelete from "~icons/espresso/delete";
 
 const authStore = useAuthStore();
 const showArticleResponseDialog = ref(false);
-const showCannedResponsesDialog = ref(false);
+const showCannedResponses = ref(false);
 const isTextFormattingVisible = ref(false);
 const dropdownOptions = [
 	{
@@ -135,6 +132,10 @@ const insertRes = createResource({
 		clean();
 	},
 });
+
+function removeAttachment(name: string) {
+	editor.attachments = editor.attachments.filter((x) => x.name != name);
+}
 
 function newCommunication() {
 	ticket.replyViaAgent.submit({
