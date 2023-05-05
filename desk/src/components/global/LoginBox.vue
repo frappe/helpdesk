87b99afe92b44326f67df7775cda98c4eabb956c
Sabu Siyad <hello@ssiyad.com>
2023-05-05 13:06:52 +0530

    feat: brand logo for auth pages

--- desk/src/components/global/LoginBox.vue
+++ desk/src/components/global/LoginBox.vue
@@ -1,37 +1,36 @@
 <template>
-	<div class="h-full pt-4 sm:pt-16">
-		<div>
-			<div class="flex">
-				<div class="mx-auto h-4 w-auto">
-					<CustomIcons name="frappedesk" class="`w-5 h-5" />
+	<div class="flex h-screen w-screen items-center justify-center">
+		<span>
+			<img
+				v-if="configStore.brandLogo"
+				:src="configStore.brandLogo"
+				class="m-auto h-6"
+			/>
+			<div v-else class="flex">
+				<div class="mx-auto h-6 w-auto">
+					<CustomIcons name="frappedesk" class="h-6" />
 				</div>
 			</div>
-			<div
-				class="sm:w-0112 mx-auto bg-white px-4 py-8 sm:mt-6 sm:w-96 sm:rounded-lg sm:px-10 sm:shadow-xl"
-			>
-				<div class="mb-6 text-center">
-					<span class="text-lg">{{ title }}</span>
+			<div class="w-96 rounded-xl py-8 px-10 shadow-xl">
+				<div class="mb-6 text-center text-lg">
+					{{ title }}
 				</div>
 				<slot></slot>
 			</div>
-		</div>
+		</span>
 	</div>
 </template>
 
-<script>
-import CustomIcons from "@/components/desk/global/CustomIcons.vue"
+<script setup lang="ts">
+import { useConfigStore } from "@/stores/config";
+import CustomIcons from "@/components/desk/global/CustomIcons.vue";
 
-export default {
-	name: "LoginBox",
-	props: ["title"],
-	components: {
-		CustomIcons,
+defineProps({
+	title: {
+		type: String,
+		required: true,
 	},
-	methods: {
-		redirectForFrappeioAuth() {
-			// TODO: ref (https://github.com/frappe/press/commit/f38b26dca5ea44c9b57de37a0162f9707356a903)
-			window.location = "/f-login"
-		},
-	},
-}
+});
+
+const configStore = useConfigStore();
 </script>
