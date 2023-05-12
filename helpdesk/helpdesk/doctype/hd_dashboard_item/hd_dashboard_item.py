d79ace8be4a2cefc08e654efa18265b51665c0b0
Sabu Siyad <hello@ssiyad.com>
2023-05-12 12:14:35 +0530

    feat: controller: check if `res` is assigned

--- helpdesk/helpdesk/doctype/hd_dashboard_item/hd_dashboard_item.py
+++ helpdesk/helpdesk/doctype/hd_dashboard_item/hd_dashboard_item.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2023, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import re
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
+
+HAS_RES_REGEX = "res\s?=\s?.+"
 
 
 class HDDashboardItem(Document):
-	pass
+	def before_save(self):
+		if not re.search(HAS_RES_REGEX, self.snippet):
+			frappe.throw(
+				_("Snippet must assign the `res` variable!"),
+				title=_("Invalid Snippet"),
+			)
