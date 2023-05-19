3524efa739b7491f5a21cf4449abff7ccfb7e28c
Sabu Siyad <hello@ssiyad.com>
2023-05-19 14:33:42 +0530

    fix(ux): customer portal: auto update on new communication (#1202)

--- desk/src/components/portal/ticket/Conversations.vue
+++ desk/src/components/portal/ticket/Conversations.vue
@@ -4,16 +4,14 @@
 			<div
 				v-for="(conversation, index) in conversations"
 				:key="conversation.name"
-				class="flex flex-col"
 				ref="conversationContainer"
+				class="flex flex-col"
 			>
 				<div :ref="`conversation-${index}`">
 					<ConversationCard
-						:userName="getUserName(conversation)"
-						:profilePicUrl="
-							conversation.sender.image
-								? conversation.sender.image
-								: ''
+						:user-name="getUserName(conversation)"
+						:profile-pic-url="
+							conversation.sender.image ? conversation.sender.image : ''
 						"
 						:time="conversation.creation"
 						:message="conversation.content"
@@ -31,22 +29,22 @@
 </template>
 
 <script>
-import ConversationCard from "./ConversationCard.vue"
-import { LoadingText } from "frappe-ui"
-import { ref } from "vue"
+import ConversationCard from "./ConversationCard.vue";
+import { LoadingText } from "frappe-ui";
+import { ref } from "vue";
 
 export default {
 	name: "Conversations",
-	props: ["ticketId", "scrollToBottom"],
 	components: {
 		ConversationCard,
 		LoadingText,
 	},
+	props: ["ticketId", "scrollToBottom"],
 	setup() {
-		const userColors = ref({})
-		const lastColorIndex = ref(-1)
+		const userColors = ref({});
+		const lastColorIndex = ref(-1);
 
-		return { userColors, lastColorIndex }
+		return { userColors, lastColorIndex };
 	},
 	resources: {
 		conversations() {
@@ -56,73 +54,66 @@ export default {
 					ticket_id: this.ticketId,
 				},
 				auto: true,
-			}
+			};
 		},
 	},
 	computed: {
 		conversations() {
 			this.$nextTick(() => {
-				this.autoScrollToBottom()
-			})
-			return this.$resources.conversations.data || null
+				this.autoScrollToBottom();
+			});
+			return this.$resources.conversations.data || null;
 		},
 	},
 	watch: {
 		scrollToBottom(scroll) {
 			if (scroll) {
-				this.autoScrollToBottom()
+				this.autoScrollToBottom();
 			}
 		},
 	},
 	mounted() {
-		this.$socket.on("list_update", (data) => {
-			if (data["doctype"] == "HD Ticket" && data["name"] == this.ticketId) {
-				this.$resources.conversations.fetch()
-			}
-		})
+		this.$socket.on("helpdesk:new-communication", (data) => {
+			if (data.ticket_id !== this.ticketId) return;
+			this.$resources.conversations.fetch();
+		});
 	},
 	unmounted() {
-		this.$socket.off("list_update")
+		this.$socket.off("helpdesk:new-communication");
 	},
 	methods: {
 		getUserName(conversation) {
 			return (
-				(conversation.sender.first_name
-					? conversation.sender.first_name
-					: "") +
+				(conversation.sender.first_name ? conversation.sender.first_name : "") +
 				" " +
-				(conversation.sender.last_name
-					? conversation.sender.last_name
-					: "")
-			)
+				(conversation.sender.last_name ? conversation.sender.last_name : "")
+			);
 		},
 		autoScrollToBottom() {
 			if (this.conversations) {
 				const [el] =
-					this.$refs[
-						"conversation-" + (this.conversations.length - 1)
-					]
+					this.$refs["conversation-" + (this.conversations.length - 1)];
 				if (el) {
-					el.scrollIntoView({ behavior: "smooth" })
+					el.scrollIntoView({ behavior: "smooth" });
 				}
 			}
 		},
 		getConversationCardColor(conversation) {
-			const userName = this.getUserName(conversation)
-			let cardColors = ["blue", "gray", "green", "red"]
+			const userName = this.getUserName(conversation);
+			let cardColors = ["blue", "gray", "green", "red"];
 
 			if (!this.userColors[userName]) {
 				if (this.lastColorIndex == cardColors.length - 1) {
-					this.lastColorIndex = 0
+					this.lastColorIndex = 0;
 				} else {
-					this.lastColorIndex++
+					this.lastColorIndex++;
 				}
-				this.userColors[userName] = cardColors[this.lastColorIndex]
+				this.userColors[userName] = cardColors[this.lastColorIndex];
 			}
-			return this.userColors[userName]
+			return this.userColors[userName];
 		},
 	},
-}
+};
 </script>
 
 <style></style>
