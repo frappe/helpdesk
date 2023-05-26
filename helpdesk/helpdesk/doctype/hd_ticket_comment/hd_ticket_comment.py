fc602988d079fd5c398343c7cc46179f2a715f5d
Sabu Siyad <hello@ssiyad.com>
2023-05-26 16:13:14 +0530

    chore: remove comment notification (unused)

--- helpdesk/helpdesk/doctype/hd_ticket_comment/hd_ticket_comment.py
+++ helpdesk/helpdesk/doctype/hd_ticket_comment/hd_ticket_comment.py
@@ -4,33 +4,9 @@
 import frappe
 from frappe.model.document import Document
 from frappe.realtime import get_website_room
-from frappe.utils import get_fullname
-
-from helpdesk.utils import extract_mentions
 
 
 class HDTicketComment(Document):
-	def on_change(self):
-		mentions = extract_mentions(self.content)
-
-		for mention in mentions:
-			values = frappe._dict(
-				from_user=self.commented_by,
-				to_user=mention.email,
-				ticket=self.reference_ticket,
-				comment=self.name,
-			)
-
-			if frappe.db.exists("HD Notification", values):
-				continue
-
-			notification = frappe.get_doc(doctype="HD Notification")
-			notification.message = (
-				f"{get_fullname(self.owner)} mentioned you in Ticket #{self.reference_ticket}",
-			)
-			notification.update(values)
-			notification.insert(ignore_permissions=True)
-
 	def after_insert(self):
 		event = "helpdesk:new-ticket-comment"
 		data = {"ticket_id": self.reference_ticket}
