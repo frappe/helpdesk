f313854ce0538b9e8e72d065477e01ade68efe8d
Sabu Siyad <hello@ssiyad.com>
2023-05-27 13:35:37 +0530

    feat: agent portal: response editor: conditionally hide email fields (#1238)

--- helpdesk/api/config.py
+++ helpdesk/api/config.py
@@ -3,18 +3,12 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_config():
-	brand_logo = frappe.db.get_single_value("HD Settings", "brand_logo")
-	brand_favicon = frappe.db.get_single_value("HD Settings", "brand_favicon")
-	helpdesk_name = frappe.db.get_single_value("HD Settings", "helpdesk_name")
-	is_setup_complete = frappe.db.get_single_value("HD Settings", "setup_complete")
-	suppress_default_email_toast = frappe.db.get_single_value(
-		"HD Settings", "suppress_default_email_toast"
-	)
+	settings = frappe.get_single("HD Settings")
 
 	return {
-		"brand_logo": brand_logo,
-		"brand_favicon": brand_favicon,
-		"helpdesk_name": helpdesk_name,
-		"is_setup_complete": is_setup_complete,
-		"suppress_default_email_toast": suppress_default_email_toast,
+		"brand_logo": settings.brand_logo,
+		"brand_favicon": settings.brand_favicon,
+		"helpdesk_name": settings.helpdesk_name,
+		"is_setup_complete": settings.setup_complete,
+		"skip_email_workflow": settings.skip_email_workflow,
 	}
