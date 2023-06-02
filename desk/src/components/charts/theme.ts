c67060f6818cbb58a80d1e67688ca553256d4be8
Sabu Siyad <hello@ssiyad.com>
2023-06-02 21:47:50 +0530

    fix: tailwind config import

--- desk/src/components/charts/theme.ts
+++ desk/src/components/charts/theme.ts
@@ -1,5 +1,5 @@
 import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "tailwind.config.js";
+import * as tailwindConfig from "tailwind.config.js";
 
 const config = resolveConfig(tailwindConfig);
 const ignore = ["slate", "gray", "zinc", "neutral", "stone"];
