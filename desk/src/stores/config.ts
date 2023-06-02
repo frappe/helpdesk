1b9f7f0fcf5fe644e7b61dc5542ee58229942bcc
Sabu Siyad <hello@ssiyad.com>
2023-06-02 13:13:57 +0530

    chore: page title: use `•` instead of `|`

--- desk/src/stores/config.ts
+++ desk/src/stores/config.ts
@@ -29,7 +29,7 @@ export const useConfigStore = defineStore("config", () => {
 	const pageTitle = ref(null);
 	const windowTitle = computed(() =>
 		pageTitle.value
-			? `${pageTitle.value} | ${helpdeskName.value}`
+			? `${pageTitle.value} • ${helpdeskName.value}`
 			: helpdeskName.value
 	);
 
