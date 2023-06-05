a269ed4f932f6af85e8c281f3c400c2472728ad4
Sabu Siyad <hello@ssiyad.com>
2023-06-05 18:41:08 +0530

    refactor: agent portal: new ticket dialog (#1249)
    * refactor: agent portal: new ticket dialog
    
    * chore: remove primary button icon

--- desk/src/components/SearchComplete.vue
+++ desk/src/components/SearchComplete.vue
@@ -1,11 +1,11 @@
 <template>
-	<Autocomplete
-		placeholder="Select an option"
-		:options="options"
-		:value="selection"
-		@update:query="(q) => onUpdateQuery(q)"
-		@change="(v) => (selection = v)"
-	/>
+  <Autocomplete
+    placeholder="Select an option"
+    :options="options"
+    :value="selection"
+    @update:query="(q) => onUpdateQuery(q)"
+    @change="(v) => (selection = v)"
+  />
 </template>
 
 <script setup lang="ts">
@@ -13,54 +13,54 @@ import { computed, ref } from "vue";
 import { createListResource, Autocomplete } from "frappe-ui";
 
 const props = defineProps({
-	doctype: {
-		type: String,
-		required: true,
-	},
-	searchField: {
-		type: String,
-		required: false,
-		default: "name",
-	},
-	labelField: {
-		type: String,
-		required: false,
-		default: "name",
-	},
-	valueField: {
-		type: String,
-		required: false,
-		default: "name",
-	},
-	pageLength: {
-		type: Number,
-		required: false,
-		default: 10,
-	},
+  doctype: {
+    type: String,
+    required: true,
+  },
+  searchField: {
+    type: String,
+    required: false,
+    default: "name",
+  },
+  labelField: {
+    type: String,
+    required: false,
+    default: "name",
+  },
+  valueField: {
+    type: String,
+    required: false,
+    default: "name",
+  },
+  pageLength: {
+    type: Number,
+    required: false,
+    default: 10,
+  },
 });
 
 const r = createListResource({
-	doctype: props.doctype,
-	pageLength: props.pageLength,
-	auto: true,
-	fields: [props.labelField, props.searchField, props.valueField],
+  doctype: props.doctype,
+  pageLength: props.pageLength,
+  auto: true,
+  fields: [props.labelField, props.searchField, props.valueField],
 });
 const options = computed(
-	() =>
-		r.data?.map((result) => ({
-			label: result[props.labelField],
-			value: result[props.valueField],
-		})) || []
+  () =>
+    r.data?.map((result) => ({
+      label: result[props.labelField],
+      value: result[props.valueField],
+    })) || []
 );
 const selection = ref(null);
 
 function onUpdateQuery(query: string) {
-	r.update({
-		filters: {
-			[props.searchField]: ["like", `%${query}%`],
-		},
-	});
+  r.update({
+    filters: {
+      [props.searchField]: ["like", `%${query}%`],
+    },
+  });
 
-	r.reload();
+  r.reload();
 }
 </script>
--- desk/src/pages/desk/ticket-list/NewTicketDialog.vue
+++ desk/src/pages/desk/ticket-list/NewTicketDialog.vue
@@ -1,354 +1,155 @@
 <template>
-	<div>
-		<Dialog v-model="open" :options="{ title: 'Create New Ticket' }">
-			<template #body-content>
-				<div class="space-y-4">
-					<div class="space-y-1">
-						<Input v-model="subject" label="Subject" type="text" />
-						<ErrorMessage :message="subjectValidationError" />
-					</div>
-					<div class="w-full space-y-1">
-						<div>
-							<span class="mb-2 block text-sm leading-4 text-gray-700">
-								Ticket Type
-							</span>
-						</div>
-						<Autocomplete
-							:value="selectedTicketType"
-							:resource-options="{
-								url: 'helpdesk.extends.client.get_list',
-								inputMap: (query) => {
-									return {
-										doctype: 'HD Ticket Type',
-										pluck: 'name',
-										filters: [['name', 'like', `%${query}%`]],
-									};
-								},
-								responseMap: (res) => {
-									return res.map((d) => {
-										return {
-											label: d.name,
-											value: d.name,
-										};
-									});
-								},
-							}"
-							@change="
-								(item) => {
-									if (!item) {
-										return;
-									}
-									selectedTicketType = item.value;
-								}
-							"
-						/>
-					</div>
-					<div class="w-full space-y-1">
-						<div>
-							<span class="mb-2 block text-sm leading-4 text-gray-700">
-								Created By
-							</span>
-						</div>
-						<Autocomplete
-							:value="selectedContact"
-							:resource-options="{
-								url: 'helpdesk.extends.client.get_list',
-								inputMap: (query) => {
-									return {
-										doctype: 'Contact',
-										pluck: 'name',
-										filters: [['Contact','name', 'like', `%${query}%`]],
-									};
-								},
-								responseMap: (res) => {
-									return res.map((d) => {
-										return {
-											label: d.name,
-											value: d.name,
-										};
-									});
-								},
-							}"
-							@change="
-								(item) => {
-									if (!item) {
-										return;
-									}
-									selectedContact = item.value;
-									selectedCustomer = selectedContact.split('-')[1];
-								}
-							"
-						/>
-						<ErrorMessage :message="contactValidationError" />
-					</div>
-					<div class="w-full space-y-1">
-						<div>
-							<span class="mb-2 block text-sm leading-4 text-gray-700">
-								Customer
-							</span>
-						</div>
-						<Autocomplete
-							:value="fdCustomer != null ? fdCustomer : selectedCustomer"
-							:resource-options="{
-								url: 'helpdesk.extends.client.get_list',
-								inputMap: (query) => {
-									return {
-										doctype: 'HD Customer',
-										pluck: 'name',
-										filters: [['name', 'like', `%${query}%`]],
-									};
-								},
-								responseMap: (res) => {
-									return res.map((d) => {
-										return {
-											label: d.name,
-											value: d.name,
-										};
-									});
-								},
-							}"
-							@change="
-								(item) => {
-									if (!item) {
-										return;
-									}
-									selectedCustomer = item.value;
-								}
-							"
-						/>
-					</div>
-					<div class="space-y-1">
-						<div>
-							<span class="mb-2 block text-sm leading-4 text-gray-700">
-								Description
-							</span>
-							<TextEditor
-								ref="textEditor"
-								class="rounded border"
-								editor-class="px-2 min-h-[8rem] overflow-y-auto max-h-[15vh]"
-								:content="descriptionContent"
-								:starterkit-options="{
-									heading: { levels: [2, 3, 4, 5, 6] },
-								}"
-								:editable="true"
-								@change="
-									(content) => {
-										descriptionContent = content;
-									}
-								"
-							>
-								<template #top>
-									<TextEditorFixedMenu
-										:buttons="textEditorMenuButtons"
-										class="bg-gray-100"
-									/>
-								</template>
-							</TextEditor>
-						</div>
-						<ErrorMessage :message="descriptionValidationError" />
-					</div>
-					<div class="float-right flex space-x-2">
-						<Button
-							appearance="primary"
-							:loading="isCreating"
-							@click="createTicket()"
-						>
-							Create
-						</Button>
-					</div>
-				</div>
-			</template>
-		</Dialog>
-	</div>
+  <Dialog :options="options">
+    <template #body-content>
+      <div class="flex flex-col gap-3 text-xs text-gray-700">
+        <div class="grid grid-cols-3 gap-2">
+          <div class="flex flex-col gap-1">
+            <div class="flex gap-1">
+              Ticket Type
+              <div class="text-red-500">*</div>
+            </div>
+            <SearchComplete
+              doctype="HD Ticket Type"
+              label="Ticket Type"
+              @change="ticketType = $event['value']"
+            />
+          </div>
+          <div class="flex w-full flex-col gap-1 place-self-center">
+            <div class="flex gap-1">
+              Contact
+              <div class="text-red-500">*</div>
+            </div>
+            <SearchComplete
+              doctype="Contact"
+              label="Ticket Type"
+              @change="contact = $event['value']"
+            />
+          </div>
+          <div class="flex w-full flex-col gap-1 place-self-center">
+            Customer
+            <SearchComplete
+              doctype="HD Customer"
+              label="Ticket Type"
+              @change="customer = $event['value']"
+            />
+          </div>
+        </div>
+        <div class="flex flex-col gap-1">
+          <div class="flex gap-1">
+            Subject
+            <div class="text-red-500">*</div>
+          </div>
+          <Input placeholder="Short message" @input="subject = $event" />
+        </div>
+        <div class="flex flex-col gap-1">
+          <div class="flex gap-1">
+            Description
+            <div class="text-red-500">*</div>
+          </div>
+          <TextEditor
+            ref="textEditor"
+            placeholder="Detailed explanation"
+            :content="description"
+            @change="description = $event"
+          >
+            <template #bottom="{ editor }">
+              <TextEditorBottom
+                :editor="editor"
+                :attachments="Array.from(attachments)"
+                @attachment-added="(file) => attachments.add(file)"
+                @attachment-removed="(file) => attachments.delete(file)"
+              >
+                <template #actions-right>
+                  <Button
+                    class="bg-gray-900 text-white hover:bg-gray-800"
+                    label="Create"
+                    :disabled="isDisabled"
+                    @click="insertRes.submit()"
+                  />
+                </template>
+              </TextEditorBottom>
+            </template>
+          </TextEditor>
+        </div>
+      </div>
+    </template>
+  </Dialog>
 </template>
 
-<script>
-import { ErrorMessage, TextEditor } from "frappe-ui";
-import { TextEditorFixedMenu } from "frappe-ui/src/components/TextEditor";
-import Autocomplete from "@/components/global/Autocomplete.vue";
+<script setup lang="ts">
 import { ref, computed } from "vue";
-export default {
-	name: "NewTicketDialog",
-	components: {
-		TextEditor,
-		TextEditorFixedMenu,
-		Autocomplete,
-		ErrorMessage,
-	},
-	props: {
-		modelValue: {
-			type: Boolean,
-			required: true,
-		},
-		fdCustomer: {
-			type: String,
-			default: null,
-		},
-	},
-	setup(props, { emit }) {
-		const isCreating = ref(false);
-		const selectedContact = ref("");
-		const selectedTicketType = ref("");
-		const contactValidationError = ref("");
-		const subjectValidationError = ref("");
-		const descriptionValidationError = ref("");
-		const selectedCustomer = ref("");
-		let open = computed({
-			get: () => props.modelValue,
-			set: (val) => {
-				emit("update:modelValue", val);
-				if (!val) {
-					emit("close");
-				}
-			},
-		});
+import { createResource, Button } from "frappe-ui";
+import { isEmpty } from "lodash";
+import { createToast } from "@/utils/toasts";
+import SearchComplete from "@/components/SearchComplete.vue";
+import TextEditor from "@/components/text-editor/TextEditor.vue";
+import TextEditorBottom from "@/components/text-editor/TextEditorBottom.vue";
 
-		return {
-			isCreating,
-			selectedContact,
-			selectedTicketType,
-			contactValidationError,
-			subjectValidationError,
-			descriptionValidationError,
-			open,
-			selectedCustomer,
-		};
-	},
-	data() {
-		return {
-			subject: "",
-			descriptionContent: "",
-		};
-	},
-	computed: {
-		textEditorMenuButtons() {
-			return [
-				"Paragraph",
-				["Heading 2", "Heading 3", "Heading 4", "Heading 5", "Heading 6"],
-				"Separator",
-				"Bold",
-				"Italic",
-				"Separator",
-				"Bullet List",
-				"Numbered List",
-				"Separator",
-				"Align Left",
-				"Align Center",
-				"Align Right",
-				"Separator",
-				"Image",
-				"Link",
-				"Blockquote",
-				"Code",
-				"Horizontal Rule",
-				[
-					"InsertTable",
-					"AddColumnBefore",
-					"AddColumnAfter",
-					"DeleteColumn",
-					"AddRowBefore",
-					"AddRowAfter",
-					"DeleteRow",
-					"MergeCells",
-					"SplitCell",
-					"ToggleHeaderColumn",
-					"ToggleHeaderRow",
-					"ToggleHeaderCell",
-					"DeleteTable",
-				],
-				"Separator",
-			];
-		},
-	},
-	watch: {
-		selectedContact(newValue) {
-			this.validateContactInput(newValue);
-		},
-		subject(newValue) {
-			this.validateSubjectInput(newValue);
-		},
-		descriptionContent(newValue) {
-			this.validateDescriptionInput(newValue);
-		},
-	},
-	methods: {
-		createTicket() {
-			if (this.validateInputs()) return;
-			this.$resources.newTicket.submit();
-		},
-		validateInputs() {
-			let error = this.validateContactInput(this.selectedContact);
-			error += this.validateSubjectInput(this.subject);
-			error += this.validateDescriptionInput(this.descriptionContent);
-			return error;
-		},
-		validateContactInput(value) {
-			this.contactValidationError = "";
-			if (!value) {
-				this.contactValidationError = "Contact should not be empty";
-			} else if (value.trim() == "") {
-				this.contactValidationError = "Contact should not be empty";
-			}
-			// TODO: check if the selected contact is in the list of contacts
-			return this.contactValidationError;
-		},
-		validateSubjectInput(value) {
-			this.subjectValidationError = "";
-			if (!value) {
-				this.subjectValidationError = "Subject should not be empty";
-			} else if (value.trim() == "") {
-				this.subjectValidationError = "Subject should not be empty";
-			} else if (value.length <= 2) {
-				this.subjectValidationError = "Subject should be longer than that";
-			}
-			return this.subjectValidationError;
-		},
-		validateDescriptionInput(value) {
-			this.descriptionValidationError = "";
-			if (!value) {
-				this.descriptionValidationError = "Description should not be empty";
-			} else if (
-				["<p><br></p>", "<p></p>"].includes(value.replaceAll(" ", ""))
-			) {
-				this.descriptionValidationError = "Description should not be empty";
-			}
-			return this.subjectValidationError;
-		},
-	},
-	resources: {
-		newTicket() {
-			return {
-				url: "frappe.client.insert",
-				makeParams() {
-					return {
-						doc: {
-							doctype: "HD Ticket",
-							contact: this.selectedContact,
-							subject: this.subject,
-							ticket_type: this.selectedTicketType,
-							description: this.descriptionContent,
-							customer: this.fdCustomer || this.selectedCustomer,
-						},
-					};
-				},
-				beforeSubmit() {
-					this.isCreating = true;
-				},
-				onSuccess() {
-					this.isCreating = false;
-					this.$emit("ticket-created");
-				},
-				onError(error) {
-					this.isCreating = false;
-					this.$toast({
-						title: "Error while creating ticket",
-						text: error.messages.join(", "),
-						icon: "x",
-						iconClasses: "text-red-500",
-					});
-				},
-			};
-		},
-	},
+const emit = defineEmits<{
+  (event: "close"): void;
+}>();
+
+const subject = ref("");
+const description = ref("");
+const attachments = ref(new Set([]));
+const contact = ref("");
+const customer = ref("");
+const ticketType = ref("");
+const textEditor = ref(null);
+const isDisabled = computed(
+  () =>
+    insertRes.loading ||
+    isEmpty(subject.value) ||
+    textEditor.value?.editor.isEmpty
+);
+
+const insertRes = createResource({
+  url: "helpdesk.api.ticket.create_new",
+  debounce: 500,
+  makeParams() {
+    return {
+      values: {
+        subject: subject.value,
+        description: description.value,
+        contact: contact.value,
+        ticket_type: ticketType.value,
+        customer: customer.value,
+      },
+      attachments: Array.from(attachments.value).map(
+        (attachment) => attachment.name
+      ),
+      via_customer_portal: true,
+    };
+  },
+  validate(params) {
+    if (isEmpty(params.values.ticket_type)) return "Ticket type is mandatory";
+    if (isEmpty(params.values.subject)) return "Subject should not be empty";
+    if (textEditor.value?.editor.isEmpty)
+      return "Description should not be empty";
+  },
+  onError(error) {
+    const text = error.message ? error.message : error.messages?.join(", ");
+
+    createToast({
+      title: "Error creating ticket",
+      text,
+      icon: "x",
+      iconClasses: "text-red-500",
+    });
+  },
+  onSuccess() {
+    createToast({
+      title: "Ticket created",
+      icon: "check",
+      iconClasses: "text-green-500",
+    });
+
+    emit("close");
+  },
+});
+
+const options = {
+  title: "New Ticket",
+  size: "3xl",
 };
 </script>
