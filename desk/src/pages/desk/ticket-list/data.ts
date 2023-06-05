edd3783953e48410c931291d3fda3e88a99eb41d
Sabu Siyad <hello@ssiyad.com>
2023-06-05 16:00:24 +0530

    fix(ux): filters not getting applied

--- desk/src/pages/desk/ticket-list/data.ts
+++ desk/src/pages/desk/ticket-list/data.ts
@@ -3,53 +3,53 @@ import { useKeymapStore } from "@/stores/keymap";
 import { createListManager } from "@/composables/listManager";
 
 export const useTicketListStore = defineStore("ticketList", () => {
-	const KEYMAPS = [
-		{
-			button: "R",
-			status: "Replied",
-		},
-		{
-			button: "E",
-			status: "Resolved",
-		},
-		{
-			button: "C",
-			status: "Closed",
-		},
-	];
-	const KEYMAP_PREFIX = "Control";
-	const keymapStore = useKeymapStore();
+  const KEYMAPS = [
+    {
+      button: "R",
+      status: "Replied",
+    },
+    {
+      button: "E",
+      status: "Resolved",
+    },
+    {
+      button: "C",
+      status: "Closed",
+    },
+  ];
+  const KEYMAP_PREFIX = "Control";
+  const keymapStore = useKeymapStore();
 
-	function init() {
-		KEYMAPS.forEach((o) => {
-			keymapStore.add(
-				[KEYMAP_PREFIX, o.button],
-				() => {
-					selected.value.forEach((ticketId) => {
-						tickets.setValue.submit({
-							name: ticketId,
-							status: o.status,
-						});
-					});
-				},
-				`Set ticket as ${o.status.toLowerCase()}`
-			);
-		});
-	}
+  function init() {
+    KEYMAPS.forEach((o) => {
+      keymapStore.add(
+        [KEYMAP_PREFIX, o.button],
+        () => {
+          selected.value.forEach((ticketId) => {
+            tickets.setValue.submit({
+              name: ticketId,
+              status: o.status,
+            });
+          });
+        },
+        `Set ticket as ${o.status.toLowerCase()}`
+      );
+    });
+  }
 
-	function deinit() {
-		KEYMAPS.forEach((o) => keymapStore.remove([KEYMAP_PREFIX, o.button]));
-	}
+  function deinit() {
+    KEYMAPS.forEach((o) => keymapStore.remove([KEYMAP_PREFIX, o.button]));
+  }
 
-	const tickets = createListManager({
-		doctype: "HD Ticket",
-		pageLength: 20,
-		auto: true,
-	});
+  const tickets = createListManager({
+    doctype: "HD Ticket",
+    pageLength: 20,
+    auto: true,
+  });
 
-	return {
-		init,
-		deinit,
-		tickets,
-	};
+  return {
+    init,
+    deinit,
+    tickets,
+  };
 });
