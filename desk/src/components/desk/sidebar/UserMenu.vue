e741887a38919cd642dd98bbe3dbdde54e9927a5
Sabu Siyad <hello@ssiyad.com>
2023-06-08 19:53:21 +0530

    feat(ui): sidebar: adjust font size
    Reduce font size, item height and make name bold

--- desk/src/components/desk/sidebar/UserMenu.vue
+++ desk/src/components/desk/sidebar/UserMenu.vue
@@ -1,36 +1,36 @@
 <template>
-	<Dropdown :options="options">
-		<template #default="{ open }">
-			<div
-				class="flex w-max cursor-pointer items-center gap-2 rounded-md px-1.5 py-2 transition-all"
-				:class="{
-					'hover:bg-gray-100': sidebarStore.isExpanded,
-				}"
-			>
-				<Avatar
-					size="sm"
-					:label="authStore.userName"
-					:image-u-r-l="authStore.userImage"
-				/>
-				<div
-					class="flex shrink-0 items-center gap-1 text-gray-800 duration-300"
-					:class="{
-						'opacity-100': sidebarStore.isExpanded,
-						'opacity-0': !sidebarStore.isExpanded,
-						'-z-50': !sidebarStore.isExpanded,
-					}"
-				>
-					<div>
-						{{ authStore.userName }}
-					</div>
-					<FeatherIcon
-						:name="open ? 'chevron-up' : 'chevron-down'"
-						class="h-4 w-4 text-gray-600"
-					/>
-				</div>
-			</div>
-		</template>
-	</Dropdown>
+  <Dropdown :options="options">
+    <template #default="{ open }">
+      <div
+        class="flex w-max cursor-pointer items-center gap-2 rounded-md px-1.5 py-2 transition-all"
+        :class="{
+          'hover:bg-gray-100': sidebarStore.isExpanded,
+        }"
+      >
+        <Avatar
+          size="sm"
+          :label="authStore.userName"
+          :image-u-r-l="authStore.userImage"
+        />
+        <div
+          class="flex shrink-0 items-center gap-1 text-gray-800 duration-300"
+          :class="{
+            'opacity-100': sidebarStore.isExpanded,
+            'opacity-0': !sidebarStore.isExpanded,
+            '-z-50': !sidebarStore.isExpanded,
+          }"
+        >
+          <div class="text-sm font-semibold">
+            {{ authStore.userName }}
+          </div>
+          <FeatherIcon
+            :name="open ? 'chevron-up' : 'chevron-down'"
+            class="h-4 w-4 text-gray-600"
+          />
+        </div>
+      </div>
+    </template>
+  </Dropdown>
 </template>
 
 <script setup lang="ts">
@@ -39,10 +39,10 @@ import { useAuthStore } from "@/stores/auth";
 import { useSidebarStore } from "@/stores/sidebar";
 
 defineProps({
-	options: {
-		type: Array,
-		required: true,
-	},
+  options: {
+    type: Array,
+    required: true,
+  },
 });
 
 const authStore = useAuthStore();
