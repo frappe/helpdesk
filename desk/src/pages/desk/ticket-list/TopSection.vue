6c09e2a74e2e27655086d229ac2ac39de9fe0817
Sabu Siyad <hello@ssiyad.com>
2023-06-13 23:34:13 +0530

    fix(ui): page title padding

--- desk/src/pages/desk/ticket-list/TopSection.vue
+++ desk/src/pages/desk/ticket-list/TopSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between px-6 py-3">
+  <div class="flex justify-between px-6 pb-3">
     <div class="flex gap-2">
       <PresetFilters doctype="HD Ticket" />
       <Dropdown
