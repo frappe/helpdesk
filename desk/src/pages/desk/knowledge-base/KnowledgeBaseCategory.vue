eb560666d47c7ac3bd0570fe1e1ac9b2e9bbcca6
Sabu Siyad <hello@ssiyad.com>
2023-07-13 11:27:43 +0530

    feat: new sub category

--- desk/src/pages/desk/knowledge-base/KnowledgeBaseCategory.vue
+++ desk/src/pages/desk/knowledge-base/KnowledgeBaseCategory.vue
@@ -9,9 +9,16 @@
           <Button label="Edit" theme="gray" variant="outline"
             ><template #prefix><IconEdit class="h-4 w-4" /></template
           ></Button>
-          <Button label="Add new" theme="gray" variant="solid"
-            ><template #prefix><IconPlus class="h-4 w-4" /></template
-          ></Button>
+          <Button
+            label="Add new"
+            theme="gray"
+            variant="solid"
+            @click="showNewSubCategory = !showNewSubCategory"
+          >
+            <template #prefix>
+              <IconPlus class="h-4 w-4" />
+            </template>
+          </Button>
         </div>
       </div>
       <div
@@ -27,25 +34,101 @@
       <KnowledgeBaseCategoryCard
         v-for="c in subCategories.data"
         :key="c.name"
-        class="place-self-center"
+        class="w-full place-self-center"
         :title="c.category_name"
         :description="c.description"
         :article-count="c.count_article"
       />
     </div>
+    <Dialog
+      v-model="showNewSubCategory"
+      :options="{ title: 'New Sub category' }"
+    >
+      <template #body-content>
+        <form @submit.prevent="newSubCategory.submit">
+          <div class="space-y-4">
+            <FormControl
+              v-model="newSubCategoryName"
+              type="text"
+              label="Name"
+              placeholder="Name"
+            />
+            <FormControl
+              v-model="newSubCategoryDescription"
+              type="textarea"
+              label="Description"
+              placeholder="Description"
+            />
+            <Button
+              :disabled="isEmpty(newSubCategoryName)"
+              class="w-full"
+              label="Create"
+              theme="gray"
+              variant="solid"
+            />
+          </div>
+        </form>
+      </template>
+    </Dialog>
   </div>
 </template>
 <script setup lang="ts">
-import { createDocumentResource, Button } from "frappe-ui";
+import { ref } from "vue";
+import {
+  createResource,
+  createDocumentResource,
+  Button,
+  Dialog,
+  FormControl,
+} from "frappe-ui";
+import { isEmpty } from "lodash";
+import { createToast } from "@/utils/toasts";
 import { createListManager } from "@/composables/listManager";
 import KnowledgeBaseCategoryCard from "./KnowledgeBaseCategoryCard.vue";
 import IconEdit from "~icons/lucide/edit-3";
 import IconPlus from "~icons/lucide/plus";
 
+const newSubCategoryName = ref("");
+const newSubCategoryDescription = ref("");
+const showNewSubCategory = ref(false);
+
 const category = createDocumentResource({
   doctype: "HD Article Category",
   name: "dd01268bcc",
   auto: true,
+});
+
+const newSubCategory = createResource({
+  url: "frappe.client.insert",
+  debounce: 500,
+  makeParams() {
+    return {
+      doc: {
+        doctype: "HD Article Category",
+        category_name: newSubCategoryName.value,
+        description: newSubCategoryDescription.value,
+        parent_category: category.doc?.name,
+      },
+    };
+  },
+  validate(params) {
+    if (isEmpty(params.doc.category_name)) {
+      return "Category name is required";
+    }
+  },
+  onSuccess() {
+    showNewSubCategory.value = false;
+    subCategories.reload();
+  },
+  onError(error) {
+    const msg = error.message ? error.message : error.messages.join(", ");
+    createToast({
+      title: "Error creating sub category",
+      text: msg,
+      icon: "x",
+      iconClasses: "text-red-500",
+    });
+  },
 });
 
 const subCategories = createListManager({
