a0f4ff18a7bf1ac1f8d0081253aa9af2d7b48289
Sabu Siyad <hello@ssiyad.com>
2023-07-14 14:51:06 +0530

    fix(patch): add fallback ticket type (#1312)

--- helpdesk/helpdesk/doctype/hd_ticket/patches/fallback_ticket_type.py
+++ helpdesk/helpdesk/doctype/hd_ticket/patches/fallback_ticket_type.py
@@ -4,6 +4,20 @@ from helpdesk.consts import FALLBACK_TICKET_TYPE
 
 
 def execute():
+	add_fallback()
+	set_ticket_type()
+
+
+def add_fallback():
+	if frappe.db.exists("HD Ticket Type", FALLBACK_TICKET_TYPE):
+		return
+	d = frappe.new_doc("HD Ticket Type")
+	d.is_system = True
+	d.name = FALLBACK_TICKET_TYPE
+	d.save()
+
+
+def set_ticket_type():
 	QBTicket = frappe.qb.DocType("HD Ticket")
 	(
 		frappe.qb.update(QBTicket)
