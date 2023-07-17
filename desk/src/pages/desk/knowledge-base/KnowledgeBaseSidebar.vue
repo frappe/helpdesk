c2010d247bce816655cfa6c60dc8b9ad49d564cd
Sabu Siyad <hello@ssiyad.com>
2023-07-17 20:26:01 +0530

    feat: add category

--- desk/src/pages/desk/knowledge-base/KnowledgeBaseSidebar.vue
+++ desk/src/pages/desk/knowledge-base/KnowledgeBaseSidebar.vue
@@ -8,9 +8,13 @@
   >
     <div class="flex items-center justify-between">
       <div class="text-sm font-medium text-gray-600">Categories</div>
-      <Button theme="gray" variant="ghost">
+      <Button
+        theme="gray"
+        variant="ghost"
+        @click="showNewDialog = !showNewDialog"
+      >
         <template #icon>
-          <IconMoreHorizontal class="h-4 w-4" />
+          <Icon icon="lucide:plus" class="h-4 w-4" />
         </template>
       </Button>
     </div>
@@ -24,16 +28,84 @@
         @click="activeCategory = category.name"
       />
     </div>
+    <Dialog v-model="showNewDialog" :options="dialogOptions">
+      <template #body-content>
+        <form @submit.prevent="newCategoryRes.submit">
+          <div class="space-y-4">
+            <div class="space-y-2">
+              <div class="text-xs text-gray-700">Title</div>
+              <div class="flex items-center gap-2">
+                <Popover>
+                  <template #target="{ togglePopover }">
+                    <Button @click="togglePopover">
+                      <template #icon>
+                        <component :is="getIcon(newCategoryIcon)" />
+                      </template>
+                    </Button>
+                  </template>
+                  <template #body-main="{ togglePopover }">
+                    <div class="grid grid-cols-5 gap-2 p-2">
+                      <Button
+                        v-for="icon in icons"
+                        :key="icon"
+                        class="place-self-center"
+                        @click="
+                          () => {
+                            newCategoryIcon = icon;
+                            togglePopover();
+                          }
+                        "
+                      >
+                        <template #icon>
+                          <component :is="getIcon(icon)" />
+                        </template>
+                      </Button>
+                    </div>
+                  </template>
+                </Popover>
+                <FormControl
+                  v-model="newCategoryName"
+                  placeholder="A brief guide"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div class="space-y-2">
+              <div class="text-xs text-gray-700">Description</div>
+              <FormControl
+                v-model="newCategoryDescription"
+                placeholder="A short description"
+                type="textarea"
+              />
+            </div>
+            <Button
+              class="w-full"
+              label="Create"
+              theme="gray"
+              variant="solid"
+            />
+          </div>
+        </form>
+      </template>
+    </Dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { createListResource, Button } from "frappe-ui";
+import { ref } from "vue";
+import {
+  createResource,
+  createListResource,
+  Button,
+  Dialog,
+  FormControl,
+  Popover,
+} from "frappe-ui";
 import { storeToRefs } from "pinia";
+import { Icon } from "@iconify/vue";
 import SidebarLink from "@/components/SidebarLink.vue";
-import { useKnowledgeBaseStore } from "./data";
+import { useKnowledgeBaseStore, icons } from "./data";
 import { getIcon } from "./util";
-import IconMoreHorizontal from "~icons/lucide/more-horizontal";
 
 const { activeCategory } = storeToRefs(useKnowledgeBaseStore());
 const categories = createListResource({
@@ -44,4 +116,35 @@ const categories = createListResource({
     parent_category: "",
   },
 });
+const newCategoryName = ref("");
+const newCategoryDescription = ref("");
+const newCategoryIcon = ref("");
+const showNewDialog = ref(false);
+const newCategoryRes = createResource({
+  url: "frappe.client.insert",
+  makeParams() {
+    return {
+      doc: {
+        doctype: "HD Article Category",
+        category_name: newCategoryName.value,
+        description: newCategoryDescription.value,
+        icon: newCategoryIcon.value,
+      },
+    };
+  },
+  validate(params) {
+    const requiredFields = ["category_name", "description", "icon"];
+    for (const f of requiredFields) {
+      if (params.doc[f]) return;
+      const field = f.replace("_", " ").toUpperCase();
+      return `${field} is required`;
+    }
+  },
+  onSuccess(data) {
+    categories.reload().then(() => (activeCategory.value = data.name));
+  },
+});
+const dialogOptions = {
+  title: "New category",
+};
 </script>
--- desk/src/pages/desk/knowledge-base/util.ts
+++ desk/src/pages/desk/knowledge-base/util.ts
@@ -1,7 +1,7 @@
 import { h } from "vue";
 import { Icon } from "@iconify/vue";
 
-export function getIcon(icon: string) {
+export function getIcon(icon?: string) {
   const i = icon || "box";
   const l = `lucide:${i}`;
   return h(Icon, { icon: l });
