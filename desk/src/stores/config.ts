9bb5886831705ef85793dc4a56d5eaba38fdba00
Sabu Siyad <hello@ssiyad.com>
2023-07-25 11:57:06 +0530

    feat: switch to prefer knowledge base (#1340)
    If enabled,
    - redirect to knowledge base
    - hide `new ticket` button

--- desk/src/stores/config.ts
+++ desk/src/stores/config.ts
@@ -9,45 +9,49 @@ const URI_CONFIG = "helpdesk.api.config.get_config";
 const DEFAULT_TITLE = "Helpdesk";
 
 export const useConfigStore = defineStore("config", () => {
-	const configRes = createResource({
-		url: URI_CONFIG,
-		auto: true,
-	});
+  const configRes = createResource({
+    url: URI_CONFIG,
+    auto: true,
+  });
 
-	const config = computed(() => configRes.data || {});
-	const brandLogo = computed(() => config.value.brand_logo);
-	const brandFavicon = computed(() => config.value.brand_favicon);
-	const helpdeskName: ComputedRef<string> = computed(
-		() => config.value.helpdesk_name || DEFAULT_TITLE
-	);
-	const isSetupComplete: ComputedRef<boolean> = computed(
-		() => config.value.is_setup_complete
-	);
-	const skipEmailWorkflow: ComputedRef<string> = computed(
-		() => config.value.skip_email_workflow
-	);
-	const pageTitle = ref(null);
-	const windowTitle = computed(() =>
-		pageTitle.value
-			? `${pageTitle.value} • ${helpdeskName.value}`
-			: helpdeskName.value
-	);
+  const config = computed(() => configRes.data || {});
+  const brandLogo = computed(() => config.value.brand_logo);
+  const brandFavicon = computed(() => config.value.brand_favicon);
+  const helpdeskName: ComputedRef<string> = computed(
+    () => config.value.helpdesk_name || DEFAULT_TITLE
+  );
+  const isSetupComplete: ComputedRef<boolean> = computed(
+    () => !!parseInt(config.value.setup_complete)
+  );
+  const skipEmailWorkflow: ComputedRef<boolean> = computed(
+    () => !!parseInt(config.value.skip_email_workflow)
+  );
+  const preferKnowledgeBase = computed(
+    () => !!parseInt(config.value.prefer_knowledge_base)
+  );
+  const pageTitle = ref(null);
+  const windowTitle = computed(() =>
+    pageTitle.value
+      ? `${pageTitle.value} • ${helpdeskName.value}`
+      : helpdeskName.value
+  );
 
-	function setTitle(title?: string) {
-		pageTitle.value = title ? title : null;
-	}
+  function setTitle(title?: string) {
+    pageTitle.value = title ? title : null;
+  }
 
-	useFavicon(brandFavicon);
-	useTitle(windowTitle);
+  useFavicon(brandFavicon);
+  useTitle(windowTitle);
 
-	socket.on("helpdesk:settings-updated", () => configRes.reload());
+  socket.on("helpdesk:settings-updated", () => configRes.reload());
 
-	return {
-		brandLogo,
-		config,
-		helpdeskName,
-		isSetupComplete,
-		setTitle,
-		skipEmailWorkflow,
-	};
+  return {
+    brandLogo,
+    config,
+    preferKnowledgeBase,
+    helpdeskName,
+    isSetupComplete,
+    setTitle,
+    skipEmailWorkflow,
+  };
 });
