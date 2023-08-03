908b972c95f5a73a5f5f33f6549cdc09358da2d5
Sabu Siyad <hello@ssiyad.com>
2023-08-04 02:47:22 +0530

    feat: template: prevent `Default` from deleting

--- helpdesk/helpdesk/doctype/hd_ticket_template/hd_ticket_template.py
+++ helpdesk/helpdesk/doctype/hd_ticket_template/hd_ticket_template.py
@@ -10,6 +10,9 @@ class HDTicketTemplate(Document):
 	def validate(self):
 		self.verify_field_exists()
 
+	def on_trash(self):
+		self.prevent_default_delete()
+
 	def verify_field_exists(self):
 		for f in self.fields:
 			docfield_exits = frappe.db.exists(
@@ -21,3 +24,8 @@ class HDTicketTemplate(Document):
 			if not docfield_exits and not custom_field_exists:
 				text = _("Field `{0}` does not exist in Ticket").format(f.fieldname)
 				frappe.throw(text, frappe.DoesNotExistError)
+
+	def prevent_default_delete(self):
+		if self.name == "Default":
+			text = _("Default template can not be deleted")
+			frappe.throw(text, frappe.PermissionError)
