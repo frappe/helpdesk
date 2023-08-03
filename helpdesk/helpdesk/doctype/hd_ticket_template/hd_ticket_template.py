5c78785077a1d9f18b18051c7c07a496e7575c16
Sabu Siyad <hello@ssiyad.com>
2023-08-04 01:46:38 +0530

    feat: template: verify if fields exist

--- helpdesk/helpdesk/doctype/hd_ticket_template/hd_ticket_template.py
+++ helpdesk/helpdesk/doctype/hd_ticket_template/hd_ticket_template.py
@@ -2,25 +2,22 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
-from frappe.website.utils import cleanup_page_name
 
 
 class HDTicketTemplate(Document):
-	# def validate(self):
-	# 	allowed_field_types = [
-	# 		"Link",
-	# 		"Select",
-	# 	]
-	#
-	# 	for field in self.fields:
-	# 		if field.fieldtype not in allowed_field_types:
-	# 			frappe.throw(
-	# 				f"Type {field.fieldtype} not allowed, should be in {allowed_field_types}"
-	# 			)
-	# 		if not field.fieldname:
-	# 			field.fieldname = cleanup_page_name(field.label)
-	#
-	# 		if field.fieldname == "description" and field.fieldtype != "Text Editor":
-	# 			frappe.throw("field type for description field should be Text Editor")
-	pass
+	def validate(self):
+		self.verify_field_exists()
+
+	def verify_field_exists(self):
+		for f in self.fields:
+			docfield_exits = frappe.db.exists(
+				{"doctype": "DocField", "fieldname": f.fieldname, "parent": "HD Ticket"}
+			)
+			custom_field_exists = frappe.db.exists(
+				{"doctype": "Custom Field", "fieldname": f.fieldname, "dt": "HD Ticket"}
+			)
+			if not docfield_exits and not custom_field_exists:
+				text = _(f"Field `{f.fieldname}` does not exist in Ticket")
+				frappe.throw(text, frappe.DoesNotExistError)
