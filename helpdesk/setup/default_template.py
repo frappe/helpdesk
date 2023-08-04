986f17a23dc6719e04238e055e0923bd8b852601
Sabu Siyad <hello@ssiyad.com>
2023-08-04 13:36:41 +0530

    fix: setup: creating default template

--- helpdesk/setup/default_template.py
+++ helpdesk/setup/default_template.py
@@ -4,19 +4,8 @@ from helpdesk.consts import DEFAULT_TICKET_TEMPLATE
 
 
 def create_default_template():
-	if frappe.db.exists("HD Ticket Template", DEFAULT_TICKET_TEMPLATE):
-		return
-
-	template = frappe.new_doc("HD Ticket Template")
-	template.template_name = DEFAULT_TICKET_TEMPLATE
-	template.append(
-		"fields",
-		{
-			"label": "Type",
-			"fieldname": "ticket_type",
-			"fieldtype": "Link",
-			"options": "HD Ticket Type",
-			"reqd": False,
-		},
-	)
-	template.insert()
+	doc = {
+		"doctype": "HD Ticket Template",
+		"template_name": DEFAULT_TICKET_TEMPLATE,
+	}
+	frappe.get_doc(doc).save()
