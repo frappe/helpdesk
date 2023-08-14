2da50dd03ef2551fa0ffac7657961fc412f3c0bc
Sabu Siyad <hello@ssiyad.com>
2023-08-14 12:30:46 +0530

    chore(ui): dashboard: title

--- desk/src/pages/desk/DeskDashboard.vue
+++ desk/src/pages/desk/DeskDashboard.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="flex flex-col divide-y">
-    <PageTitle>
-      <template #title>
-        <div
-          class="flex select-none items-center text-2xl font-semibold text-gray-900"
-        >
-          Hello,
-          <div class="mx-1 text-gray-800">{{ authStore.userFirstName }}</div>
-          👋
-        </div>
-      </template>
+    <PageTitle title="Dashboard">
       <template #right>
         <Tooltip v-if="!isEmpty(items.data)" placement="left" :text="dateInfo">
           <IconInfo class="h-5 w-5 text-gray-800" />
@@ -56,7 +47,6 @@
 
 <script setup lang="ts">
 import { createResource, Tooltip } from "frappe-ui";
-import { useAuthStore } from "@/stores/auth";
 import { isEmpty } from "lodash";
 import PageTitle from "@/components/PageTitle.vue";
 import LineChart from "@/components/charts/LineChart.vue";
@@ -64,7 +54,6 @@ import PieChart from "@/components/charts/PieChart.vue";
 import SingleString from "@/components/charts/SingleString.vue";
 import IconInfo from "~icons/espresso/alert-circle";
 
-const authStore = useAuthStore();
 const items = createResource({
   url: "helpdesk.api.dashboard.get_all",
   auto: true,
