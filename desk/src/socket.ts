c191255e919ac92af4bd95252aa2490d918e56ea
Sabu Siyad <hello@ssiyad.com>
2023-08-21 23:00:43 +0530

    fix: socket: use site name as namespace

--- desk/src/socket.ts
+++ desk/src/socket.ts
@@ -5,7 +5,9 @@ function initSocket() {
   const host = window.location.hostname;
   const port = window.location.port ? `:${socketio_port}` : "";
   const protocol = port ? "http" : "https";
-  const url = `${protocol}://${host}${port}/${host}`;
+  const siteName = window["site_name"];
+  const namespace = !siteName?.startsWith("{{") ? siteName : host;
+  const url = `${protocol}://${host}${port}/${namespace}`;
   const socket = io(url, { withCredentials: true });
   return socket;
 }
--- helpdesk/www/helpdesk/index.py
+++ helpdesk/www/helpdesk/index.py
@@ -1,9 +1,13 @@
 import frappe
 
+from helpdesk import __version__
+
 no_cache = 1
 
 
 def get_context(context):
-	csrf_token = frappe.sessions.get_csrf_token()
+	context.csrf_token = frappe.sessions.get_csrf_token()
+	context.frappe_version = frappe.__version__
+	context.helpdesk_version = __version__
+	context.site_name = frappe.local.site
 	frappe.db.commit()
-	context.csrf_token = csrf_token
