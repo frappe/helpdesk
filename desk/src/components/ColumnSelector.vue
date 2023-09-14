57b23e716331a632f65f8780b1a61c7893d3dac3
Sabu Siyad <hello@ssiyad.com>
2023-09-14 11:03:21 +0530

    fix(ui): column selector: use `ring`

--- desk/src/components/ColumnSelector.vue
+++ desk/src/components/ColumnSelector.vue
@@ -13,7 +13,9 @@
       </Button>
     </template>
     <template #body>
-      <div class="mt-2 divide-y rounded bg-white p-1 shadow-2xl">
+      <div
+        class="mt-2 divide-y rounded bg-white p-1 shadow-2xl ring-1 ring-black ring-opacity-5"
+      >
         <Switch
           v-for="c in columns"
           :key="c.key"
