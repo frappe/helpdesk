9999df4f2dfd0fc40a73f288509018eff52c17dd
Sabu Siyad <hello@ssiyad.com>
2023-10-02 16:41:12 +0530

    chore: ticket details: add `id`

--- desk/src/pages/ticket/TicketDetails.vue
+++ desk/src/pages/ticket/TicketDetails.vue
@@ -4,6 +4,12 @@
       <span>
         <TicketSidebarHeader title="Details" />
         <div class="mx-5 my-6 flex flex-col justify-between gap-3.5 text-base">
+          <div class="space-y-1.5">
+            <span class="block text-sm text-gray-700">ID</span>
+            <span class="block break-words font-medium text-gray-900">
+              {{ data.name }}
+            </span>
+          </div>
           <div v-if="data.customer" class="space-y-1.5">
             <span class="block text-sm text-gray-700">Customer</span>
             <span class="block break-words font-medium text-gray-900">
