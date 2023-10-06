51f466c1c9998daecc7531085c3e5f78b0582808
Sabu Siyad <hello@ssiyad.com>
2023-10-06 18:42:23 +0530

    feat: use `CommandPalette` from `frappe-ui`

--- desk/src/App.vue
+++ desk/src/App.vue
@@ -3,17 +3,25 @@
     <RouterView class="antialiased" />
     <Toasts />
     <KeymapDialog />
+    <CommandPalette
+      v-model:show="commandPalette.show"
+      :search-query="commandPalette.query"
+      :groups="commandPalette.result"
+      @select="({ action }) => action.call() && commandPalette.close()"
+    />
   </span>
 </template>
 
 <script setup lang="ts">
 import { provide, ref, onMounted } from "vue";
-import { Toasts } from "frappe-ui";
+import { CommandPalette, Toasts } from "frappe-ui";
 import { createToast } from "@/utils";
 import { useConfigStore } from "@/stores/config";
+import { useCommandPalette } from "@/composables";
 import KeymapDialog from "@/pages/KeymapDialog.vue";
 
 useConfigStore();
+const commandPalette = useCommandPalette();
 
 const viewportWidth = ref(
   Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
