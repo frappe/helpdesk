5942bb609853e76782c39d12622cf41566792a99
Sabu Siyad <hello@ssiyad.com>
2023-10-11 16:38:34 +0530

    chore: use `CommandPalette` from gameplan

--- desk/src/App.vue
+++ desk/src/App.vue
@@ -3,25 +3,19 @@
     <RouterView class="antialiased" />
     <Toasts />
     <KeymapDialog />
-    <CommandPalette
-      v-model:show="commandPalette.show"
-      :search-query="commandPalette.query"
-      :groups="commandPalette.result"
-      @select="({ action }) => action.call() && commandPalette.close()"
-    />
+    <CommandPalette />
   </span>
 </template>
 
 <script setup lang="ts">
 import { provide, ref, onMounted } from "vue";
-import { CommandPalette, Toasts } from "frappe-ui";
+import { Toasts } from "frappe-ui";
 import { createToast } from "@/utils";
 import { useConfigStore } from "@/stores/config";
-import { useCommandPalette } from "@/composables";
+import { CommandPalette } from "@/components";
 import KeymapDialog from "@/pages/KeymapDialog.vue";
 
 useConfigStore();
-const commandPalette = useCommandPalette();
 
 const viewportWidth = ref(
   Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
