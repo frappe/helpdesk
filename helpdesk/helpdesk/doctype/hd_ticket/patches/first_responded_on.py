e96c7886e5bad614e48ef40e24674c3ac8fb4bbd
Ankush Menat <ankush@frappe.io>
2023-10-23 18:26:02 +0530

    fix: fetch value directly and avoid update where applicable

--- helpdesk/helpdesk/doctype/hd_ticket/patches/first_responded_on.py
+++ helpdesk/helpdesk/doctype/hd_ticket/patches/first_responded_on.py
@@ -2,22 +2,26 @@ import frappe
 
 
 def execute():
-	for t in frappe.get_all("HD Ticket"):
-		try:
-			c = frappe.get_last_doc(
-				"Communication",
-				filters={
-					"reference_doctype": "HD Ticket",
-					"reference_name": t.name,
-					"sent_or_received": "Sent",
-				},
-				order_by="creation asc",
-			)
-		except frappe.DoesNotExistError:
+	for ticket in frappe.get_all("HD Ticket", ["name", "first_responded_on"]):
+		first_communication_creation = frappe.db.get_value(
+			"Communication",
+			filters={
+				"reference_doctype": "HD Ticket",
+				"reference_name": ticket.name,
+				"sent_or_received": "Sent",
+			},
+			order_by="creation asc",
+		)
+		if (
+			not first_communication_creation
+			or first_communication_creation >= ticket.first_responded_on
+		):
+			# Already set correctly, or no communication found.
 			continue
-		ticket = frappe.get_doc("HD Ticket", t.name)
+
+		ticket = frappe.get_doc("HD Ticket", ticket.name)
 		modified = ticket.modified
-		ticket.first_responded_on = c.creation
+		ticket.first_responded_on = first_communication_creation
 		ticket.save()
 		ticket.db_set("modified", modified, update_modified=False)
 		frappe.db.commit()
