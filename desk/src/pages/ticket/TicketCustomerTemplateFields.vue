b0c9358491fb338bc1dd3f01ccefe3a784f5c941
rutwik <rutwikhdev@gmail.com>
2023-12-20 18:28:37 +0530

    fix(ui): Add gaps between grid

--- desk/src/pages/ticket/TicketCustomerTemplateFields.vue
+++ desk/src/pages/ticket/TicketCustomerTemplateFields.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="grid grid-cols-1 border-b px-5 py-2.5 sm:grid-cols-3">
+  <span class="grid grid-cols-1 gap-4 border-b px-5 py-2.5 sm:grid-cols-3">
     <div class="space-y-1.5">
       <span class="block text-sm text-gray-700"> Status </span>
       <span class="block break-words text-base font-medium text-gray-900">
