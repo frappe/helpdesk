2fe6d5a2fbde4d4dd3e3b8031fb3469269e07429
safvan ph <safvanph41@gmail.com>
2024-03-25 10:44:01 +0530

    feat: add missing cc in ticketagent2

--- desk/src/pages/ticket/TicketAgent2.vue
+++ desk/src/pages/ticket/TicketAgent2.vue
@@ -102,6 +102,33 @@
                       "
                     />
                 </div>
+                <div v-if="showCc">
+                  <span class="inline-flex flex-wrap items-center gap-1">
+                    <span class="mr-2 text-xs text-gray-500">CC:</span>
+                    <Button
+                      v-for="i in cc.split(',').filter(Boolean)"
+                      :key="i"
+                      :label="i"
+                      @click="
+                        () =>
+                          (cc = cc
+                            .split(',')
+                            .filter((s) => s !== i)
+                            .join(','))
+                      "
+                    />
+                    <FormControl
+                      type="text"
+                      placeholder="hello@example.com"
+                      @keyup.prevent.enter="
+                        (event) => {
+                          cc = [...cc.split(','), event.target.value].join(',');
+                          event.target.value = '';
+                        }
+                      "
+                    />
+                  </span>
+                </div>
                 <div v-if="showBcc">
                   <span class="inline-flex flex-wrap items-center gap-1">
                     <span class="mr-2 text-xs text-gray-500">BCC:</span>
