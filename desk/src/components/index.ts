51d48f3b1a76f4223b46437d6e50bc0ab0c1b621
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-04-02 22:02:31 +0530

    chore: remove deprecated file

--- desk/src/components/index.ts
+++ desk/src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AttachmentItem } from "./AttachmentItem.vue";
 export { default as BreadCrumbs } from "./BreadCrumbs.vue";
 export { default as CommandPalette } from "./command-palette/CP.vue";
-export { default as ColumnSelector } from "./ColumnSelector.vue";
 export { default as HCard } from "./HCard.vue";
 export { default as ListView } from "./list-view/LV.vue";
 export { default as NestedPopover } from "./NestedPopover.vue";
