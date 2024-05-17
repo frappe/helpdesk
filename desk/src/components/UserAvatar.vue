8919a2f9e34b247f332a4cc3081eacef599f06d4
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-05-17 17:54:24 +0530

    fix: user image typo

--- desk/src/components/UserAvatar.vue
+++ desk/src/components/UserAvatar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center gap-2">
-    <Avatar :label="user.name" :image="user.image" v-bind="$attrs" />
+    <Avatar :label="user.name" :image="user.user_image" v-bind="$attrs" />
   </div>
 </template>
 <script setup lang="ts">
