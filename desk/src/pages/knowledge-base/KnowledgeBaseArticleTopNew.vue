92806c0fb2166dce204bd65af5e927322da93799
Sumit Bhanushali <sumit.bhanushali63@gmail.com>
2024-07-19 16:05:03 +0530

    fix(KB): article breadcrumbs fixes

--- desk/src/pages/knowledge-base/KnowledgeBaseArticleTopNew.vue
+++ desk/src/pages/knowledge-base/KnowledgeBaseArticleTopNew.vue
@@ -1,13 +1,4 @@
 <template>
-  <div class="mb-7 flex w-max items-center gap-1 rounded bg-gray-100 px-2 py-1">
-    <div class="text-base text-gray-600">
-      {{ categoryName }}
-    </div>
-    <IconChevronRight class="h-3 w-3 text-gray-600" />
-    <div class="text-base text-gray-800">
-      {{ subCategoryName }}
-    </div>
-  </div>
   <div class="mb-4.5 flex items-center justify-between">
     <div class="flex items-center gap-2">
       <Avatar :label="authorFullname" :image="authorImage" />
@@ -27,11 +18,8 @@
 
 <script setup lang="ts">
 import { Avatar, FormControl } from "frappe-ui";
-import IconChevronRight from "~icons/lucide/chevron-right";
 
 interface P {
-  categoryName: string;
-  subCategoryName: string;
   title: string;
   authorFullname: string;
   authorImage: string;
