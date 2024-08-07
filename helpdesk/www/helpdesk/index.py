80bce0a9d8a955f81b01b6fc08a69ebb711b1334
RitvikSardana <ritvik.sardana11@gmail.com>
2024-08-07 19:27:01 +0530

    fix(formatting): add pre commit and format files

--- helpdesk/www/helpdesk/index.py
+++ helpdesk/www/helpdesk/index.py
@@ -1,7 +1,9 @@
 import frappe
-from helpdesk import __version__
 from frappe import _
 from frappe.utils.telemetry import capture
+
+from helpdesk import __version__
+
 no_cache = 1
 
 
@@ -17,24 +19,25 @@ def get_context(context):
 		capture("active_site", "helpdesk")
 	return context
 
+
 @frappe.whitelist(methods=["POST"], allow_guest=True)
 def get_context_for_dev():
 	if not frappe.conf.developer_mode:
 		frappe.throw(_("This method is only meant for developer mode"))
-		
 	return get_boot()
 
+
 def get_boot():
-    return frappe._dict(
-        {
-            "frappe_version": frappe.__version__,
-            "default_route": get_default_route(),
-            "site_name": frappe.local.site,
-            "read_only_mode": frappe.flags.read_only,
-			"csrf_token": frappe.sessions.get_csrf_token()
-        }
-    )
+	return frappe._dict(
+		{
+			"frappe_version": frappe.__version__,
+			"default_route": get_default_route(),
+			"site_name": frappe.local.site,
+			"read_only_mode": frappe.flags.read_only,
+			"csrf_token": frappe.sessions.get_csrf_token(),
+		}
+	)
+
 
 def get_default_route():
-    return "/helpdesk"
-
+	return "/helpdesk"
