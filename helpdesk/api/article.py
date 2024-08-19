ab22c081f228accbcc55e7dc51a361a32a5d7188
Balamurali M <balamurali@frappe.io>
2024-08-19 15:44:38 +0530

    chore(HelpdeskSearch): Remove breakpoint

--- helpdesk/api/article.py
+++ helpdesk/api/article.py
@@ -6,7 +6,6 @@ from helpdesk.search import search as hd_search
 @frappe.whitelist()
 def search(query: str):
 	out = hd_search(query, only_articles=True)
-	breakpoint()
 	if not out:
 		return []
 	return out[0].get("items", [])
