a69dd18c00ee077a3f003b8430b939fe07c4ae19
RitvikSardana <ritvik.sardana11@gmail.com>
2024-09-02 13:04:47 +0530

    fix(styles): mobile ticket agent component

--- desk/src/pages/MobileTicketAgent.vue
+++ desk/src/pages/MobileTicketAgent.vue
@@ -27,7 +27,7 @@
       </template>
     </LayoutHeader>
     <header
-      class="flex h-10.5 items-center justify-between py-[7px] px-3 border-b"
+      class="flex h-12 items-center justify-between py-[7px] px-3 border-b"
       v-if="ticket.data"
     >
       <!-- left side -->
