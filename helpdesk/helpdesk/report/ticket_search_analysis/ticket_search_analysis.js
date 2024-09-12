304476d2ffa4ad91e7fcf7c01aca8a40f5bf2b31
Balamurali M <balamurali@frappe.io>
2024-09-12 17:26:19 +0530

    style: black + prettier

--- helpdesk/helpdesk/report/ticket_search_analysis/ticket_search_analysis.js
+++ helpdesk/helpdesk/report/ticket_search_analysis/ticket_search_analysis.js
@@ -2,12 +2,12 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Ticket-Search Analysis"] = {
-	filters: [
-		// {
-		// 	"fieldname": "my_filter",
-		// 	"label": __("My Filter"),
-		// 	"fieldtype": "Data",
-		// 	"reqd": 1,
-		// },
-	],
+  filters: [
+    // {
+    // 	"fieldname": "my_filter",
+    // 	"label": __("My Filter"),
+    // 	"fieldtype": "Data",
+    // 	"reqd": 1,
+    // },
+  ],
 };
