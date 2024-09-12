a566ada60d6d007bde8ae210e70aff0ec3a5fde7
Balamurali M <balamurali@frappe.io>
2024-09-12 12:32:02 +0530

    fix(HelpdeskSearch): Add more fallbacks
    - noun phrases
    - or queries (worst case)

--- helpdesk/api/article.py
+++ helpdesk/api/article.py
@@ -4,10 +4,16 @@ from textblob import TextBlob
 from helpdesk.search import search as hd_search
 
 
-def get_nouns(text: str):
-    blob = TextBlob(text)
+def get_nouns(blob: TextBlob):
     try:
         return [word for word, pos in blob.pos_tags if pos[0] == "N"]
+    except LookupError:
+        return []
+
+
+def get_noun_phrases(blob: TextBlob):
+    try:
+        return blob.noun_phrases
     except LookupError:
         return []
 
@@ -16,9 +22,19 @@ def get_nouns(text: str):
 def search(query: str):
     out = hd_search(query, only_articles=True)
     if not out:  # fallback
-        if nouns := get_nouns(query):
-            query = " ".join(nouns)
-            out = hd_search(query, only_articles=True)
+        blob = TextBlob(query)
+        if noun_phrases := get_noun_phrases(blob):
+            and_query = " ".join(noun_phrases)
+            or_query = "|".join(noun_phrases)
+            out = hd_search(and_query, only_articles=True) or hd_search(
+                or_query, only_articles=True
+            )
+        if not out and (nouns := get_nouns(blob)):
+            and_query = " ".join(nouns)
+            or_query = "|".join(nouns)
+            out = hd_search(and_query, only_articles=True) or hd_search(
+                or_query, only_articles=True
+            )
         if not out:
             return []
     return out[0].get("items", [])
