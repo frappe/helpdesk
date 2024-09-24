d2eb6f10c35a2f5055e70217d1298ff47bb78499
RitvikSardana <ritvik.sardana11@gmail.com>
2024-09-24 23:48:34 +0530

    fix(styles): activity header mobile view

--- desk/src/components/ticket/ActivityHeader.vue
+++ desk/src/components/ticket/ActivityHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="mx-10 my-8 flex items-center justify-between">
+  <div
+    class="md:mx-10 md:my-8 flex items-center justify-between text-lg font-medium mx-5 mb-4 mt-8"
+  >
     <div class="flex h-8 items-center text-xl font-semibold text-gray-800">
       {{ title }}
     </div>
