06f504538dc004a93c3999c999d3641098a5b71f
RitvikSardana <ritvik.sardana11@gmail.com>
2024-09-30 19:50:19 +0530

    fix(timeline): single ticket view

--- desk/src/pages/ticket/TicketConversation.vue
+++ desk/src/pages/ticket/TicketConversation.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="divide-y overflow-auto px-5 pb-32">
-    <div v-for="c in conversation" :id="c.name" :key="c.name" class="mt-4">
-      <TicketCommunication
-        :content="c.content"
-        :date="c.creation"
-        :user="c.user"
-        :sender-image="c.sender"
-        :cc="c.cc || ''"
-        :bcc="c.bcc || ''"
-        :attachments="c.attachments"
+  <div
+    class="md:mx-5 md:my-8 mb-4 mt-8 flex items-center justify-between h-8 text-xl font-semibold text-gray-800"
+  >
+    Activity
+  </div>
+  <div class="overflow-auto px-5 pb-20 grow">
+    <div
+      v-for="c in communications"
+      :id="c.name"
+      :key="c.name"
+      class="mt-4 flex items-between justify-center gap-4 relative"
+    >
+      <div
+        class="w-full activity grid grid-cols-[30px_minmax(auto,_1fr)] gap-2 sm:gap-4"
       >
-        <template #top-right="d">
-          <slot name="communication-top-right" v-bind="d" />
-        </template>
-      </TicketCommunication>
+        <div
+          class="relative flex justify-center after:absolute after:left-[50%] after:top-[12%] after:-z-10 after:border-l after:border-gray-200 after:h-full"
+        >
+          <Avatar
+            size="lg"
+            :label="c.user.name"
+            :image="c.user.image"
+            class="mt-1 relative"
+          />
+        </div>
+        <TicketCommunication
+          :content="c.content"
+          :date="c.creation"
+          :user="c.user"
+          :sender-image="c.sender"
+          :cc="c.cc || ''"
+          :bcc="c.bcc || ''"
+          :attachments="c.attachments"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -22,9 +42,9 @@
 import { computed, inject, nextTick, watch } from "vue";
 import { useRoute } from "vue-router";
 import { useElementVisibility } from "@vueuse/core";
+import { Avatar } from "frappe-ui";
 import { orderBy } from "lodash";
 import { dayjs } from "@/dayjs";
-import TicketComment from "./TicketComment.vue";
 import TicketCommunication from "./TicketCommunication.vue";
 import { ITicket } from "./symbols";
 
@@ -37,11 +57,12 @@ const props = withDefaults(defineProps<P>(), {
 });
 const route = useRoute();
 const ticket = inject(ITicket);
-const data = computed(() => ticket.data || {});
-const communications = computed(() => data.value.communications || []);
-const conversation = computed(() =>
-  orderBy([...communications.value], (c) => dayjs(c.creation))
-);
+const communications = computed(() => {
+  const _communications = ticket.data.communications || [];
+  return orderBy(_communications, (c) => dayjs(c.creation));
+});
+
+console.log(communications.value);
 
 function scroll(id: string) {
   const e = document.getElementById(id);
