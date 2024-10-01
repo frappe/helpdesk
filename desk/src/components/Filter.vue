1b586ac5b1bac7da47f6be59bf8265232b72a88f
RitvikSardana <ritvik.sardana11@gmail.com>
2024-10-01 20:12:58 +0530

    fix(filter): null error

--- desk/src/components/Filter.vue
+++ desk/src/components/Filter.vue
@@ -184,6 +184,7 @@ const props = defineProps({
     required: true,
   },
 });
+debugger;
 
 const { isMobileView } = useScreenSize();
 
@@ -213,7 +214,7 @@ function setfilter(data) {
 function updateFilter(
   index: number,
   field = null,
-  value: string = null,
+  value: string = "",
   operator: string = null
 ) {
   let filter = JSON.parse(JSON.stringify(props.filters[index]));
