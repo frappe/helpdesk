7982c7d9e375488c434c9cf3c1c9d1a1f28118fd
RitvikSardana <ritvik.sardana11@gmail.com>
2025-03-21 14:10:21 +0530

    fix: outDir frontend

--- desk/vite.config.js
+++ desk/vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
       buildConfig: {
         emptyOutDir: true,
         indexHtmlPath: "../helpdesk/www/helpdesk/index.html",
+        outDir: `../helpdesk/public/frontend`,
       },
     }),
     vue(),
