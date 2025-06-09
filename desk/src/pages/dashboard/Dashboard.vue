959ed00f70b5a8c2906293aba9d6389fb522c673
RitvikSardana <ritvik.sardana11@gmail.com>
2025-06-10 02:46:12 +0530

    fix: make number card data dynamic

--- desk/src/pages/dashboard/Dashboard.vue
+++ desk/src/pages/dashboard/Dashboard.vue
@@ -55,9 +55,12 @@
           </template>
         </Autocomplete>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+      <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4"
+        v-if="!numberCards.loading"
+      >
         <NumberChart
-          v-for="(config, index) in numberCards"
+          v-for="(config, index) in numberCards.data"
           :key="index"
           class="border rounded-md"
           :config="config"
@@ -88,7 +91,14 @@
 </template>
 
 <script setup lang="ts">
-import { AxisChart, DonutChart, NumberChart, usePageMeta } from "frappe-ui";
+import {
+  AxisChart,
+  createResource,
+  DateRangePicker,
+  DonutChart,
+  NumberChart,
+  usePageMeta,
+} from "frappe-ui";
 import { ref } from "vue";
 
 const selectedPeriod = ref("");
@@ -127,44 +137,44 @@ const agentOptions = [
   },
 ];
 
-const numberCards = [
-  {
-    title: "Tickets",
-    value: 452,
-    delta: -2,
-    deltaSuffix: "%",
-    negativeIsBetter: true,
-  },
-  {
-    title: "% Resolved",
-    value: 92,
-    suffix: "%",
-    delta: -3,
-    deltaSuffix: "%",
-  },
-  {
-    title: "% SLA Fulfilled",
-    value: 86,
-    suffix: "%",
-    delta: -2,
-    deltaSuffix: "%",
-  },
-  {
-    title: "Avg. Resolution Time",
-    value: 4,
-    suffix: " days",
-    delta: -0.2,
-    deltaSuffix: " days",
-    negativeIsBetter: true,
-  },
-  {
-    title: "Avg. Feedback Rating",
-    value: 4.5,
-    suffix: "/5",
-    delta: 0.2,
-    deltaSuffix: " stars",
-  },
-];
+// const numberCards = [
+//   {
+//     title: "Tickets",
+//     value: 452,
+//     delta: -2,
+//     deltaSuffix: "%",
+//     negativeIsBetter: true,
+//   },
+//   {
+//     title: "% Resolved",
+//     value: 92,
+//     suffix: "%",
+//     delta: -3,
+//     deltaSuffix: "%",
+//   },
+//   {
+//     title: "% SLA Fulfilled",
+//     value: 86,
+//     suffix: "%",
+//     delta: -2,
+//     deltaSuffix: "%",
+//   },
+//   {
+//     title: "Avg. Resolution Time",
+//     value: 4,
+//     suffix: " days",
+//     delta: -0.2,
+//     deltaSuffix: " days",
+//     negativeIsBetter: true,
+//   },
+//   {
+//     title: "Avg. Feedback Rating",
+//     value: 4.5,
+//     suffix: "/5",
+//     delta: 0.2,
+//     deltaSuffix: " stars",
+//   },
+// ];
 
 const colors = [
   "#318AD8",
@@ -198,6 +208,21 @@ const ticketTrendConfig = {
     { date: new Date("2024-05-15"), open: 12, closed: 67, sla_fulfilled: 97 },
     { date: new Date("2024-05-16"), open: 16, closed: 62, sla_fulfilled: 98 },
     { date: new Date("2024-05-17"), open: 24, closed: 56, sla_fulfilled: 78 },
+    // make till 30 with random data
+    { date: new Date("2024-05-18"), open: 10, closed: 50, sla_fulfilled: 80 },
+    { date: new Date("2024-05-19"), open: 15, closed: 45, sla_fulfilled: 85 },
+    { date: new Date("2024-05-20"), open: 20, closed: 40, sla_fulfilled: 90 },
+    { date: new Date("2024-05-21"), open: 25, closed: 35, sla_fulfilled: 95 },
+    { date: new Date("2024-05-22"), open: 30, closed: 30, sla_fulfilled: 100 },
+    { date: new Date("2024-05-23"), open: 35, closed: 25, sla_fulfilled: 85 },
+    { date: new Date("2024-05-24"), open: 40, closed: 20, sla_fulfilled: 90 },
+    { date: new Date("2024-05-25"), open: 45, closed: 15, sla_fulfilled: 95 },
+    { date: new Date("2024-05-26"), open: 50, closed: 10, sla_fulfilled: 100 },
+    { date: new Date("2024-05-27"), open: 55, closed: 5, sla_fulfilled: 85 },
+    { date: new Date("2024-05-28"), open: 60, closed: 0, sla_fulfilled: 90 },
+    { date: new Date("2024-05-29"), open: 65, closed: -5, sla_fulfilled: 95 },
+    { date: new Date("2024-05-30"), open: 70, closed: -10, sla_fulfilled: 100 },
+    //
   ],
   title: "Tickets Trend",
   subtitle: "Average tickets per day is around 124",
@@ -324,229 +349,16 @@ const ticketsByChannel = {
   valueColumn: "count",
 };
 
-usePageMeta(() => {
-  return {
-    title: "Dashboard",
-  };
+const numberCards = createResource({
+  url: "helpdesk.api.dashboard.get_dashboard_data",
+  params: {
+    type: "number_card",
+  },
+  auto: true,
+  onSuccess: (data) => {
+    console.log(data);
+  },
 });
-</script>
-
-<template>
-  <div class="flex flex-col">
-    <LayoutHeader>
-      <template #left-header>
-        <div class="text-lg font-medium text-gray-900">Dashboard</div>
-      </template>
-      <template #right-header> </template>
-    </LayoutHeader>
-
-    <div class="p-5 w-full overflow-y-scroll">
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
-        <NumberChart
-          v-for="(config, index) in numberCards"
-          :key="index"
-          class="border rounded-md"
-          :config="config"
-        />
-      </div>
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
-        <div class="border rounded-md min-h-80">
-          <AxisChart :config="ticketTrendConfig" />
-        </div>
-        <div class="border rounded-md min-h-80">
-          <AxisChart :config="feedbackTrendConfig" />
-        </div>
-        <div class="border rounded-md">
-          <DonutChart :config="ticketsByTeam" />
-        </div>
-        <div class="border rounded-md">
-          <DonutChart :config="ticketsByType" />
-        </div>
-        <div class="border rounded-md">
-          <DonutChart :config="ticketsByPriority" />
-        </div>
-        <div class="border rounded-md">
-          <DonutChart :config="ticketsByChannel" />
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-import { AxisChart, DonutChart, NumberChart, usePageMeta } from "frappe-ui";
-
-const numberCards = [
-  {
-    title: "Tickets",
-    value: 452,
-    delta: -2,
-    deltaSuffix: "%",
-    negativeIsBetter: true,
-  },
-  {
-    title: "% Resolved",
-    value: 92,
-    suffix: "%",
-    delta: -3,
-    deltaSuffix: "%",
-  },
-  {
-    title: "% SLA Fulfilled",
-    value: 86,
-    suffix: "%",
-    delta: -2,
-    deltaSuffix: "%",
-  },
-  {
-    title: "Avg. Resolution Time",
-    value: 4,
-    suffix: " days",
-    delta: -0.2,
-    deltaSuffix: " days",
-    negativeIsBetter: true,
-  },
-  {
-    title: "Avg. Feedback Rating",
-    value: 4.5,
-    suffix: "/5",
-    delta: 0.2,
-    deltaSuffix: " stars",
-  },
-];
-
-const ticketTrendConfig = {
-  data: [
-    { date: new Date("2024-05-01"), open: 6, closed: 122, sla_fulfilled: 78 },
-    { date: new Date("2024-05-02"), open: 8, closed: 163, sla_fulfilled: 82 },
-    { date: new Date("2024-05-03"), open: 3, closed: 119, sla_fulfilled: 85 },
-    { date: new Date("2024-05-04"), open: 7, closed: 161, sla_fulfilled: 80 },
-    { date: new Date("2024-05-05"), open: 2, closed: 138, sla_fulfilled: 90 },
-    { date: new Date("2024-05-06"), open: 8, closed: 113, sla_fulfilled: 88 },
-    { date: new Date("2024-05-07"), open: 3, closed: 107, sla_fulfilled: 92 },
-    { date: new Date("2024-05-08"), open: 9, closed: 102, sla_fulfilled: 95 },
-    { date: new Date("2024-05-09"), open: 5, closed: 96, sla_fulfilled: 89 },
-    { date: new Date("2024-05-10"), open: 9, closed: 92, sla_fulfilled: 91 },
-    { date: new Date("2024-05-11"), open: 6, closed: 88, sla_fulfilled: 87 },
-    { date: new Date("2024-05-12"), open: 8, closed: 83, sla_fulfilled: 93 },
-    { date: new Date("2024-05-13"), open: 7, closed: 77, sla_fulfilled: 94 },
-    { date: new Date("2024-05-14"), open: 7, closed: 72, sla_fulfilled: 96 },
-    { date: new Date("2024-05-15"), open: 12, closed: 67, sla_fulfilled: 97 },
-    { date: new Date("2024-05-16"), open: 16, closed: 62, sla_fulfilled: 98 },
-    { date: new Date("2024-05-17"), open: 24, closed: 56, sla_fulfilled: 78 },
-  ],
-  title: "Tickets Trend",
-  subtitle: "Average tickets per day is around 124",
-  xAxis: {
-    key: "date",
-    type: "time",
-    title: "Date",
-    timeGrain: "day",
-  },
-  yAxis: {
-    title: "Tickets",
-  },
-  y2Axis: {
-    title: "% SLA",
-    yMin: 0,
-    yMax: 100,
-  },
-  stacked: true,
-  series: [
-    { name: "closed", type: "bar" },
-    { name: "open", type: "bar" },
-    { name: "sla_fulfilled", type: "line", showDataPoints: true, axis: "y2" },
-  ],
-};
-
-const feedbackTrendConfig = {
-  data: [
-    { date: new Date("2024-05-01"), rating: 4.5, rated_tickets: 67 },
-    { date: new Date("2024-05-02"), rating: 4.6, rated_tickets: 89 },
-    { date: new Date("2024-05-03"), rating: 4.7, rated_tickets: 95 },
-    { date: new Date("2024-05-04"), rating: 4.8, rated_tickets: 120 },
-    { date: new Date("2024-05-05"), rating: 4.9, rated_tickets: 79 },
-    { date: new Date("2024-05-06"), rating: 5.0, rated_tickets: 59 },
-    { date: new Date("2024-05-07"), rating: 4.8, rated_tickets: 82 },
-    { date: new Date("2024-05-08"), rating: 4.7, rated_tickets: 91 },
-    { date: new Date("2024-05-09"), rating: 4.6, rated_tickets: 103 },
-    { date: new Date("2024-05-10"), rating: 4.5, rated_tickets: 87 },
-    { date: new Date("2024-05-11"), rating: 4.6, rated_tickets: 93 },
-    { date: new Date("2024-05-12"), rating: 4.7, rated_tickets: 73 },
-    { date: new Date("2024-05-13"), rating: 4.8, rated_tickets: 79 },
-    { date: new Date("2024-05-14"), rating: 4.9, rated_tickets: 80 },
-    { date: new Date("2024-05-15"), rating: 4.6, rated_tickets: 92 },
-    { date: new Date("2024-05-16"), rating: 4.3, rated_tickets: 112 },
-    { date: new Date("2024-05-17"), rating: 4.7, rated_tickets: 95 },
-  ],
-  title: "Feedback Trend",
-  subtitle: "Average feedback rating per day is around 4.8",
-  xAxis: {
-    key: "date",
-    type: "time",
-    title: "Date",
-    timeGrain: "day",
-  },
-  yAxis: {
-    title: "Rated Tickets",
-  },
-  y2Axis: {
-    title: "Rating",
-    yMin: 0,
-    yMax: 5,
-  },
-  series: [
-    { name: "rated_tickets", type: "bar" },
-    { name: "rating", type: "line", showDataPoints: true, axis: "y2" },
-  ],
-};
-
-const ticketsByPriority = {
-  data: [
-    { priority: "High", count: 120 },
-    { priority: "Medium", count: 200 },
-    { priority: "Low", count: 80 },
-  ],
-  title: "Tickets by Priority",
-  subtitle: "Total tickets by priority",
-  categoryColumn: "priority",
-  valueColumn: "count",
-};
-
-const ticketsByTeam = {
-  data: [
-    { team: "Support", count: 300 },
-    { team: "Development", count: 150 },
-    { team: "Sales", count: 50 },
-  ],
-  title: "Tickets by Team",
-  subtitle: "Total tickets by team",
-  categoryColumn: "team",
-  valueColumn: "count",
-};
-
-const ticketsByType = {
-  data: [
-    { type: "Bug", count: 200 },
-    { type: "Feature", count: 23 },
-    { type: "Task", count: 83 },
-  ],
-  title: "Tickets by Type",
-  subtitle: "Total tickets by type",
-  categoryColumn: "type",
-  valueColumn: "count",
-};
-
-const ticketsByChannel = {
-  data: [
-    { channel: "Email", count: 12 },
-    { channel: "Portal", count: 223 },
-  ],
-  title: "Tickets by Channel",
-  subtitle: "Total tickets by channel",
-  categoryColumn: "channel",
-  valueColumn: "count",
-};
 
 usePageMeta(() => {
   return {
--- helpdesk/api/dashboard.py
+++ helpdesk/api/dashboard.py
@@ -3,8 +3,8 @@ import frappe
 from helpdesk.utils import agent_only
 
 
-@agent_only
 @frappe.whitelist()
+@agent_only
 def get_dashboard_data(type, filters=None):
     """
     Get dashboard data based on the type and date range.
@@ -36,43 +36,26 @@ def get_number_card_data(from_date, to_date):
     # Remove from and to date after testing
 
     ticket_chart_data = get_ticket_count(from_date, to_date)
+    resolved_tickets = get_resolved_tickets(from_date, to_date)
+    sla_fulfilled_count = get_sla_fulfilled_count(from_date, to_date)
+    avg_resolution_time = get_avg_resolution_time(from_date, to_date)
     avg_feedback_score = get_avg_feedback_score(from_date, to_date)
 
-    return [ticket_chart_data, avg_feedback_score]
+    return [
+        ticket_chart_data,
+        resolved_tickets,
+        sla_fulfilled_count,
+        avg_resolution_time,
+        avg_feedback_score,
+    ]
 
 
 def get_ticket_count(from_date, to_date):
     """
     Get ticket data for the dashboard.
     """
-    ticket_count = frappe.get_list(
-        "HD Ticket",
-        filters=[
-            {"creation": [">=", from_date]},
-            {"creation": ["<=", to_date]},
-        ],
-        fields=["count(*) as count"],
-    )
-    ticket_count = ticket_count[0].count if ticket_count else 0
-    ticket_count_last_month = frappe.get_list(
-        "HD Ticket",
-        filters=[
-            {"creation": [">=", frappe.utils.add_months(from_date, -1)]},
-            {"creation": ["<", from_date]},
-        ],
-        fields=["count(*) as count"],
-    )
-
-    ticket_count_last_month = (
-        ticket_count_last_month[0].count if ticket_count_last_month else 0
-    )
-
-    delta_in_percentage = (
-        (ticket_count - ticket_count_last_month) / ticket_count_last_month * 100
-        if ticket_count_last_month
-        else 0
-    )
-
+    from_date = frappe.utils.add_days(frappe.utils.nowdate(), -30)
+    to_date = frappe.utils.nowdate()
     result = frappe.db.sql(
         """
 		SELECT
@@ -80,13 +63,13 @@ def get_ticket_count(from_date, to_date):
 					WHEN creation >= %(from_date)s AND creation <= %(to_date)s 
 					THEN name
 					ELSE NULL
-				END) as current_month_count
+				END) as current_month_tickets,
 
 				COUNT(CASE
 					WHEN creation >= %(prev_from_date)s AND creation < %(from_date)s 
 					THEN name
 					ELSE NULL
-				END) as prev_month_count
+				END) as prev_month_tickets
 		FROM `tabHD Ticket`
     """,
         {
@@ -97,9 +80,17 @@ def get_ticket_count(from_date, to_date):
         as_dict=1,
     )
 
+    current_month_tickets = result[0].current_month_tickets or 0
+    prev_month_tickets = result[0].prev_month_tickets or 0
+    delta_in_percentage = (
+        (current_month_tickets - prev_month_tickets) / prev_month_tickets * 100
+        if prev_month_tickets
+        else 0
+    )
+
     return {
         "title": "Tickets",
-        "value": ticket_count,
+        "value": current_month_tickets,
         "delta": delta_in_percentage,
         "deltaSuffix": "%",
         "negativeIsBetter": True,
@@ -141,18 +132,15 @@ def get_avg_feedback_score(from_date, to_date):
     current_month_avg = result[0].current_month_avg or 0
     prev_month_avg = result[0].prev_month_avg or 0
 
-    delta_in_percentage = (
-        (current_month_avg - prev_month_avg) / prev_month_avg * 100
-        if prev_month_avg
-        else 0
-    )
+    delta = current_month_avg - prev_month_avg
+    print("\n\n", result, "\n\n")
 
     return {
         "title": "Avg. Feedback Rating",
-        "value": current_month_avg,
+        "value": current_month_avg * 5,
         "suffix": "/5",
-        "delta": delta_in_percentage,
-        "deltaSuffix": "%",
+        "delta": delta * 5,
+        "deltaSuffix": " stars",
     }
 
 
@@ -160,17 +148,129 @@ def get_resolved_tickets(from_date, to_date):
     """
     Get resolved tickets for the dashboard.
     """
-    resolved_tickets = frappe.get_list(
-        "HD Ticket",
-        filters=[
-            {"creation": [">=", from_date]},
-            {"creation": ["<=", to_date]},
-            {"status": ["in", ["Resolved", "Closed"]]},
-        ],
-        fields=["count(*) as count"],
+    result = frappe.db.sql(
+        """
+        SELECT 
+            COUNT(CASE 
+                WHEN creation >= %(from_date)s AND creation <= %(to_date)s AND status IN ('Resolved', 'Closed')
+                THEN name 
+                ELSE NULL
+            END) as current_month_resolved,
+            COUNT(CASE 
+                WHEN creation >= %(prev_from_date)s AND creation < %(from_date)s AND status IN ('Resolved', 'Closed')
+                THEN name 
+                ELSE NULL
+            END) as prev_month_resolved
+        FROM `tabHD Ticket`
+    """,
+        {
+            "from_date": from_date,
+            "to_date": to_date,
+            "prev_from_date": frappe.utils.add_months(from_date, -1),
+        },
+        as_dict=1,
     )
-    resolved_tickets_count = resolved_tickets[0].count if resolved_tickets else 0
+    current_month_resolved = result[0].current_month_resolved or 0
+    prev_month_resolved = result[0].prev_month_resolved or 0
+
+    delta_in_percentage = (
+        (current_month_resolved - prev_month_resolved) / prev_month_resolved * 100
+        if prev_month_resolved
+        else 0
+    )
     return {
-        "title": " %Resolved",
-        "value": 2,
+        "title": "% Resolved",
+        "value": current_month_resolved,
+        "suffix": "%",
+        "delta": delta_in_percentage,
+        "deltaSuffix": "%",
+    }
+
+
+def get_sla_fulfilled_count(from_date, to_date):
+    #    "fieldname": "agreement_status",
+    # value:"Fulfilled"
+    """
+    Get the percent of SLA tickets fulfilled for the dashboard.
+    """
+    result = frappe.db.sql(
+        """
+        SELECT 
+            COUNT(CASE 
+                WHEN creation >= %(from_date)s AND creation <= %(to_date)s AND agreement_status = 'Fulfilled'
+                THEN name 
+                ELSE NULL
+            END) as current_month_fulfilled,
+            COUNT(CASE 
+                WHEN creation >= %(prev_from_date)s AND creation < %(from_date)s AND agreement_status = 'Fulfilled'
+                THEN name 
+                ELSE NULL
+            END) as prev_month_fulfilled
+        FROM `tabHD Ticket`
+    """,
+        {
+            "from_date": from_date,
+            "to_date": to_date,
+            "prev_from_date": frappe.utils.add_months(from_date, -1),
+        },
+        as_dict=1,
+    )
+    current_month_fulfilled = result[0].current_month_fulfilled or 0
+    prev_month_fulfilled = result[0].prev_month_fulfilled or 0
+
+    delta_in_percentage = (
+        (current_month_fulfilled - prev_month_fulfilled) / prev_month_fulfilled * 100
+        if prev_month_fulfilled
+        else 0
+    )
+    return {
+        "title": "% SLA Fulfilled",
+        "value": current_month_fulfilled,
+        "suffix": "%",
+        "delta": delta_in_percentage,
+        "deltaSuffix": "%",
+    }
+
+
+def get_avg_resolution_time(from_date, to_date):
+    """
+    Get average resolution time for the dashboard.
+    """
+    from_date = frappe.utils.add_days(frappe.utils.nowdate(), -30)
+    to_date = frappe.utils.nowdate()
+    result = frappe.db.sql(
+        """
+        SELECT 
+            AVG(CASE 
+                WHEN status = 'Resolved' AND creation >= %(from_date)s AND creation <= %(to_date)s
+                THEN TIMESTAMPDIFF(DAY, creation, resolution_date)
+                ELSE NULL
+            END) as current_month_avg,
+            AVG(CASE 
+                WHEN status = 'Resolved' AND creation >= %(prev_from_date)s AND creation < %(from_date)s
+                THEN TIMESTAMPDIFF(DAY, creation, resolution_date)
+                ELSE NULL
+            END) as prev_month_avg
+        FROM `tabHD Ticket`
+        WHERE 
+            (creation >= %(prev_from_date)s AND creation <= %(to_date)s)
+    """,
+        {
+            "from_date": from_date,
+            "to_date": to_date,
+            "prev_from_date": frappe.utils.add_months(from_date, -1),
+        },
+        as_dict=1,
+    )
+    current_month_avg = result[0].current_month_avg or 0
+    prev_month_avg = result[0].prev_month_avg or 0
+
+    delta = current_month_avg - prev_month_avg if prev_month_avg else 0
+    return {
+        "title": "Avg. Resolution Time",
+        "value": current_month_avg,
+        "suffix": " days",
+        "delta": delta,
+        "deltaSuffix": " days",
+        "negativeIsBetter": True,
     }
