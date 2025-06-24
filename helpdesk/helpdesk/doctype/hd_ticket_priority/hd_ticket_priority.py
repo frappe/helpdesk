9b65350d437a1d1a7c73eee12bd67c99d40426b3
Ritvik Sardana <ritviksardana@Ritviks-MacBook-Air-2.local>
2025-06-24 16:57:33 +0530

    fix: better error if priority not found in SLA

--- helpdesk/helpdesk/doctype/hd_ticket_priority/hd_ticket_priority.py
+++ helpdesk/helpdesk/doctype/hd_ticket_priority/hd_ticket_priority.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
-
 from __future__ import unicode_literals
 
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class HDTicketPriority(Document):
-    pass
+    def before_save(self):
+        priority_in_sla = frappe.db.get_value(
+            "HD Service Level Priority", {"priority": self.name}
+        )
+        if priority_in_sla:
+            return
+        frappe.msgprint(
+            msg=_(
+                "Please add this priority in the <a href='/app/hd-service-level-agreement'>required SLA documents</a>"
+            ),
+            title=_("Action Required"),
+            indicator="orange",
+        )
