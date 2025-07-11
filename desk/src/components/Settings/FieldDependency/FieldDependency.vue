01529d76f750759ab6390045072552bc5d78cd8a
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-11 18:18:56 +0530

    chore: organize api folder

--- desk/src/components/Settings/FieldDependency/FieldDependency.vue
+++ desk/src/components/Settings/FieldDependency/FieldDependency.vue
@@ -230,7 +230,7 @@ function getSelectedChildValueCount(parent) {
 }
 
 const fields = createResource({
-  url: "helpdesk.api.ticket_meta.get_fields_meta",
+  url: "helpdesk.helpdesk.doctype.hd_ticket.api.get_fields_meta",
   auto: true,
   params: {
     doctype: "HD Ticket",
