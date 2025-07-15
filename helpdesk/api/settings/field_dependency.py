d4b0a857a0a902bbbbce182148d0716a4f71582a
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-15 14:15:47 +0530

    fix: add function to form script

--- helpdesk/api/settings/field_dependency.py
+++ helpdesk/api/settings/field_dependency.py
@@ -1,4 +1,4 @@
-# import re
+import re
 
 import frappe
 from frappe import _
@@ -11,26 +11,49 @@ def create_field_dependency(parent_field, child_field, parent_child_mapping):
         frappe.throw(
             _("Parent field, child field, and parent-child mapping are required.")
         )
-    print("\n\n", parent_field, "\n\n")
-    print("\n\n", child_field, "\n\n")
-    print("\n\n", parent_child_mapping, "\n\n")
-    script = generate_on_change_function(
+    # print("\n\n", parent_field, "\n\n")
+    # print("\n\n", child_field, "\n\n")
+    # print("\n\n", parent_child_mapping, "\n\n")
+    func = generate_on_change_function(
         parent_child_mapping=frappe.parse_json(parent_child_mapping),
         parent_field=parent_field,
         child_field=child_field,
     )
-    print("\n\n", script, "\n\n")
-    pass
+
+    script_doc = frappe.new_doc("HD Form Script")
+    script_doc.script = add_function_to_script(
+        script_doc.script, func, parent_field, child_field
+    )
+
+    # script_doc.save()
 
 
 def generate_on_change_function(parent_child_mapping, parent_field, child_field):
-    script = f"function update_{parent_field}(value){{\n"
+    script = f"function update_{child_field}(value){{\n"
     for parent, children in parent_child_mapping.items():
         # replace '' with ""
         options = ",".join([f'"{child}"' for child in children])
-        script += f'    if(value=="{parent}"){{\n'
-        script += f"        options = [{options}]\n"
-        script += f'        applyFilters("{child_field}",options)\n'
-        script += "    }}\n"
-    script += "}\n"
+        script += f'        if(value=="{parent}"){{\n'
+        script += f"            options = [{options}]\n"
+        script += f'            applyFilters("{child_field}",options)\n'
+        script += "        }\n"
+    script += "    }\n"
+    # one more line here
+    script += "\n"
+    return script
+
+
+def add_function_to_script(script, func, parent_field, child_field):
+    # print('\n\n',script,'\n\n')
+    # print('\n\n',func,'\n\n')
+    # print('\n\n',parent_field,'\n\n')
+    # print('\n\n',child_field,'\n\n')
+    # Add function above the return statement
+    match = re.search(r"return\s*{", script)
+    if match:
+        insert_position = match.start()
+        print("\n\n", match.start(), "\n\n")
+        # take evertything before the position, add function, take everythin after the position
+        script = script[:insert_position] + "\n    " + func + script[insert_position:]
+
     return script
