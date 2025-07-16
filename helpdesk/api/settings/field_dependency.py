ca76fb21cb7b6b5e681c78c9b729129d5737fad8
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-16 19:43:47 +0530

    fix: create separate script for each field dependency

--- helpdesk/api/settings/field_dependency.py
+++ helpdesk/api/settings/field_dependency.py
@@ -1,5 +1,3 @@
-import re
-
 import frappe
 from frappe import _
 
@@ -32,21 +30,27 @@ def create_field_dependency(parent_field, child_field, parent_child_mapping):
         child_field=child_field,
     )
 
-    script_doc = get_or_create_standard_form_script()
+    script_doc = get_or_create_standard_form_script(parent_field, child_field)
     script_doc.script = add_function_to_script(
-        script_doc.script, func, parent_field, child_field
+        parent_field,
+        child_field,
+        func,
     )
 
     script_doc.save()
 
 
-def get_or_create_standard_form_script():
-    if existing_doc := frappe.db.exists("HD Form Script", {"is_standard": 1}):
+def get_or_create_standard_form_script(parent_field, child_field):
+    if existing_doc := frappe.db.exists(
+        "HD Form Script",
+        {"name": ["like", f"FieldDependency-{parent_field}-{child_field}"]},
+    ):
         return frappe.get_doc("HD Form Script", existing_doc)
     else:
         doc = frappe.new_doc("HD Form Script")
         doc.is_standard = 1
         doc.enabled = 1
+        doc.name = f"FieldDependency-{parent_field}-{child_field}"
         return doc
 
 
@@ -59,65 +63,27 @@ def generate_on_change_function(parent_child_mapping, parent_field, child_field)
         script += f"            options = [{options}]\n"
         script += f'            applyFilters("{child_field}",options)\n'
         script += "        }\n"
+        script += "\n"
     script += "    }\n"
     script += "\n"
     return script
 
 
-def add_function_to_script(script, func, parent_field, child_field):
-    # Add function above the return statement
-    match = re.search(r"return\s*{", script)
-    if match:
-        insert_position = match.start()
-        script = (
-            script[:insert_position] + "\n    " + func + "\n" + script[insert_position:]
-        )
-
-    # Check if the script value is default (true for new_doc)
-    if re.search(r"actions:\s*\[\s*\]", script):
-        script = handle_new_doc_script(script, parent_field, child_field)
-    else:
-        script = handle_existing_doc_script(script, parent_field, child_field)
-    return script
-
-
-def handle_new_doc_script(script, parent_field, child_field):
-    return_match = re.search(r"return\s*{", script)
-    if return_match:
-        start_pos = return_match.start()
-        pos = return_match.end() - 1
-        ending_pos = find_closing_brace_position(script, pos)
-        if ending_pos:
-            ending_pos += 1
-            new_return_block = f"""return {{
+def add_function_to_script(parent_field, child_field, func):
+    script = "function setupForm({doc, updateField, call, router, toast, $dialog, createToast ,applyFilters}) {"
+    script += "\n"
+    script += "\n"
+    script += f"    {func}"
+    script += "\n"
+    script += f"""   return {{
         onChange: {{
-            {parent_field}: (newVal) => update_{child_field}(newVal),
+            {parent_field}: (newVal) => update_{child_field}(newVal)
         }}
-    }}"""
-            script = script[:start_pos] + new_return_block + script[ending_pos:]
-    return script
-
-
-def handle_existing_doc_script(script, parent_field, child_field):
-    onChange_match = re.search(r"onChange\s*:\s*{", script)
-    if not onChange_match:
-        return script
-
-    pos = onChange_match.end() - 1
-    ending_pos = find_closing_brace_position(script, pos)
-    if not ending_pos:
-        return script
-
-    onChange_content = script[onChange_match.end() : ending_pos]
-    if onChange_content.strip():
-        new_field = (
-            f"\n            {parent_field}: (newVal) => update_{child_field}(newVal),"
-        )
-        script = script[:ending_pos] + new_field + "\n        " + script[ending_pos:]
-    else:
-        new_field = f"\n            {parent_field}: (newVal) => update_{child_field}(newVal),\n        "
-        script = script[:ending_pos] + new_field + script[ending_pos:]
-
+    }}
+    """
+    script += "\n"
+    script += "}"
+    print("\n\n", script, "\n\n")
     return script
 
 
