5d7fa3075a990c9e1b9177bc6f1915836319cf26
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-16 12:37:01 +0530

    fix: create standard doc in form script
    (cherry picked from commit b58053ae25f39920fe869dc6db5228a0d42c549a)

--- helpdesk/helpdesk/doctype/hd_form_script/hd_form_script.js
+++ helpdesk/helpdesk/doctype/hd_form_script/hd_form_script.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("HD Form Script", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("HD Form Script", {
+  refresh(frm) {
+    if (frm.doc.is_standard && !frappe.boot.developer_mode) {
+      frm.disable_form();
+      frappe.show_alert(
+        __(
+          "Standard Form Scripts can't be modified, duplicate the script instead."
+        )
+      );
+    }
+  },
+});
