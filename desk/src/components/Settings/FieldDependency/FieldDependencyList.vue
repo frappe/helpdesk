dd82a9b1974627c1a56fd98b3917d1026eb18ebc
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-07-17 09:23:25 +0530

    fix: start list view
    (cherry picked from commit aa36617cd76059ff03073065f43a48a7b92d55a7)

--- desk/src/components/Settings/FieldDependency/FieldDependencyList.vue
+++ desk/src/components/Settings/FieldDependency/FieldDependencyList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <header class="flex justify-between">
+    <header class="flex justify-between mb-4">
       <div>
         <h1 class="text-lg font-semibold mb-1">Field Dependencies</h1>
         <p class="text-ink-gray-6 text-base">
@@ -18,9 +18,31 @@
         </template>
       </Button>
     </header>
+
+    <div class="w-full">
+      <!-- table heading -->
+      <div class="flex w-full p-2 border-b">
+        <p class="w-4/12 text-p-sm text-ink-gray-5">Name</p>
+        <p class="w-4/12 text-p-sm text-ink-gray-5">Created By</p>
+        <p class="w-3/12 text-p-sm text-ink-gray-5">Enabled</p>
+        <p class="w-1/12 text-p-sm text-ink-gray-5"></p>
+      </div>
+
+      <!-- Table content -->
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { createListResource } from "frappe-ui";
+
+const list = createListResource({
+  doctype: "HD Form Script",
+  filters: { is_standard: 1 },
+  fields: ["name", "enabled", "owner"],
+  auto: true,
+  cache: ["FD", "List"],
+});
+</script>
 
 <style scoped></style>
