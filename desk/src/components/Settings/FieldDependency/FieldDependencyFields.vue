18a57242cf0fa28e125c6df8b50fffeda5313354
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-08-05 13:26:42 +0530

    chore: improve code readability in Field Dependency file
    (cherry picked from commit d9710690e6ffdedf52b18dc52b0591d17d723d2c)

---
+++ desk/src/components/Settings/FieldDependency/FieldDependencyFields.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex gap-3 w-full justify-between">
+    <FormControl
+      v-model="state.selectedParentField"
+      label="Parent Field"
+      placeholder="Select Parent Field"
+      required
+      class="flex-1"
+      type="select"
+      :options="parentFields"
+      :disabled="!isNew"
+    />
+    <FormControl
+      v-model="state.selectedChildField"
+      label="Child Field"
+      placeholder="Select Child Field"
+      required
+      class="flex-1"
+      :disabled="!state.selectedParentField || !isNew"
+      type="select"
+      :options="state.childFields"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FieldCriteriaState } from "@/types";
+
+const state = defineModel<FieldCriteriaState>();
+
+const props = defineProps<{
+  isNew: boolean;
+  parentFields: any[];
+}>();
+</script>
+
+<style scoped></style>
