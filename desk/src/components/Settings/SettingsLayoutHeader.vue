89a53a1f3dbf9eaef2d4f8f9d78b2da133f96748
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-08-12 02:22:13 +0530

    fix(styles): letter spacing in settings header
    (cherry picked from commit e214a3eae73c9998634c2d1a4c7608e858362391)
    
    # Conflicts:
    #	desk/src/components/Settings/SettingsLayoutHeader.vue
    #	desk/src/components/Settings/Sla/SlaPolicies.vue

---
+++ desk/src/components/Settings/SettingsLayoutHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-start justify-between">
+    <div class="flex flex-col gap-1">
+      <slot name="title">
+        <h1 class="text-lg font-semibold text-ink-gray-8">
+          {{ title }}
+        </h1>
+      </slot>
+      <slot name="description">
+        <p class="text-p-sm text-gray-700 max-w-md text-ink-gray-6">
+          {{ description }}
+        </p>
+      </slot>
+    </div>
+    <slot name="actions" />
+  </div>
+  <div class="mt-6" v-if="Boolean($slots['bottom-section'])">
+    <slot name="bottom-section" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+});
+</script>
---
+++ desk/src/components/Settings/Sla/SlaPolicies.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="px-10 py-8">
+    <SettingsLayoutHeader>
+      <template #title>
+        <h1 class="text-lg font-semibold text-ink-gray-8">
+          Service Level Agreements (SLAs)
+        </h1>
+      </template>
+      <template #description>
+        <p class="text-p-sm max-w-md text-ink-gray-6">
+          SLAs align your team and customers with defined timelines for a
+          reliable experience.
+          <a
+            href="https://docs.frappe.io/helpdesk/service-level-agreement"
+            target="_blank"
+            class="underline"
+            >Learn more about SLA
+          </a>
+        </p>
+      </template>
+      <template #actions>
+        <Button
+          label="New"
+          theme="gray"
+          variant="solid"
+          @click="goToNew()"
+          icon-left="plus"
+        />
+      </template>
+    </SettingsLayoutHeader>
+  </div>
+  <div class="px-10 pb-8 overflow-y-auto">
+    <SlaPolicyList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { resetSlaData, slaActiveScreen } from "@/stores/sla";
+import { Button } from "frappe-ui";
+import SlaPolicyList from "./SlaPolicyList.vue";
+import SettingsLayoutHeader from "../SettingsLayoutHeader.vue";
+
+const goToNew = () => {
+  resetSlaData();
+  slaActiveScreen.value = {
+    screen: "view",
+    data: null,
+    fetchData: true,
+  };
+};
+</script>
