a82af354c01e7351cc9c196b0bcfab170b3dabcc
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-08-11 15:37:57 +0530

    fix: add documentation link
    (cherry picked from commit ffd840ebf6f5ad5c6456fac91742e73fb27d6e4a)

---
+++ desk/src/components/DocumentationButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <Tooltip text="View documentation">
+    <a :href="url" target="_blank">
+      <lucide-info class="h-4 w-4 text-ink-gray-6" :class="color" />
+    </a>
+  </Tooltip>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  url: string;
+  color?: string;
+}>();
+</script>
+
+<style scoped></style>
