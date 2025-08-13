cb831f989f6ac9907162992c978e255f12e8fced
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-08-13 21:20:43 +0530

    fix: validations + test cases in HD Ticket Status
    (cherry picked from commit 7803ad2c1e651e94f513985be784463b7b17eb7a)

--- helpdesk/helpdesk/doctype/hd_ticket_status/hd_ticket_status.py
+++ helpdesk/helpdesk/doctype/hd_ticket_status/hd_ticket_status.py
@@ -1,9 +1,56 @@
 # Copyright (c) 2025, Frappe Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class HDTicketStatus(Document):
-    pass
+    def validate(self):
+        self.validate_closed_status_change()
+        self.validate_required_categories()
+
+    def validate_closed_status_change(self):
+        if self.is_new():
+            return
+        old_name = (
+            self.get_doc_before_save().name if self.get_doc_before_save() else None
+        )
+        if old_name == "Closed" and not self.has_value_changed("order"):
+            # Prevent modification or deletion of the 'Closed' status
+            frappe.throw(_("The 'Closed' status cannot be modified or deleted."))
+
+    def validate_required_categories(self):
+        if self.is_new():
+            return
+        if not self.has_value_changed("category"):
+            return
+        old_category = (
+            self.get_doc_before_save().category if self.get_doc_before_save() else None
+        )
+
+        if not old_category:
+            return
+
+        # If category is being changed, check if we're removing the last status of a required category
+        if old_category != self.category:
+            count = frappe.db.count(
+                "HD Ticket Status",
+                {"category": old_category, "name": ["!=", self.name]},
+            )
+
+            if count == 0:
+                frappe.throw(
+                    _(
+                        f"At least one ticket status with category '{old_category}' must exist in the system."
+                    )
+                )
+
+    def before_save(self):
+        self.update_customer_label()
+
+    def update_customer_label(self):
+        if self.different_view:
+            return
+        self.label_customer = self.label_agent
