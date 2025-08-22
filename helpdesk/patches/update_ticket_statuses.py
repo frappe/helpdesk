8603873e5a5daabddb1cb0e31a499a9523e4c1dd
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-08-22 22:51:38 +0530

    fix: add default status and reopen ticket status fields in helpdesk

--- helpdesk/patches/update_ticket_statuses.py
+++ helpdesk/patches/update_ticket_statuses.py
@@ -9,5 +9,8 @@ def execute():
     if frappe.db.get_single_value("HD Settings", "auto_update_status"):
         frappe.db.set_single_value("HD Settings", "update_status_to", "Replied")
 
+    frappe.db.set_single_value("HD Settings", "default_ticket_status", "Open")
+    frappe.db.set_single_value("HD Settings", "ticket_reopen_status", "Open")
+
     frappe.db.sql("DELETE FROM `tabHD Pause Service Level Agreement On Status`")
     frappe.db.sql("DELETE FROM `tabHD Service Level Agreement Fulfilled On Status`")
