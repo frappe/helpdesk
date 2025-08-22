b0ad8732745de4984e3366dd96d35f6a696d3d1c
Elton Lobo <eltonsw00@gmail.com>
2025-08-22 19:34:28 +0530

    feat(email-notifications): validate jinja templates before updating settings

--- helpdesk/api/settings/email_notifications.py
+++ helpdesk/api/settings/email_notifications.py
@@ -1,7 +1,8 @@
-from typing import Literal
+from typing import Any, Literal
 
 import frappe
 from frappe import _
+from frappe.utils.jinja import validate_template
 
 from helpdesk.helpdesk.doctype.hd_settings.hd_settings import HDSettings
 
@@ -89,6 +90,30 @@ def get_data(notification: str):
     frappe.throw(_("Invalid notification"))
 
 
+def set_common_settings(
+    enabled_field_name: str, enabled: bool, content_field_name: str, content: str
+):
+    validate_template(content)
+    frappe.db.set_single_value(
+        "HD Settings",
+        enabled_field_name,
+        int(enabled),
+    )
+    frappe.db.set_single_value("HD Settings", content_field_name, content)
+    return {
+        "enabled": enabled,
+        "content": content,
+    }
+
+
+def merge(*mps: dict[str, Any]):
+    res: dict[str, Any] = {}
+    for mp in mps:
+        for key, value in mp.items():
+            res[key] = value
+    return res
+
+
 @frappe.whitelist(methods=["PUT"])
 def update_share_feedback(
     ticket_status: Literal["Closed", "Resolved"],
@@ -96,70 +121,48 @@ def update_share_feedback(
     content: str,
 ):
     only_for_managers()
-
-    # todo: add input validation
-
+    common_settings = set_common_settings(
+        "enable_email_ticket_feedback",
+        enabled,
+        "feedback_email_content",
+        content,
+    )
     frappe.db.set_single_value(
         "HD Settings",
         "send_email_feedback_on_status",
         ticket_status,
     )
-    frappe.db.set_single_value(
-        "HD Settings",
-        "enable_email_ticket_feedback",
-        int(enabled),
-    )
-    frappe.db.set_single_value("HD Settings", "feedback_email_content", content)
-    return {
-        "ticket_status": ticket_status,
-        "enabled": enabled,
-        "content": content,
-    }
+    return merge(common_settings, {"ticket_status": ticket_status})
 
 
 @frappe.whitelist(methods=["PUT"])
 def update_acknowledgement(enabled: bool, content: str):
     only_for_managers()
-
-    # todo: add input validation
-
-    frappe.db.set_single_value(
-        "HD Settings", "send_acknowledgement_email", int(enabled)
+    return set_common_settings(
+        "send_acknowledgement_email",
+        enabled,
+        "acknowledgement_email_content",
+        content,
     )
-    frappe.db.set_single_value("HD Settings", "acknowledgement_email_content", content)
-    return {
-        "enabled": enabled,
-        "content": content,
-    }
 
 
 @frappe.whitelist(methods=["PUT"])
 def update_reply_to_agents(enabled: bool, content: str):
     only_for_managers()
-
-    # todo: add input validation
-
-    frappe.db.set_single_value(
-        "HD Settings", "enable_reply_email_to_agent", int(enabled)
+    return set_common_settings(
+        "enable_reply_email_to_agent",
+        enabled,
+        "reply_email_to_agent_content",
+        content,
     )
-    frappe.db.set_single_value("HD Settings", "reply_email_to_agent_content", content)
-    return {
-        "enabled": enabled,
-        "content": content,
-    }
 
 
 @frappe.whitelist(methods=["PUT"])
 def update_reply_via_agent(enabled: bool, content: str):
     only_for_managers()
-
-    # todo: add input validation
-
-    frappe.db.set_single_value(
-        "HD Settings", "enable_reply_email_via_agent", int(enabled)
+    return set_common_settings(
+        "enable_reply_email_via_agent",
+        enabled,
+        "reply_via_agent_email_content",
+        content,
     )
-    frappe.db.set_single_value("HD Settings", "reply_via_agent_email_content", content)
-    return {
-        "enabled": enabled,
-        "content": content,
-    }
