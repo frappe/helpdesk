746fb39d1c86bd02fc69ecb0d58230ce7869df7d
Pratik Badhe <badhepd@gmail.com>
2025-10-09 07:35:28 +0000

    fix: handle json parse error & json validation on doctype save

--- helpdesk/extends/assignment_rule.py
+++ helpdesk/extends/assignment_rule.py
@@ -1,4 +1,7 @@
 import frappe
+from frappe import _
+
+from helpdesk.utils import is_json_valid
 
 
 def on_assignment_rule_trash(doc, event):
@@ -7,3 +10,21 @@ def on_assignment_rule_trash(doc, event):
         filters={"document_type": "HD Ticket", "name": ["!=", doc.name]},
     ):
         frappe.throw("There should atleast be 1 assignment rule for ticket")
+
+
+def on_assignment_rule_validate(doc, event):
+    if doc.assign_condition_json and not is_json_valid(doc.assign_condition_json):
+        frappe.throw(
+            _("The Assign Condition json '{0}' is invalid: {1}").format(
+                doc.assign_condition_json,
+                "Condition format should be like this e.g [['status','==','open']], it's recommended to use portal view to create conditions.",
+            )
+        )
+
+    if doc.unassign_condition_json and not is_json_valid(doc.unassign_condition_json):
+        frappe.throw(
+            _("The Unassign Condition json '{0}' is invalid: {1}").format(
+                doc.unassign_condition_json,
+                "Condition format should be like this e.g [['status','==','open']], it's recommended to use portal view to create conditions.",
+            )
+        )
