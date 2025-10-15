e53d52c2b73831a3ff9920dfdc4c6b7b7ff17cea
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-10-15 11:52:54 +0530

    chore: format error message in assignment rule
    (cherry picked from commit 2b62a32f55e6366592fbfbd034230f81bdca9953)

--- helpdesk/extends/assignment_rule.py
+++ helpdesk/extends/assignment_rule.py
@@ -15,7 +15,7 @@ def on_assignment_rule_trash(doc, event):
 def on_assignment_rule_validate(doc, event):
     if doc.assign_condition_json and not is_json_valid(doc.assign_condition_json):
         frappe.throw(
-            _("The Assign Condition json '{0}' is invalid: {1}").format(
+            _("The Assign Condition JSON '{0}' is invalid: </br> {1}").format(
                 doc.assign_condition_json,
                 "Condition format should be like this e.g [['status','==','open']], it's recommended to use portal view to create conditions.",
             )
@@ -23,7 +23,7 @@ def on_assignment_rule_validate(doc, event):
 
     if doc.unassign_condition_json and not is_json_valid(doc.unassign_condition_json):
         frappe.throw(
-            _("The Unassign Condition json '{0}' is invalid: {1}").format(
+            _("The Unassign Condition JSON '{0}' is invalid: </br> {1}").format(
                 doc.unassign_condition_json,
                 "Condition format should be like this e.g [['status','==','open']], it's recommended to use portal view to create conditions.",
             )
