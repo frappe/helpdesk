da4d1810b720ab89ab2f5398e8a19572fd27badf
Ritvik Sardana <sardanaritvik1211@gmail.com>
2025-11-25 16:35:06 +0530

    chore: code cleanup
    (cherry picked from commit 09ef6a1d18e4776c6d66a02c014df47ae7a36820)

--- desk/src/components/layouts/Sidebar.vue
+++ desk/src/components/layouts/Sidebar.vue
@@ -206,16 +206,6 @@ import {
   customerPortalSidebarOptions,
 } from "./layoutSettings";
 
-// Unavailable:
-// ImageExtension,ConfirmDialog
-
-// ### getCommonSiteConfig
-// this should not be part of src/index.ts since it is a nodejs utility and not for the browser.
-// we should probably put it in vite/index.ts
-
-import { saveLocal } from "frappe-ui";
-console.log(saveLocal("test_key", "test_value"));
-
 import { useShortcut } from "@/composables/shortcuts";
 import { useTelephonyStore } from "@/stores/telephony";
 import LucideArrowLeftFromLine from "~icons/lucide/arrow-left-from-line";
