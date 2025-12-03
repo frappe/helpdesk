e2a2d29b843a0445463ad42a0fd16145cbd5b0fd
Pratik <badhepd@gmail.com>
2025-12-03 12:19:42 +0000

    fix: bugs related to frappe-ui update

--- desk/src/components/Settings/General/components/TicketSettings.vue
+++ desk/src/components/Settings/General/components/TicketSettings.vue
@@ -35,34 +35,37 @@
           class="grid grid-cols-2 gap-4 mt-3"
           v-if="settingsData.restrictTicketsByAgentGroup"
         >
-          <div
-            class="flex items-start sm:items-center gap-2"
-            @click="
-              () => {
-                settingsData.doNotRestrictTicketsWithoutAnAgentGroup =
-                  !settingsData.doNotRestrictTicketsWithoutAnAgentGroup;
-              }
-            "
-          >
-            <Checkbox
-              :model-value="
-                settingsData.doNotRestrictTicketsWithoutAnAgentGroup
-              "
-            />
-            <FormLabel :label="__('Do not restrict tickets without a team')" />
+          <div class="flex items-start sm:items-center gap-2">
+            <div class="inline-flex space-x-2 rounded transition">
+              <input
+                class="rounded-sm mt-[1px] bg-surface-white border-outline-gray-4 text-ink-gray-9 hover:border-outline-gray-5 focus:ring-offset-0 focus:border-outline-gray-8 active:border-outline-gray-6 transition hover:shadow-sm focus:ring-0 focus-visible:ring-2 focus-visible:ring-outline-gray-3 active:bg-surface-gray-2 w-3.5 h-3.5"
+                type="checkbox"
+                id="doNotRestrictTicketsWithoutAnAgentGroup"
+                v-model="settingsData.doNotRestrictTicketsWithoutAnAgentGroup"
+              />
+              <label
+                class="block text-sm font-medium text-ink-gray-8 select-none"
+                for="doNotRestrictTicketsWithoutAnAgentGroup"
+              >
+                {{ __("Do not restrict tickets without a team") }}
+              </label>
+            </div>
           </div>
-          <div
-            class="flex items-start sm:items-center gap-2"
-            @click="
-              () => {
-                settingsData.assignWithinTeam = !settingsData.assignWithinTeam;
-              }
-            "
-          >
-            <Checkbox :model-value="settingsData.assignWithinTeam" />
-            <FormLabel
-              :label="__('Restrict agent assignment to selected team')"
-            />
+          <div class="flex items-start sm:items-center gap-2">
+            <div class="inline-flex space-x-2 rounded transition">
+              <input
+                class="rounded-sm mt-[1px] bg-surface-white border-outline-gray-4 text-ink-gray-9 hover:border-outline-gray-5 focus:ring-offset-0 focus:border-outline-gray-8 active:border-outline-gray-6 transition hover:shadow-sm focus:ring-0 focus-visible:ring-2 focus-visible:ring-outline-gray-3 active:bg-surface-gray-2 w-3.5 h-3.5"
+                type="checkbox"
+                id="assignWithinTeam"
+                v-model="settingsData.assignWithinTeam"
+              />
+              <label
+                class="block text-sm font-medium text-ink-gray-8 select-none"
+                for="assignWithinTeam"
+              >
+                {{ __("Restrict agent assignment to selected team") }}
+              </label>
+            </div>
           </div>
         </div>
       </div>
@@ -92,7 +95,7 @@
             }
           "
           target-class="max-w-40"
-          placement="bottom-end"
+          placement="bottom-start"
         />
       </div>
       <div class="flex items-center justify-between">
@@ -128,7 +131,7 @@
           :options="ticketTypeList.data"
           v-model="settingsData.defaultTicketType"
           target-class="max-w-40"
-          placement="bottom-end"
+          placement="bottom-start"
         />
       </div>
       <div>
@@ -160,7 +163,7 @@
                 }
               "
               target-class="w-full"
-              placement="bottom-end"
+              placement="bottom-start"
             />
           </div>
           <div class="flex flex-col gap-1.5">
