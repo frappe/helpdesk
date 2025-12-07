7fa910dee6d7a05d16ec2ce51d891a1b3c3e19cc
Pratik <badhepd@gmail.com>
2025-12-07 14:43:46 +0000

    fix: add patch for email template fields

--- helpdesk/patches/add_fields_in_assignment_rule.py
+++ helpdesk/patches/add_fields_in_assignment_rule.py
@@ -2,10 +2,10 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 from helpdesk.setup.install import (
     add_assignment_rule_property_setters,
-    get_custom_fields_for_assignment_rule,
+    get_custom_fields,
 )
 
 
 def execute():
-    create_custom_fields(get_custom_fields_for_assignment_rule())
+    create_custom_fields(get_custom_fields())
     add_assignment_rule_property_setters()
