61ff676bf424040eb2dd6fbc01ef0b1594cba1b3
samarth93 <palsamarth9@gmail.com>
2025-12-08 20:49:00 +0530

    feat: add comment reactions (thumbs up) feature
    Allows users to react to comments with a thumbs up emoji instead of
    creating new comments. This reduces ticket clutter when acknowledging
    a comment.
    
    Changes:
    - Add new HD Comment Reaction DocType to store reactions
    - Add toggle_reaction and get_comment_reactions API endpoints
    - Add reaction UI to CommentBox.vue with hover tooltip showing users
    - Send notification to comment author when reaction is added
    
    Backend:
    - helpdesk/helpdesk/doctype/hd_comment_reaction/ - new DocType
    - API: toggle_reaction(comment, reaction) - add/remove reaction
    - API: get_comment_reactions(comment) - list reactions with users
    
    Frontend:
    - CommentBox.vue - thumbs up button with count and user tooltip
    - Highlights button when current user has reacted
    - Real-time update via publish_event
    
    (cherry picked from commit d8c1f8ca1982856ab431b12dba819f09ae257917)

---
+++ helpdesk/helpdesk/doctype/hd_comment_reaction/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2025, Frappe Technologies and contributors
+# For license information, please see license.txt
---
+++ helpdesk/helpdesk/doctype/hd_comment_reaction/test_hd_comment_reaction.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2025, Frappe Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class UnitTestHDCommentReaction(UnitTestCase):
+    """
+    Unit tests for HDCommentReaction.
+    Use this class for testing individual functions and methods.
+    """
+
+    pass
+
+
+class IntegrationTestHDCommentReaction(IntegrationTestCase):
+    """
+    Integration tests for HDCommentReaction.
+    Use this class for testing interactions between multiple components.
+    """
+
+    pass
