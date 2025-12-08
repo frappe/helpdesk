5695f86e16465d04a7eca00c33d0f652d348540a
Pratik <badhepd@gmail.com>
2025-12-08 11:18:42 +0000

    refactor: change file name

--- desk/src/components/Settings/SavedReplies/SavedReplies.vue
+++ desk/src/components/Settings/SavedReplies/SavedReplies.vue
@@ -1,11 +1,11 @@
 <template>
   <SavedRepliesList v-if="savedRepliesActiveScreen.screen == 'list'" />
-  <SavedRepliesView v-else-if="savedRepliesActiveScreen.screen == 'view'" />
+  <SavedReplyView v-else-if="savedRepliesActiveScreen.screen == 'view'" />
 </template>
 
 <script setup lang="ts">
 import SavedRepliesList from "./SavedRepliesList.vue";
-import SavedRepliesView from "./SavedRepliesView.vue";
+import SavedReplyView from "./SavedReplyView.vue";
 import { provide, ref } from "vue";
 
 const savedRepliesActiveScreen = ref({
