8c8480d5585456fa695f83b1d9045660babe5dfe
samarth93 <palsamarth9@gmail.com>
2025-12-09 17:35:00 +0530

    refactor: store reactions as child table with emoji picker
    Based on feedback from @RitvikSardana:
    - Replace standalone HD Comment Reaction with HD Reaction child table
    - Store reactions inside HD Ticket Comment (like Gameplan)
    - Add emoji picker with multiple emoji options (👍👎❤️😄🎉😕👀🚀)
    - Users can only have one reaction per comment (clicking different emoji replaces)
    
    Changes:
    - New HD Reaction DocType (child table with emoji + user)
    - Add reactions Table field to HD Ticket Comment
    - Add toggle_reaction() and get_reactions() APIs
    - Update CommentBox.vue with emoji picker popover
    - Remove old HD Comment Reaction standalone DocType
    
    (cherry picked from commit 55ce67903bc7084a33f0dd1ef9bdefd74d275c95)

---
+++ helpdesk/helpdesk/doctype/hd_reaction/hd_reaction.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class HDReaction(Document):
+    pass
