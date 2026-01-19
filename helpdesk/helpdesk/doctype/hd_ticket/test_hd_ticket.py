e9aa8951593cb666b298f874fdc823b67c706ea7
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-01-19 23:43:19 +0530

    test: add agent_name field in test utils
    (cherry picked from commit 6944796c8ee5981b78c98f46fc167dc37ef9a833)

--- helpdesk/helpdesk/doctype/hd_ticket/test_hd_ticket.py
+++ helpdesk/helpdesk/doctype/hd_ticket/test_hd_ticket.py
@@ -51,9 +51,9 @@ class TestHDTicket(FrappeTestCase):
         frappe.get_doc(
             {"doctype": "User", "first_name": "Agent2", "email": agent2}
         ).insert(ignore_if_duplicate=True)
-        frappe.get_doc({"doctype": "HD Agent", "user": agent2}).insert(
-            ignore_if_duplicate=True
-        )
+        frappe.get_doc(
+            {"doctype": "HD Agent", "user": agent2, " agent_name": "agent2"}
+        ).insert(ignore_if_duplicate=True)
 
     def test_ticket_creation(self):
         ticket = frappe.get_doc(get_ticket_obj())
