f86732e1a8e532021449d898e1f3f6f44b2fe8fc
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-02-02 13:29:39 +0530

    fix: version bump
    (cherry picked from commit b219aefb204152d03ca865c5a2f167dceb6537e7)
    
    # Conflicts:
    #	helpdesk/__init__.py

--- helpdesk/__init__.py
+++ helpdesk/__init__.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 __version__ = "1.19.5"
 =======
 __version__ = "1.19.3"
 >>>>>>> d150c290 (fix: version bump)
+=======
+__version__ = "1.19.4"
+>>>>>>> b219aefb (fix: version bump)
