b0f275487afc921948ecbdbb096dd36961a63448
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-02-16 17:37:35 +0530

    fix: CP styles
    (cherry picked from commit 29ad7da21184e90abddea4aab125a931f00743d5)

--- desk/src/components/command-palette/CP.vue
+++ desk/src/components/command-palette/CP.vue
@@ -26,7 +26,7 @@
             <div
               v-for="group in groupedSearchResults"
               :key="group.title"
-              class="mt-4.5 mb-2 first:mt-3"
+              :class="group.title !== 'Jump To' && 'mt-4.5 mb-2 first:mt-3'"
             >
               <div
                 v-if="!group.hideTitle"
@@ -66,13 +66,12 @@ import {
   ComboboxOptions,
 } from "@headlessui/vue";
 
+import { __ } from "@/translation";
 import { Dialog } from "frappe-ui";
 import { computed, h, onBeforeUnmount, onMounted, ref, watch } from "vue";
 import { useRouter } from "vue-router";
-import { __ } from "@/translation";
 
 import LucideBookOpen from "~icons/lucide/book-open";
-import { showCommentBox, showEmailBox } from "@/pages/ticket/modalStates";
 import LucideTicket from "~icons/lucide/ticket";
 import CPGroup from "./CPGroup.vue";
 const router = useRouter();
@@ -94,6 +93,12 @@ const navigationItems = computed(() => {
         params: { ticketId: query.value.slice(1) },
       },
     });
+  } else if (query.value) {
+    return {
+      title: __("Jump to"),
+      component: h(CPGroup),
+      items,
+    };
   } else {
     items.push({
       title: __("Tickets"),
