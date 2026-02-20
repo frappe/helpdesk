8d59c75e6ad12019158bcabacab46ad6d81c7f59
Sydney <sydney223as@gmail.com>
2026-02-20 10:20:51 -0500

    chore: add fix for email editor

--- desk/src/components/EmailEditor.vue
+++ desk/src/components/EmailEditor.vue
@@ -12,7 +12,7 @@
     :placeholder="placeholder"
     :editable="editable"
     @change="editable ? (newEmail = $event) : null"
-    :extensions="[PreserveVideoControls]"
+    :extensions="[PreserveVideoControls, ExcelPasteFix]"
     :uploadFunction="(file:any)=>uploadFunction(file, doctype, ticketId)"
   >
     <template #top>
@@ -170,7 +170,7 @@ import {
 import { AttachmentIcon } from "@/components/icons";
 import { useTyping } from "@/composables/realtime";
 import { useAuthStore } from "@/stores/auth";
-import { PreserveVideoControls } from "@/tiptap-extensions";
+import { ExcelPasteFix, PreserveVideoControls } from "@/tiptap-extensions";
 import {
   getFontFamily,
   isContentEmpty,
