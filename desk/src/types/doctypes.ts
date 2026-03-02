7b317571a55ba7d659e97b1e22b2d4e5dfc9e82d
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-03-02 11:54:39 +0530

    chore: revert formating

--- desk/src/types/doctypes.ts
+++ desk/src/types/doctypes.ts
@@ -1,35 +1,22 @@
 interface DocType {
-  name: string;
-  creation: string;
-  modified: string;
-  owner: string;
-  modified_by: string;
-}
+    name: string;
+    creation: string;
+    modified: string;
+    owner: string;
+    modified_by: string;
+  }
 
-interface ChildDocType extends DocType {
-  parent?: string;
-  parentfield?: string;
-  parenttype?: string;
-  idx?: number;
-}
-
+  interface ChildDocType extends DocType {
+    parent?: string;
+    parentfield?: string;
+    parenttype?: string;
+    idx?: number;
+  }
+  
 // Last updated: 2026-02-02 17:22:11.094897
 export interface HDTicketStatus extends DocType {
   /** Color: Select */
-  color?:
-    | "Black"
-    | "Gray"
-    | "Blue"
-    | "Green"
-    | "Red"
-    | "Pink"
-    | "Orange"
-    | "Amber"
-    | "Yellow"
-    | "Cyan"
-    | "Teal"
-    | "Violet"
-    | "purple";
+  color?: 'Black' | 'Gray' | 'Blue' | 'Green' | 'Red' | 'Pink' | 'Orange' | 'Amber' | 'Yellow' | 'Cyan' | 'Teal' | 'Violet' | 'purple';
   /** Label: Data */
   label_agent: string;
   /** Show end users a different view: Check */
@@ -37,7 +24,7 @@ export interface HDTicketStatus extends DocType {
   /** Label (customer view): Data */
   label_customer?: string;
   /** Category: Select */
-  category: "Open" | "Paused" | "Resolved";
+  category: 'Open' | 'Paused' | 'Resolved';
   /** Order: Int */
   order?: number;
   /** Enabled: Check */
@@ -70,13 +57,7 @@ export interface HDTicket extends DocType {
   /** Response By: Datetime */
   response_by?: string;
   /** SLA Status: Select */
-  agreement_status?:
-    | ""
-    | "First Response Due"
-    | "Resolution Due"
-    | "Failed"
-    | "Fulfilled"
-    | "Paused";
+  agreement_status?: '' | 'First Response Due' | 'Resolution Due' | 'Failed' | 'Fulfilled' | 'Paused';
   /** Resolution By: Datetime */
   resolution_by?: string;
   /** SLA Creation: Datetime */
@@ -145,14 +126,7 @@ export interface AssignmentRuleUser extends ChildDocType {
 // Last updated: 2024-03-23 16:01:27.759155
 export interface AssignmentRuleDay extends ChildDocType {
   /** Day: Select */
-  day?:
-    | "Monday"
-    | "Tuesday"
-    | "Wednesday"
-    | "Thursday"
-    | "Friday"
-    | "Saturday"
-    | "Sunday";
+  day?: 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
 }
 
 // Last updated: 2025-08-25 17:09:11.644603
@@ -170,7 +144,7 @@ export interface AssignmentRule extends DocType {
   /** Unassign Condition: Code */
   unassign_condition?: string;
   /** Rule: Select */
-  rule: "Round Robin" | "Load Balancing" | "Based on Field";
+  rule: 'Round Robin' | 'Load Balancing' | 'Based on Field';
   /** Users: Table MultiSelect (Assignment Rule User) */
   users: AssignmentRuleUser[];
   /** Last User: Link (User) */
@@ -206,15 +180,7 @@ export interface HDServiceHolidayList extends DocType {
   /** Total Holidays: Int */
   total_holidays?: number;
   /** Weekly Off: Select */
-  weekly_off?:
-    | ""
-    | "Sunday"
-    | "Monday"
-    | "Tuesday"
-    | "Wednesday"
-    | "Thursday"
-    | "Friday"
-    | "Saturday";
+  weekly_off?: '' | 'Sunday' | 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday';
   /** Holidays: Table (HD Holiday) */
   holidays: HDHoliday[];
   /** Color: Color */
@@ -228,14 +194,7 @@ export interface HDServiceHolidayList extends DocType {
 // Last updated: 2021-10-21 14:27:08.190239
 export interface HDServiceDay extends ChildDocType {
   /** Workday: Select */
-  workday:
-    | "Monday"
-    | "Tuesday"
-    | "Wednesday"
-    | "Thursday"
-    | "Friday"
-    | "Saturday"
-    | "Sunday";
+  workday: 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
   /** Start Time: Time */
   start_time: any;
   /** End Time: Time */
