614a9f8ffd02460cddfc0f22fe4f715423cfe635
Sydney <sydney223as@gmail.com>
2026-03-17 00:26:31 +0530

    fix: prevent saved replies to run queries in modal selector view

--- helpdesk/api/saved_replies.py
+++ helpdesk/api/saved_replies.py
@@ -1,7 +1,22 @@
+import re
+
 import frappe
 from frappe import _
 
 from helpdesk.utils import agent_only
+
+
+def sanitize_template(template: str) -> str:
+    original = template
+    # Remove any jinja blocks/expressions containing frappe methods
+    template = re.sub(r"\{\{[^}]*\.[^}]*\}\}", "", template)
+    # Remove {% %} code block tags
+    template = re.sub(r"\{%.*?%\}", "", template, flags=re.DOTALL)
+    # Remove {# #} comments in case pasting payload through it
+    template = re.sub(r"\{#.*?#\}", "", template, flags=re.DOTALL)
+    if template != original:
+        frappe.throw(_("Saved reply contains disallowed syntax."))
+    return template
 
 
 @frappe.whitelist()
@@ -12,4 +27,7 @@ def get_rendered_saved_reply(ticket_id: str | int, saved_reply_id: str | None = 
     saved_reply = frappe.get_doc("HD Saved Reply", saved_reply_id).message
     ticket = frappe.get_doc("HD Ticket", ticket_id).as_dict()
     user = frappe.get_doc("User", frappe.session.user).as_dict()
-    return frappe.render_template(saved_reply, {**ticket, **user})
+    safe_saved_reply = sanitize_template(saved_reply)
+    return frappe.render_template(
+        safe_saved_reply, {**ticket, **user}, safe_render=True
+    )
