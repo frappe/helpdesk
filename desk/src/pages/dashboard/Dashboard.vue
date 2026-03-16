b2963c6d79a2afdf29f9f88d6565fd8cbd49c18b
Sydney <sydney223as@gmail.com>
2026-03-16 17:17:15 +0530

    fix: improve variable name and fix stats reload bug

--- desk/src/pages/dashboard/Dashboard.vue
+++ desk/src/pages/dashboard/Dashboard.vue
@@ -434,7 +434,7 @@ const { isManager, userId } = useAuthStore();
 
 const viewMyStats = ref(false);
 const activeTab = useStorage("dashboard_active_tab", "organization");
-function setView(myStats: boolean) {
+function validateView(myStats: boolean) {
   viewMyStats.value = myStats;
   if (myStats) {
     filters.team = null;
@@ -445,7 +445,7 @@ function setView(myStats: boolean) {
 }
 
 watch(activeTab, (val) => {
-  setView(val === "my_stats");
+  validateView(val === "my_stats");
 });
 
 watch(
@@ -631,7 +631,9 @@ onMounted(() => {
   } else {
     validateView(activeTab.value === "my_stats");
   }
-  setView(activeTab.value === "my_stats");
+  numberCards.reload();
+  masterData.reload();
+  trendData.reload();
 });
 
 usePageMeta(() => {
