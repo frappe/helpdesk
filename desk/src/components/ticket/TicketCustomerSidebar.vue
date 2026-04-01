8cc10bcd1685c077831737f365c0ee8c526c918c
Sydney <sydney223as@gmail.com>
2026-04-01 19:49:05 +0530

    fix: prevent non-date fields from being parsed as valid dates

--- desk/src/components/ticket/TicketCustomerSidebar.vue
+++ desk/src/components/ticket/TicketCustomerSidebar.vue
@@ -97,8 +97,8 @@
           <template
             v-if="
               field.value &&
-              dayjs(field.value).isValid() &&
-              (field.fieldtype === 'Date' || field.fieldtype === 'Datetime')
+              (field.fieldtype === 'Date' || field.fieldtype === 'Datetime') &&
+              dayjs(field.value).isValid()
             "
           >
             {{ dateFormat(field.value, dateTooltipFormat) }}
