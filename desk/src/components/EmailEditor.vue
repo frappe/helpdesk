31099aa202e8fd0fb971351bca06de2df8933e8b
Sydney <sydney223as@gmail.com>
2026-04-01 12:37:07 +0530

    fix: utilize tiptap select all command for tiptap based elements in dom
    (cherry picked from commit 3990ce5f6f1d00cb45b7ef019d2e199ba0739299)

--- desk/src/components/EmailEditor.vue
+++ desk/src/components/EmailEditor.vue
@@ -440,9 +440,11 @@ onMounted(() => {
 
 function handleSelectAll(e: KeyboardEvent) {
   const active = document.activeElement;
-  const editorDom = editorRef.value?.editor?.view?.dom as
-    | HTMLElement
-    | undefined;
+  const editorContext = editorRef.value?.editor;
+  if (editorContext) {
+    editorContext.commands.selectAll();
+  }
+  const editorDom = editorContext?.view?.dom as HTMLElement | undefined;
   const quotedEl = quotedContentRef.value;
   const sel = window.getSelection();
   if (!sel || !editorDom) return;
