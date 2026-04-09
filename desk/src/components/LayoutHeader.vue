aaf1a2973ede34acc46cfeddaa75e7868dc92172
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-04-09 10:09:22 +0530

    fix: subject cut in Ticket View

--- desk/src/components/LayoutHeader.vue
+++ desk/src/components/LayoutHeader.vue
@@ -4,10 +4,10 @@
       <header
         class="flex h-10.5 items-center justify-between mx-4 md:mx-5 md:mr-0"
       >
-        <div class="flex items-center gap-2 max-w-[70%] md:max-w-[50%]">
+        <div class="flex items-center gap-2 min-w-0 flex-1">
           <slot name="left-header" />
         </div>
-        <div class="flex items-center gap-2">
+        <div class="flex items-center gap-2 shrink-0">
           <slot name="right-header" class="flex items-center gap-2" />
         </div>
       </header>
