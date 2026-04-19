035cb0fe0c2c73b3be566be3df0bd2a4ba322165
Sydney <sydney223as@gmail.com>
2026-04-19 21:38:53 +0530

    fix: add extenstion for text editor used in customer portal
    (cherry picked from commit 0aab4f694f5132fed6e4842eaf892c27547048ba)

--- desk/src/components/TextEditor.vue
+++ desk/src/components/TextEditor.vue
@@ -2,7 +2,7 @@
   <div class="rounded p-3 shadow w-full">
     <FTextEditor
       ref="e"
-      :extensions="[ComponentUtils, HandleExcelPaste]"
+      :extensions="[ComponentUtils, HandleExcelPaste, CleanStyles]"
       v-bind="$attrs"
       :editor-class="[
         'prose-f max-h-64 max-w-none  overflow-auto my-4 min-h-[5rem]',
@@ -59,7 +59,11 @@
 <script setup lang="ts">
 import { UserAvatar } from "@/components";
 import { useAuthStore } from "@/stores/auth";
-import { ComponentUtils, HandleExcelPaste } from "@/tiptap-extensions";
+import {
+  CleanStyles,
+  ComponentUtils,
+  HandleExcelPaste,
+} from "@/tiptap-extensions";
 import { ClearFormattingUtility, getFontFamily } from "@/utils";
 import { TextEditor as FTextEditor, TextEditorFixedMenu } from "frappe-ui";
 import { computed, nextTick, ref } from "vue";
