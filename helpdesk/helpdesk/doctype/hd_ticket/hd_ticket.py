bf358d42478131b2c9019e7f76e70089994a3d11
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-05-01 12:25:00 +0530

    test: team and assignment rule creation
    (cherry picked from commit c29dba481bd0daacfaf69bb40204c1e59dfb66a9)

--- helpdesk/helpdesk/doctype/hd_ticket/hd_ticket.py
+++ helpdesk/helpdesk/doctype/hd_ticket/hd_ticket.py
@@ -84,7 +84,6 @@ class HDTicket(Document):
         self.set_feedback_values()
         self.set_default_status()
         self.set_status_category()
-        # self.apply_escalation_rule()
         self.set_sla()
 
         self.set_contact()
@@ -399,11 +398,6 @@ class HDTicket(Document):
                 not is_agent_in_assigned_team
             ) and self.users_present_in_team_assignment_rule():
                 clear_all_assignments("HD Ticket", self.name)
-                frappe.publish_realtime(
-                    "helpdesk:update-ticket-assignee",
-                    {"ticket_id": self.name},
-                    after_commit=True,
-                )
 
     def agent_in_assigned_team(self, agent, team):
         return frappe.db.exists(
@@ -858,19 +852,6 @@ class HDTicket(Document):
                     return rule
             except Exception:
                 pass
-
-    def apply_escalation_rule(self):
-        if not self.status_category == "Open" or self.is_new():
-            return
-        escalation_rule = self.get_escalation_rule()
-        if not escalation_rule:
-            return
-        self.agent_group = escalation_rule.to_team or self.agent_group
-        self.priority = escalation_rule.to_priority or self.priority
-        self.ticket_type = escalation_rule.to_ticket_type or self.ticket_type
-
-        if escalation_rule.to_agent:
-            self.assign_agent(escalation_rule.to_agent)
 
     def set_sla(self):
         """
