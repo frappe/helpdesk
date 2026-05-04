080787bb668d53a6765003272a8e1c89f5ccce67
Harshit Raizada <raizadaharshit2004@gmail.com>
2026-05-04 13:44:56 +0530

    chore: fix black formatting
    (cherry picked from commit f1d823a52c808374a7c42e93b1267d505c537078)

--- helpdesk/patches/set_hd_ticket_naming_series.py
+++ helpdesk/patches/set_hd_ticket_naming_series.py
@@ -1,19 +1,21 @@
-import frappe 
+import frappe
 from frappe import _
 
+
 def execute():
-    
+
     alter_hd_ticket_name_column()
-    
+
     max_id = get_largest_hd_ticket_id()
-    
+
     if not max_id:
         return
-    
+
     update_series_counter(max_id)
-    
+
     frappe.db.commit()
-    
+
+
 def get_current_column_type():
     result = frappe.db.sql(
         """
@@ -45,28 +47,33 @@ def alter_hd_ticket_name_column():
     print(
         f"alter_hd_ticket_name_to_varchar: changed `name` from {current_type} to VARCHAR(140)"
     )
-    
+
+
 def get_largest_hd_ticket_id():
-        result = frappe.db.sql("""
+    result = frappe.db.sql(
+        """
                                SELECT MAX(CAST(name as UNSIGNED))
                                FROM `tabHD Ticket`
-                               """)
-        
-        return result[0][0] if result else 0
-    
+                               """
+    )
+
+    return result[0][0] if result else 0
+
+
 def update_series_counter(max_id):
-        series_name = ""
-        existing_counter_val = frappe.db.sql("SELECT current FROM `tabSeries` WHERE name=%s", (series_name,))
-        current_counter_val = existing_counter_val[0][0] if existing_counter_val else None
-        
-        if current_counter_val is None:
-            frappe.db.sql("INSERT INTO `tabSeries` (name, current) VALUES (%s, %s)",
-            (series_name,max_id))
-            
-        elif max_id>current_counter_val:
-            frappe.db.sql(
-            "UPDATE `tabSeries` SET current=%s WHERE name=%s", 
-            (max_id, series_name)
+    series_name = ""
+    existing_counter_val = frappe.db.sql(
+        "SELECT current FROM `tabSeries` WHERE name=%s", (series_name,)
+    )
+    current_counter_val = existing_counter_val[0][0] if existing_counter_val else None
+
+    if current_counter_val is None:
+        frappe.db.sql(
+            "INSERT INTO `tabSeries` (name, current) VALUES (%s, %s)",
+            (series_name, max_id),
         )
-    
-    
+
+    elif max_id > current_counter_val:
+        frappe.db.sql(
+            "UPDATE `tabSeries` SET current=%s WHERE name=%s", (max_id, series_name)
+        )
