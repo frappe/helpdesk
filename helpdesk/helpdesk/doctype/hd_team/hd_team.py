151373f65934d4fab90de88a297b0440aa5655a4
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-05-11 19:28:29 +0530

    refactor: HD Team doctype codebase
    (cherry picked from commit d39d56f67de6d6c2141b74d37d54ec83eb75f55c)

--- helpdesk/helpdesk/doctype/hd_team/hd_team.py
+++ helpdesk/helpdesk/doctype/hd_team/hd_team.py
@@ -8,100 +8,35 @@ from frappe.model.naming import append_number_if_name_exists
 
 from helpdesk.utils import agent_only, capture_event
 
+ASSIGNMENT_DAYS = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+
 
 class HDTeam(Document):
-    def before_save(self):
-        if self.is_new():
-            return
-        assignment_rule_doc = frappe.get_doc("Assignment Rule", self.assignment_rule)
-        if self.has_value_changed("disabled"):
-            assignment_rule_doc.disabled = bool(self.disabled)
-        assignment_rule_doc.save(ignore_permissions=True)
-
     def after_insert(self):
         self.create_assignment_rule()
-        self.sync_users_to_assignment_rule()
         self.capture_team_creation_event()
 
-    def create_assignment_rule(self):
-        """Creates the assignment rule for this group"""
-        rule_doc = frappe.new_doc("Assignment Rule")
-        rule_doc.name = append_number_if_name_exists(
-            "Assignment Rule", f"{self.name} - Support Rotation"
-        )
-        rule_doc.document_type = "HD Ticket"
-        rule_doc.assign_condition = f"status == 'Open' and agent_group == '{self.name}'"
-        rule_doc.assign_condition_json = (
-            f'[["status","==","Open"],"and",["agent_group","==","{self.name}"]]'
-        )
-        rule_doc.unassign_condition = f"agent_group != '{self.name}'"
-        rule_doc.unassign_condition_json = f'[["agent_group","!=","{self.name}"]]'
-        rule_doc.priority = 1
-        if self.disabled:
-            rule_doc.disabled = True
-
-        for day in [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-        ]:
-            rule_doc.append(
-                "assignment_days", {"doctype": "Assignment Rule Day", "day": day}
-            )
-
-        rule_doc.save(ignore_permissions=True)
-        self.assignment_rule = rule_doc.name
-        self.save(ignore_permissions=True)
-
-    def sync_users_to_assignment_rule(self):
-        """
-        Syncs HD Team members into the linked AR's `users` and `weighted_users`.
-        """
-        if not self.assignment_rule:
-            return
-
-        members = [u.user for u in self.users if u.user]
-
-        ar_doc = frappe.get_doc("Assignment Rule", self.assignment_rule)
-
-        if ar_doc.get("rule") == "Weighted Distribution":
-            existing_weights = {
-                row.user: row.weight for row in ar_doc.weighted_users if row.user
-            }
-            ar_doc.weighted_users = []
-            for user in members:
-                ar_doc.append(
-                    "weighted_users",
-                    {"user": user, "weight": existing_weights.get(user, 1)},
-                )
-            ar_doc.save(ignore_permissions=True)
-            return
-        else:
-            ar_doc.users = []
-            for user in members:
-                ar_doc.append("users", {"user": user})
-            return ar_doc.save(ignore_permissions=True)
-
-    def capture_team_creation_event(self):
-        should_capture = self.name not in ["Product Experts", "Billing"]
-        if should_capture:
-            capture_event("team_created")
-
     def on_update(self):
-        self.sync_users_to_assignment_rule()
+        ar = frappe.get_doc("Assignment Rule", self.assignment_rule)
+        self.sync_users(ar)
+        ar.disabled = bool(self.disabled)
+        ar.save(ignore_permissions=True)
 
     def on_trash(self):
-        rule = self.assignment_rule
-        if not rule:
+        if not self.assignment_rule:
             return
         try:
             frappe.delete_doc(
                 "Assignment Rule",
-                rule,
+                self.assignment_rule,
                 ignore_permissions=True,
                 force=True,
                 ignore_on_trash=True,
@@ -110,31 +45,76 @@ class HDTeam(Document):
         except DoesNotExistError:
             frappe.log_error(
                 title="Assignment Rule not found",
-                message=f"Assignment Rule {rule} not found",
+                message=f"Assignment Rule {self.assignment_rule} not found",
             )
 
     def after_rename(self, olddn, newdn, merge=False):
-        rule = self.get_assignment_rule()
-        rule_doc = frappe.get_doc("Assignment Rule", rule)
-        rule_doc.assign_condition = f"status == 'Open' and agent_group == '{newdn}'"
-        rule_doc.assign_condition_json = (
-            f'[["status", "==", "Open"], "and", ["agent_group", "==", "{newdn}"]]'
-        )
-        rule_doc.unassign_condition = f"agent_group != '{newdn}'"
-        rule_doc.unassign_condition_json = f'[["agent_group", "!=", "{newdn}"]]'
-        rule_doc.save(ignore_permissions=True)
-
-    def get_assignment_rule(self):
         if not self.assignment_rule:
             self.create_assignment_rule()
-        return self.assignment_rule
+        ar = frappe.get_doc("Assignment Rule", self.assignment_rule)
+        ar.assign_condition, ar.assign_condition_json = self.assign_condition(newdn)
+        ar.unassign_condition, ar.unassign_condition_json = self.unassign_condition(
+            newdn
+        )
+        ar.save(ignore_permissions=True)
+
+    def create_assignment_rule(self):
+        ar = frappe.new_doc("Assignment Rule")
+        ar.name = append_number_if_name_exists(
+            "Assignment Rule", f"{self.name} - Support Rotation"
+        )
+        ar.document_type = "HD Ticket"
+        ar.assign_condition, ar.assign_condition_json = self.assign_condition(self.name)
+        ar.unassign_condition, ar.unassign_condition_json = self.unassign_condition(
+            self.name
+        )
+        ar.priority = 1
+        ar.disabled = bool(self.disabled)
+
+        for day in ASSIGNMENT_DAYS:
+            ar.append("assignment_days", {"doctype": "Assignment Rule Day", "day": day})
+
+        self.sync_users(ar)
+        ar.save(ignore_permissions=True)
+        self.assignment_rule = ar.name
+        self.save(ignore_permissions=True)
+
+    def sync_users(self, ar):
+        members = [u.user for u in self.users if u.user]
+
+        if ar.get("rule") == "Weighted Distribution":
+            existing_weights = {
+                row.user: row.weight for row in ar.weighted_users if row.user
+            }
+            ar.weighted_users = []
+            for user in members:
+                ar.append(
+                    "weighted_users",
+                    {"user": user, "weight": existing_weights.get(user, 1)},
+                )
+        else:
+            ar.users = []
+            for user in members:
+                ar.append("users", {"user": user})
+
+    def capture_team_creation_event(self):
+        if self.name not in ["Product Experts", "Billing"]:
+            capture_event("team_created")
+
+    def assign_condition(self, team_name: str) -> tuple[str, str]:
+        condition = f"status == 'Open' and agent_group == '{team_name}'"
+        condition_json = (
+            f'[["status","==","Open"],"and",["agent_group","==","{team_name}"]]'
+        )
+        return condition, condition_json
+
+    def unassign_condition(self, team_name: str) -> tuple[str, str]:
+        condition = f"agent_group != '{team_name}'"
+        condition_json = f'[["agent_group","!=","{team_name}"]]'
+        return condition, condition_json
 
 
 @frappe.whitelist()
 @agent_only
 def get_team_members(team: str):
-    """
-    Returns the team members for the given team name
-    """
-    members = frappe.get_all("HD Team Member", filters={"parent": team}, pluck="user")
-    return members
+    return frappe.get_all("HD Team Member", filters={"parent": team}, pluck="user")
