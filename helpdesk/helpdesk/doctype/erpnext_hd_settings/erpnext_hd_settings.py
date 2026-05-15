ada38a4a2d48a57b22457032c301accf80c8ea93
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-05-16 00:11:38 +0530

    feat: erpnext hd customer master sync
    (cherry picked from commit b1250c737f738b28cac5bb7ade77233b145d315d)

---
+++ helpdesk/helpdesk/doctype/erpnext_hd_settings/erpnext_hd_settings.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2026, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+
+class ERPNextHDSettings(Document):
+    # validate to check if ERPNext is installed or not, if enabled is checked then frapep.throw ERPNext is not installed on your site.
+    def validate(self):
+        if not self.enabled:
+            return
+        is_erpnext_installed = "erpnext" in frappe.get_installed_apps()
+        if not is_erpnext_installed:
+            # throw this error message if ERPNext is not installed on the site and enabled is checked
+            frappe.throw(
+                _(
+                    "ERPNext is not installed on your site. Please install ERPNext to enable this setting.",
+                )
+            )
--- helpdesk/helpdesk/doctype/hd_customer/hd_customer.js
+++ helpdesk/helpdesk/doctype/hd_customer/hd_customer.js
@@ -2,6 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("HD Customer", {
-  // refresh: function(frm) {
-  // }
+  // if erpnext is not installed on the site hide the field "erp_customer"
+  // TODO: hide field
+  refresh: function (frm) {
+    // if (!frappe.modules.erpnext) {
+    // frm.set_df_property("erp_customer", "hidden", true);
+    // }
+  },
 });
