352b1f45db5e3737f8611705df784aa696f0546a
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-05-21 13:06:46 +0530

    chore: code cleanup

--- helpdesk/helpdesk/doctype/hd_customer/hd_customer.js
+++ helpdesk/helpdesk/doctype/hd_customer/hd_customer.js
@@ -2,11 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("HD Customer", {
-  // if erpnext is not installed on the site hide the field "erpnext_customer"
-  // TODO: hide field
-  refresh: function (frm) {
-    // if (!frappe.modules.erpnext) {
-    // frm.set_df_property("erpnext_customer", "hidden", true);
-    // }
-  },
+  // refresh: function (frm) {
+  // },
 });
