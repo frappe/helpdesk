0da10a0bed80e1335edcdd894568c255e72bb14c
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-05-21 13:06:46 +0530

    chore: code cleanup
    (cherry picked from commit 352b1f45db5e3737f8611705df784aa696f0546a)

--- helpdesk/helpdesk/doctype/erpnext_hd_settings/test_erpnext_hd_settings.py
+++ helpdesk/helpdesk/doctype/erpnext_hd_settings/test_erpnext_hd_settings.py
@@ -4,7 +4,7 @@
 from unittest.mock import patch
 
 import frappe
-from frappe.tests import IntegrationTestCase
+from frappe.tests.utils import FrappeTestCase
 
 # On IntegrationTestCase, the doctype test records and all
 # link-field test record dependencies are recursively loaded
@@ -13,7 +13,7 @@ EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
 
-class IntegrationTestERPNextHDSettings(IntegrationTestCase):
+class IntegrationTestERPNextHDSettings(FrappeTestCase):
     """
     Integration tests for ERPNextHDSettings.
     Use this class for testing interactions between multiple components.
@@ -27,15 +27,3 @@ class IntegrationTestERPNextHDSettings(IntegrationTestCase):
             settings_doc.save()
         except Exception as e:
             self.fail(f"Validation failed when ERPNext is installed: {e}")
-
-    # TODO: Make this test case work
-    # @patch("frappe.get_installed_apps", return_value=["frappe", "helpdesk"])
-    # def test_validate_without_erpnext_installed(self, mock_get_installed_apps):
-    #     settings_doc = frappe.get_single("ERPNext HD Settings")
-    #     settings_doc.enabled = 1
-    #     with self.assertRaises(frappe.ValidationError) as context:
-    #         settings_doc.validate()
-    #     self.assertIn(
-    #         "ERPNext is not installed on your site. Please install ERPNext to enable this setting.",
-    #         str(context.exception),
-    #     )
