7ee822347fa5c60c1e8b910c4a3aed05932f4259
Ritvik Sardana <sardanaritvik1211@gmail.com>
2026-06-03 10:08:29 +0530

    fix: move ERPNext integration components
    (cherry picked from commit ac44791470a35508e1a6973c22fe947b581d9ec5)
    
    # Conflicts:
    #	desk/src/components/Settings/General/General.vue

---
+++ desk/src/components/icons/ERPNextSettingsIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M11.6611 8.2289V9.77773H7.88672V11.9066H11.999V13.4545H6V8.2289H11.6611ZM11.9512 4.6V6.14883H6V4.6H11.9512Z"
+      fill="currentColor"
+    />
+    <rect
+      x="1.5"
+      y="1.5"
+      width="15"
+      height="15"
+      rx="3.5"
+      stroke="currentColor"
+    />
+  </svg>
+</template>
