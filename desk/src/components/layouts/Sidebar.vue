e2b5619cc60efa5c78db93e365324780b049ed0b
Sydney <sydney223as@gmail.com>
2026-06-10 16:38:28 +0530

    fix: sidebar fc banner spacing

--- desk/src/components/layouts/Sidebar.vue
+++ desk/src/components/layouts/Sidebar.vue
@@ -107,7 +107,7 @@
     </div>
     <div class="grow" />
     <div class="flex flex-col gap-2 pb-2.5">
-      <div class="px-2">
+      <div class="px-2 flex flex-col gap-2">
         <TrialBanner
           v-if="isFCSite && !isCustomerPortal"
           :isSidebarCollapsed="!isExpanded"
